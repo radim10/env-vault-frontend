cdb19310a2e5e98c84793c75b013340ffbbcf1ea
dimak <rahbussiness@gmail.com>
2023-09-21 20:41:36 +0200

    use header

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,19 @@
+import Header from '@/components/Header'
 import Sidebar from '@/components/Sidebar'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col ">
-      <div className="w-[240px] sticky h-screen">
-        <Sidebar />
+    <>
+      <div className="flex flex-row ">
+        <div className="w-[280px] lg:w-[300px] sticky h-screen">
+          <Sidebar />
+        </div>
+        <div className="h-full w-full flex-grow px-6 py-6 lg:px-10">
+          <Header />
+          <div></div>
+          {children}
+        </div>
       </div>
-      <div>{children}</div>
-    </div>
+    </>
   )
 }
