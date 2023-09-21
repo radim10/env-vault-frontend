918537b1e9f6264304abf3b14420ce5e239ca598
dimak <rahbussiness@gmail.com>
2023-09-21 20:12:04 +0200

    icons

---
+++ src/components/icons.tsx
@@ -0,0 +1,11 @@
+import { Users, Folder, KeyRound, Settings, Book, Mic2, Code } from 'lucide-react'
+
+export const Icons = {
+  users: Users,
+  folder: Folder,
+  keyRound: KeyRound,
+  settings: Settings,
+  book: Book,
+  mic2: Mic2,
+  code: Code,
+}
