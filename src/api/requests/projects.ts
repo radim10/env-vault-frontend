588c4f94a3f8ef0d3047a674d060d96aac736737
dimak <rahbussiness@gmail.com>
2023-09-22 17:08:39 +0200

    return type

--- src/api/requests/projects.ts
+++ src/api/requests/projects.ts
@@ -1,20 +1,16 @@
+import { ListProject } from '@/types/projects'
 import sendRequest, { APIError } from '../instance'
 
 export type GetProjectsError = APIError<'Collection not found'>
 export type GetProjectsData = Awaited<ReturnType<typeof getProjects>>
 
 export async function getProjects(args: { workspaceId: string }) {
-  const response = sendRequest<
-    Array<{
-      id: string
-      name: string
-      description: string | null
-      environmentCount: number
-    }>
-  >({
+  const { workspaceId } = args
+
+  const response = sendRequest<Array<ListProject>>({
     method: 'GET',
-    basePath: 'projects',
-    path: args.workspaceId,
+    basePath: 'workspaces',
+    path: `${workspaceId}/projects`,
   })
   return await response
 }
