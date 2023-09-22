e35b8262b00e8609bed910b0cfe5c0732a1ecf56
dimak <rahbussiness@gmail.com>
2023-09-22 17:08:28 +0200

    project types

---
+++ src/types/projects.ts
@@ -0,0 +1,10 @@
+interface Project {
+  id: string
+  createdAt: string
+  description: string
+  name: string
+}
+
+export type ListProject = Pick<Project, 'createdAt' | 'description' | 'name'> & {
+  environmentCount: number
+}
