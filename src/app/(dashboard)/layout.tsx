0faf4e5b84458419324bbd11c48e16ae3fd5fe25
dimak <rahbussiness@gmail.com>
2023-09-22 21:09:57 +0200

    sticky sidebar pos

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,8 +4,8 @@ import Sidebar from '@/components/Sidebar'
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="flex flex-row ">
-        <div className="w-[280px] lg:w-[320px] sticky h-screen">
+      <div className="flex flex-row">
+        <div className="w-[280px] lg:w-[320px] sticky top-0 h-screen">
           <Sidebar />
         </div>
         <div className="h-full w-full flex-grow px-6 py-6 lg:px-10">
