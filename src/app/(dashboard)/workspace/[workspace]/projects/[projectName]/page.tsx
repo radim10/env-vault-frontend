5166105b08e031d57e99bf7a91947332cfceb9b2
dimak <rahbussiness@gmail.com>
2023-09-22 17:47:39 +0200

    project page

--- src/app/(dashboard)/workspace/[workspace]/projects/[projectName]/page.tsx
+++ src/app/(dashboard)/workspace/[workspace]/projects/[projectName]/page.tsx
@@ -1,13 +1,49 @@
-import TypographyH2 from '@/components/typography/TypographyH2'
+'use client'
+import { useGetProject } from '@/api/queries/projects'
+import { Icons } from '@/components/icons'
+import Link from 'next/link'
 
 // TODO: load project SSR?
 
-export default function ProjectPage({ params }: { params: { projectName: string } }) {
+export default function ProjectPage({
+  params,
+}: {
+  params: { workspace: string; projectName: string }
+}) {
+  const {
+    data: project,
+    isLoading,
+    isError,
+  } = useGetProject({
+    workspaceId: params.workspace,
+    projectName: params.projectName,
+  })
+
+  if (isLoading) {
+    return 'loading'
+  }
+
+  if (isError) {
+    return 'error'
+  }
+
   return (
     <>
       <div>
         <div className="flex justify-between items-center">
-          <TypographyH2>Projects</TypographyH2>
+          <div className="flex gap-2 items-center">
+            <Link
+              href={`/workspace/${params.workspace}/projects`}
+              className="text-primary hover:text-primary hover:underline underline-offset-4"
+            >
+              <div className="font-semibold text-2xl">Projects</div>
+            </Link>
+            {/* // */}
+            <div className="dark:text-gray-400">
+              <Icons.chevronRight className="mt-1" />
+            </div>
+            <div className="font-semibold text-2xl ">{project.name}</div>
+          </div>
         </div>
       </div>
     </>
