17b52d4b759f838f06043693c29a543830d0d7f1
dimak <rahbussiness@gmail.com>
2023-09-23 12:47:21 +0200

    Delete project dialog

---
+++ src/components/projects/DeleteProjectDialog.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import DeleteDialog from '../DeleteDialog'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
+import { Icons } from '../icons'
+import { useDeleteProject } from '@/api/mutations/projects'
+
+interface Props {
+  workspaceId: string
+  projectName: string
+  opened: boolean
+  onClose: () => void
+  onSuccess: () => void
+}
+
+const DeleteProjectDialog: React.FC<Props> = ({
+  workspaceId,
+  projectName,
+  opened,
+  onClose,
+  onSuccess,
+}) => {
+  const [name, setName] = useState('')
+
+  const {
+    mutate: deleteProject,
+    isLoading,
+    error,
+  } = useDeleteProject({
+    onSuccess: () => onSuccess(),
+  })
+
+  return (
+    <div>
+      <DeleteDialog
+        opened={opened}
+        onClose={onClose}
+        inProgress={isLoading}
+        disabledConfirm={name !== projectName}
+        title="Delete this project?"
+        onConfirm={() => deleteProject({ workspaceId, name })}
+      >
+        <div className="flex flex-col gap-4 p0-4 pb-4">
+          <div className="text-[0.92rem]">
+            Type <span className="font-bold text-red-600">{projectName}</span> to confirm this
+            action.
+          </div>
+
+          <Input
+            placeholder={projectName}
+            disabled={isLoading}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          {error?.message && (
+            <div className="text-red-600 text-[0.92rem] flex items-center gap-2 -mt-1">
+              <Icons.xCircle className="h-4 w-4" />
+              {error?.message}
+            </div>
+          )}
+        </div>
+      </DeleteDialog>
+    </div>
+  )
+}
+
+export default DeleteProjectDialog
