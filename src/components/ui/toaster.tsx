411b34851b1117bd688e5390e54a5476566ff0c7
dimak <rahbussiness@gmail.com>
2023-09-23 14:04:21 +0200

    toast variants

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import {
   Toast,
@@ -7,27 +7,46 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from '@/components/ui/toast'
+import { useToast } from '@/components/ui/use-toast'
+import { AlertCircle, CheckCircleIcon } from 'lucide-react'
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({ id, title, description, variant, action, ...props }) {
         return (
           <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
+            <div className="flex items-center gap-4">
+              {variant && variant === 'success' && (
+                <CheckCircleIcon className="h-5 w-5 text-green-600" />
               )}
+              {variant && variant === 'destructive' && (
+                <AlertCircle className="h-5 w-5 text-destructive" />
+              )}
+              <div className="grid gap-1">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && <ToastDescription>{description}</ToastDescription>}
+              </div>
             </div>
-            {action}
+            {action && action}
             <ToastClose />
           </Toast>
         )
+
+        // return (
+        //
+        //   <Toast key={id} {...props}>
+        //     <div className="grid gap-1">
+        //       {title && <ToastTitle>{title}</ToastTitle>}
+        //       {description && <ToastDescription>{description}</ToastDescription>}
+        //     </div>
+        //     {action}
+        //     <ToastClose />
+        //   </Toast>
+        // )
       })}
       <ToastViewport />
     </ToastProvider>
