f053c9c9254a810efbc7bbfd8cb36c5867dfb945
dimak <rahbussiness@gmail.com>
2023-09-24 17:14:32 +0200

    Updated secret type

--- src/types/secrets.ts
+++ src/types/secrets.ts
@@ -2,3 +2,14 @@ export interface Secret {
   key: string
   value: string
 }
+
+export interface UpdatedSecret {
+  // original key
+  key?: string
+
+  newKey?: string
+  newValue?: string
+  deleted?: boolean
+}
+
+export type UpdatedSecretsBody = Array<UpdatedSecret>
