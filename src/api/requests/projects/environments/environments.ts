f4796cf94cc5f42b2b9d3c98487da5d1179c3755
dimak <rahbussiness@gmail.com>
2023-09-25 11:15:52 +0200

    rename environment api request

---
+++ src/api/requests/projects/environments/environments.ts
@@ -0,0 +1,29 @@
+import sendRequest, { APIError } from '@/api/instance'
+
+// TODO: error
+export type RenameEnvironmentErrorData = APIError<
+  | 'Workspace not found'
+  | 'Project not found'
+  | 'Environment not found'
+  | 'Environment already exists'
+>
+export type RenameEnvironmentResData = undefined
+
+export async function renameEnvironment(args: {
+  workspaceId: string
+  projectName: string
+  envName: string
+  data: {
+    name: string
+  }
+}) {
+  const { workspaceId, projectName, envName, data } = args
+
+  const response = sendRequest<RenameEnvironmentResData>({
+    method: 'PATCH',
+    basePath: 'workspaces',
+    path: `${workspaceId}/projects/${projectName}/environments/${envName}/rename`,
+    body: data,
+  })
+  return await response
+}
