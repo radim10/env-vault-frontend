84fdda51f3ce96e4754b43be83aaae0eaddbe41c
dimak <rahbussiness@gmail.com>
2023-09-25 11:15:45 +0200

    removed duplicate code

--- src/api/requests/projects/environments/secrets.ts
+++ src/api/requests/projects/environments/secrets.ts
@@ -1,43 +1,5 @@
 import sendRequest, { APIError } from '@/api/instance'
 import { Secret, UpdatedSecretsBody } from '@/types/secrets'
-import {
-  getProject,
-  GetProjectData,
-  GetProjectError,
-  getProjects,
-  GetProjectsData,
-  GetProjectsError,
-} from '@/api/requests/projects/root'
-import { UseQueryOptions, useQuery } from '@tanstack/react-query'
-
-export const useGetProjects = (
-  args: {
-    workspaceId: string
-  },
-  opt?: UseQueryOptions<GetProjectsData, GetProjectsError>
-) =>
-  useQuery<GetProjectsData, GetProjectsError>(
-    ['projects', args.workspaceId],
-    () => {
-      return getProjects(args)
-    },
-    opt
-  )
-
-export const useGetProject = (
-  args: {
-    workspaceId: string
-    projectName: string
-  },
-  opt?: UseQueryOptions<GetProjectData, GetProjectError>
-) =>
-  useQuery<GetProjectData, GetProjectError>(
-    ['project', args.workspaceId, args.projectName],
-    () => {
-      return getProject(args)
-    },
-    opt
-  )
 
 // TODO: error
 export type GetSecretsError = APIError<
