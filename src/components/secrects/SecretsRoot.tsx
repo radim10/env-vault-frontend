c2618b58c8e58a6183bfa116e4e951369b13e1c2
dimak <rahbussiness@gmail.com>
2023-09-25 13:36:04 +0200

    show secrets list skeleton

--- src/components/secrects/SecretsRoot.tsx
+++ src/components/secrects/SecretsRoot.tsx
@@ -3,6 +3,7 @@
 import { useGetSecrets } from '@/api/queries/projects/environments/secrets'
 import React from 'react'
 import SecretsList from './SecretsList'
+import SecretsListSkeleton from './SecretsListSkeleton'
 
 interface Props {
   workspaceId: string
@@ -14,7 +15,11 @@ const SecretsRoot: React.FC<Props> = ({ workspaceId, projectName, envName }) => 
   const { data: secrets, isLoading, error } = useGetSecrets({ workspaceId, projectName, envName })
 
   if (isLoading) {
-    return <div>Loading</div>
+    return (
+      <div>
+        <SecretsListSkeleton />
+      </div>
+    )
   }
 
   if (error) {
