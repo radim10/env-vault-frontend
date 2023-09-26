cb388d8e2dfcef350aa1dc1179a34eb80df19abe
dimak <rahbussiness@gmail.com>
2023-09-26 16:41:09 +0200

    action buttons

--- src/components/secrects/SecretsToolbar.tsx
+++ src/components/secrects/SecretsToolbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 import { Button } from '../ui/button'
 import { Icons } from '../icons'
 import { Input } from '../ui/input'
@@ -9,20 +10,84 @@ interface Props {
 }
 
 const SecretsToolbar: React.FC<Props> = ({ secretsCount }) => {
-  const { search, setSearch } = useEditedSecretsStore((state) => {
-    return {
-      search: state.search,
-      setSearch: state.setSearch,
-    }
-  })
+  const { secrets, search, setSearch, toggleVisibilityAll, toggleDescriptionAll } =
+    useEditedSecretsStore((state) => {
+      return {
+        secrets: state.secrets,
+        search: state.search,
+        setSearch: state.setSearch,
+        toggleVisibilityAll: state.toggleVisibilityAll,
+        toggleDescriptionAll: state.toggleDescriptionAll,
+      }
+    })
   return (
     <div>
-      <div className="flex justify-between items-center">
+      <div className="flex flex-col md:flex-row justify-between md:items-center">
         <div className="pl-1 dark:text-gray-400 font-bold">
           <span>Active count: {secretsCount}</span>
         </div>
         {/* */}
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-3 items-center mt-3 md:mt-0">
+          <div className="flex items-center gap-2">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  {secrets
+                    ?.filter(
+                      (val) =>
+                        val?.description || (val?.newDescription && val?.newDescription?.length > 0)
+                    )
+                    .every((val) => val?.showDescription === true) ? (
+                    <Button variant={'outline'} onClick={() => toggleDescriptionAll(true)}>
+                      <Icons.fileX className="h-4 w-4" />
+                    </Button>
+                  ) : (
+                    <Button variant={'outline'} onClick={() => toggleDescriptionAll(false)}>
+                      <Icons.fileText className="h-4 w-4" />
+                    </Button>
+                  )}
+                </TooltipTrigger>
+                <TooltipContent>
+                  {secrets
+                    ?.filter(
+                      (val) =>
+                        val?.description || (val?.newDescription && val?.newDescription?.length > 0)
+                    )
+                    .every((val) => val?.showDescription === true) ? (
+                    <p>Hide all descriptions</p>
+                  ) : (
+                    <p>Show all descriptions</p>
+                  )}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  {!secrets?.every((val) => val?.hidden === false) && (
+                    <Button variant="outline" onClick={() => toggleVisibilityAll(false)}>
+                      <Icons.eye className="h-4 w-4" />
+                    </Button>
+                  )}
+
+                  {secrets?.every((val) => val?.hidden === false) && (
+                    <Button variant="outline" onClick={() => toggleVisibilityAll(true)}>
+                      <Icons.eyeOff className="h-4 w-4" />
+                    </Button>
+                  )}
+                </TooltipTrigger>
+                <TooltipContent>
+                  {!secrets?.every((val) => val?.hidden === false) ? (
+                    <p>Reveal all secrets</p>
+                  ) : (
+                    <p>Hide all secrets</p>
+                  )}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+
           <div className="relative">
             <Icons.search className="h-4 w-4 pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-3" />
 
