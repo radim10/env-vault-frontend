76bbacd3012e231d7185a7cb3d27ac7a731ce5b5
dimak <rahbussiness@gmail.com>
2023-09-27 14:37:25 +0200

    EnvironmentType enum

--- src/types/environments.ts
+++ src/types/environments.ts
@@ -1,4 +1,12 @@
+export enum EnvironmentType {
+  DEVELOPMENT = 'DEVELOPMENT',
+  TESTING = 'TESTING',
+  STAGING = 'STAGING',
+  PRODUCTION = 'PRODUCTION',
+}
+
 export interface Environment {
   name: string
   locked: boolean
+  type: EnvironmentType
 }
