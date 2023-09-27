b390a985ed7b0ee84a55034bfc5566c7d76ef8bc
dimak <rahbussiness@gmail.com>
2023-09-27 14:57:37 +0200

    select env type

--- src/components/environments/CreateEnvironmentDialog.tsx
+++ src/components/environments/CreateEnvironmentDialog.tsx
@@ -11,17 +11,34 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 import { Icons } from '../icons'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { useCreateEnvironment } from '@/api/mutations/environments'
+import { EnvironmentType } from '@/types/environments'
+import { Badge } from '../ui/badge'
+import clsx from 'clsx'
 
 interface Props {
   workspaceId: string
   projectName: string
   btnText?: string
-  onSuccess: (name: string) => void
+  onSuccess: (args: { name: string; type: EnvironmentType }) => void
 }
+
+const envTypes: EnvironmentType[] = [
+  EnvironmentType.DEVELOPMENT,
+  EnvironmentType.TESTING,
+  EnvironmentType.STAGING,
+  EnvironmentType.PRODUCTION,
+]
 
 const CreateEnvironmentDialog: React.FC<Props> = ({
   workspaceId,
@@ -30,6 +47,7 @@ const CreateEnvironmentDialog: React.FC<Props> = ({
   onSuccess,
 }) => {
   const [opened, setOpened] = useState(false)
+  const [selectedType, setSelectedType] = useState<EnvironmentType | null>(null)
   const [name, setName] = useState('')
 
   const {
@@ -38,7 +56,7 @@ const CreateEnvironmentDialog: React.FC<Props> = ({
     error,
   } = useCreateEnvironment({
     onSuccess: () => {
-      onSuccess(name)
+      onSuccess({ name: name, type: selectedType as EnvironmentType })
       setOpened(false)
     },
   })
@@ -76,7 +94,42 @@ const CreateEnvironmentDialog: React.FC<Props> = ({
             </DialogDescription>
           </DialogHeader>
           <div className="flex flex-col gap-4 py-4 text-lg">
-            <div className="flex flex-col gap-1.5 items-start justify-center">
+            <div className="flex flex-col gap-2 items-start justify-center">
+              <Label htmlFor="type" className="text-right pl-1">
+                Type
+              </Label>
+              <Select onValueChange={(value) => setSelectedType(value as EnvironmentType)}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select type" />
+                </SelectTrigger>
+                <SelectContent>
+                  {envTypes.map((type) => (
+                    <SelectItem value={type} key={type}>
+                      <Badge
+                        variant="default"
+                        className={clsx(['text-[0.725rem] text-gray-200'], {
+                          'bg-indigo-600 dark:bg-indigo-800/80 hover:bg-indigo-600 dark:hover:bg-indigo-800/80':
+                            type === EnvironmentType.DEVELOPMENT,
+                          'bg-blue-600 dark:bg-blue-800/80 hover:bg-blue-600 dark:hover:bg-blue-800/80':
+                            type === EnvironmentType.TESTING,
+                          'bg-green-600 dark:bg-green-800/80 hover:bg-green-600 dark:hover:bg-green-800/80':
+                            type === EnvironmentType.STAGING,
+                          'bg-red-600 dark:bg-red-800/80 hover:bg-red-600 dark:hover:bg-red-800/80':
+                            type === EnvironmentType.PRODUCTION,
+                        })}
+                      >
+                        {type === EnvironmentType.DEVELOPMENT && 'Development'}
+                        {type === EnvironmentType.TESTING && 'Testing'}
+                        {type === EnvironmentType.STAGING && 'Staging'}
+                        {type === EnvironmentType.PRODUCTION && 'Production'}
+                      </Badge>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="flex flex-col gap-2 items-start justify-center">
               <Label htmlFor="name" className="text-right pl-1">
                 Name
               </Label>
@@ -89,8 +142,6 @@ const CreateEnvironmentDialog: React.FC<Props> = ({
               />
             </div>
 
-            <div className="flex flex-col gap-1.5 items-start justify-center">Type dropdown</div>
-
             {error?.message && (
               <div className="text-red-600 text-[0.92rem] flex items-center gap-2 -mt-1">
                 <Icons.xCircle className="h-4 w-4" />
@@ -102,8 +153,14 @@ const CreateEnvironmentDialog: React.FC<Props> = ({
             <Button
               type="submit"
               loading={isLoading}
-              disabled={name?.trim().length === 0 || isLoading}
-              onClick={() => createEnvironment({ workspaceId, projectName, data: { name } })}
+              disabled={name?.trim().length === 0 || isLoading || selectedType === null}
+              onClick={() => {
+                createEnvironment({
+                  workspaceId,
+                  projectName,
+                  data: { name, type: selectedType as EnvironmentType },
+                })
+              }}
             >
               Create
             </Button>
