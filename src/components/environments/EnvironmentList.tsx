e0dce6e42a98311aee628e367b8f4b88acdca95b
dimak <rahbussiness@gmail.com>
2023-09-27 10:45:04 +0200

    handle new env

--- src/components/environments/EnvironmentList.tsx
+++ src/components/environments/EnvironmentList.tsx
@@ -1,23 +1,74 @@
 import React from 'react'
-import { Environment } from '@/types/environments'
 import Link from 'next/link'
 import { Badge } from '../ui/badge'
 import { Icons } from '../icons'
 import { Button } from '../ui/button'
-import { ListEnvironment } from '@/types/projects'
+import { ListEnvironment, Project } from '@/types/projects'
 import clsx from 'clsx'
 import EnvironmentListToolbar from './EnvironmentListToolbar'
+import { QueryClient } from '@tanstack/react-query'
+import CreateEnvironmentDialog from './CreateEnvironmentDialog'
 
 interface Props {
+  queryClient: QueryClient
+  //
   workspaceId: string
-  values: ListEnvironment[]
   projectName: string
+
+  //
+  values: ListEnvironment[]
 }
 
-export const EnvironmentList: React.FC<Props> = ({ workspaceId, projectName, values }) => {
+export const EnvironmentList: React.FC<Props> = ({
+  queryClient,
+  workspaceId,
+  projectName,
+  values,
+}) => {
+  const handleNewEnvironment = (name: string) => {
+    const data = queryClient.getQueryData<Project>(['project', workspaceId, projectName])
+
+    if (data) {
+      queryClient.setQueryData(['project', workspaceId, projectName], {
+
+        ...data,
+        environments: [{ name, secretsCount: 0 }, ...data?.environments],
+      })
+    }
+  }
+
+  if (!values?.length) {
+    return (
+      <div className="flex items-center justify-center mt-36">
+        <div className="flex flex-col items-center gap-2">
+          <div>
+            <Icons.inbox className="h-20 w-20 opacity-30" />
+          </div>
+          <div className="text-center">
+            <span className="text-lg font-bold opacity-85">No environments here...</span>
+            <div className="my-1">Add environments to your project</div>
+            <div className="mt-5">
+              <CreateEnvironmentDialog
+                workspaceId={workspaceId}
+                projectName={projectName}
+                onSuccess={handleNewEnvironment}
+                btnText="Add environment"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
-      <EnvironmentListToolbar environmentsCount={values.length} />
+      <EnvironmentListToolbar
+        workspaceId={workspaceId}
+        projectName={projectName}
+        environmentsCount={values.length}
+        onCreated={handleNewEnvironment}
+      />
 
       <div className="flex flex-col gap-3 mt-6">
         {values.map(({ name, secretsCount }, index) => (
