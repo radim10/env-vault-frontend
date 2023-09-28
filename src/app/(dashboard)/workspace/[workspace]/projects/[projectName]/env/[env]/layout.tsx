85f83de4f0e01d5302d7f4e53b42e8d4cb6d3270
dimak <rahbussiness@gmail.com>
2023-09-28 13:07:53 +0200

    layout - get environmnet

--- src/app/(dashboard)/workspace/[workspace]/projects/[projectName]/env/[env]/layout.tsx
+++ src/app/(dashboard)/workspace/[workspace]/projects/[projectName]/env/[env]/layout.tsx
@@ -1,9 +1,13 @@
 'use client'
 
+import { useGetEnvironment } from '@/api/queries/projects/environments/environments'
 import EnvTabs from '@/components/environments/EnvTabs'
 import { Icons } from '@/components/icons'
+import NotFound from '@/components/projects/NotFound'
 import SaveSecretsToolbar from '@/components/secrects/SaveToolbar'
+import { Badge } from '@/components/ui/badge'
 import { useSelectedEnvironmentStore } from '@/stores/selectedEnv'
+import { EnvironmentType } from '@/types/environments'
 import clsx from 'clsx'
 import Link from 'next/link'
 import { useMount, useUnmount, useWindowScroll } from 'react-use'
@@ -19,15 +23,58 @@ export default function EnvLayout({
   const { y } = useWindowScroll()
   const selectedEnvironment = useSelectedEnvironmentStore()
 
-  useMount(() => {
-    selectedEnvironment.set({
+  useMount(() => {})
+
+  useUnmount(() => selectedEnvironment.reset())
+
+  const { isLoading, error } = useGetEnvironment(
+    {
       workspaceId: params?.workspace,
       projectName: params?.projectName,
       envName: params?.env,
-    })
-  })
+    },
+    {
+      onSuccess: (data) => {
+        selectedEnvironment.set({
+          workspaceId: params?.workspace,
+          projectName: params?.projectName,
+          //
+          name: params?.env,
+          type: data?.type,
+          locked: data?.locked,
+          createdAt: data?.createdAt,
+        })
+      },
+    }
+  )
 
-  useUnmount(() => selectedEnvironment.reset())
+  if (isLoading) {
+    return <>Loading</>
+  }
+
+  if (error) {
+    if (error?.message === 'Project not found') {
+      return (
+        <NotFound
+          link={`/workspace/${params.workspace}/projects`}
+          title="Project not found"
+          description="Looks like this project doesn't exist"
+          btnText="Go to projects"
+        />
+      )
+    } else if (error?.message === 'Environment not found') {
+      return (
+        <NotFound
+          link={`/workspace/${params.workspace}/projects/${params.projectName}`}
+          title="Environent not found"
+          description="Looks like this environment doesn't exist"
+          btnText="Go to project"
+        />
+      )
+    } else {
+      return 'error'
+    }
+  }
 
   return (
     <>
@@ -52,7 +99,31 @@ export default function EnvLayout({
           <div className="dark:text-gray-400">
             <Icons.chevronRight className="mt-1" />
           </div>
+
           <div className="font-semibold text-2xl ">{params?.env}</div>
+
+          <div className="flex gap-3 items-center">
+            <div>{selectedEnvironment?.data?.locked && <Icons.lock className="h-4 w-4" />}</div>
+
+            <Badge
+              variant="default"
+              className={clsx(['text-[0.725rem] text-gray-200'], {
+                'bg-indigo-600 dark:bg-indigo-800/80 hover:bg-indigo-600 dark:hover:bg-indigo-800/80':
+                  selectedEnvironment?.data?.type === EnvironmentType.DEVELOPMENT,
+                'bg-blue-600 dark:bg-blue-800/80 hover:bg-blue-600 dark:hover:bg-blue-800/80':
+                  selectedEnvironment?.data?.type === EnvironmentType.TESTING,
+                'bg-green-600 dark:bg-green-800/80 hover:bg-green-600 dark:hover:bg-green-800/80':
+                  selectedEnvironment?.data?.type === EnvironmentType.STAGING,
+                'bg-red-600 dark:bg-red-800/80 hover:bg-red-600 dark:hover:bg-red-800/80':
+                  selectedEnvironment?.data?.type === EnvironmentType.PRODUCTION,
+              })}
+            >
+              {selectedEnvironment?.data?.type === EnvironmentType.DEVELOPMENT && 'Development'}
+              {selectedEnvironment?.data?.type === EnvironmentType.TESTING && 'Testing'}
+              {selectedEnvironment?.data?.type === EnvironmentType.STAGING && 'Staging'}
+              {selectedEnvironment?.data?.type === EnvironmentType.PRODUCTION && 'Production'}
+            </Badge>
+          </div>
         </div>
         <div>
           <SaveSecretsToolbar />
