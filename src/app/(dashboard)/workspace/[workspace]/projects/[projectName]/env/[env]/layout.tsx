a478ed5551c19390b8d68e73208f02a54cfd5247
dimak <rahbussiness@gmail.com>
2023-09-28 20:31:35 +0200

    responsive header

--- src/app/(dashboard)/workspace/[workspace]/projects/[projectName]/env/[env]/layout.tsx
+++ src/app/(dashboard)/workspace/[workspace]/projects/[projectName]/env/[env]/layout.tsx
@@ -87,29 +87,32 @@ export default function EnvLayout({
       <div
         className={clsx(
           [
-            'flex md:flex-row flex-col justify-between md:items-center gap-2 md:gap-0 -mt-2 sticky top-0 bg-background pb-2 pt-3 w-full z-10',
+            'flex md:flex-row flex-col justify-between md:items-center gap-0 md:gap-0 -mt-2 sticky top-0 bg-background pb-2 pt-3 w-full z-10',
           ],
           {
             'border-b-2': y > 120,
           }
         )}
       >
-        <div className="hidden md:flex gap-2 items-center">
-          <Link
-            href={`/workspace/${params?.workspace}/projects/${params?.projectName}`}
-            className="text-primary hover:text-primary hover:underline underline-offset-[6px] hover:decoration-2"
-          >
-            <div className="font-semibold text-2xl">{params?.projectName}</div>
-          </Link>
-          {/* // */}
-          <div className="dark:text-gray-400">
-            <Icons.chevronRight className="mt-1" />
+        <div className="flex md:flex-row flex-col gap-2 md:items-center">
+          <div className="flex gap-2 items-center">
+            <Link
+              href={`/workspace/${params?.workspace}/projects/${params?.projectName}`}
+              className="text-primary hover:text-primary hover:underline underline-offset-[6px] hover:decoration-2"
+            >
+              <div className="font-semibold text-2xl">{params?.projectName}</div>
+            </Link>
+            {/* // */}
+            <div className="dark:text-gray-400">
+              <Icons.chevronRight className="mt-1" />
+            </div>
+            <div className="font-semibold text-2xl ">{params?.env}</div>
           </div>
-
-          <div className="font-semibold text-2xl ">{params?.env}</div>
 
           <div className="flex gap-3 items-center">
-            <div>{selectedEnvironment?.data?.locked && <Icons.lock className="h-4 w-4" />}</div>
+            <div className="hidden md:block">
+              {selectedEnvironment?.data?.locked && <Icons.lock className="h-4 w-4" />}
+            </div>
 
             <Badge
               variant="default"
@@ -129,45 +132,10 @@ export default function EnvLayout({
               {selectedEnvironment?.data?.type === EnvironmentType.STAGING && 'Staging'}
               {selectedEnvironment?.data?.type === EnvironmentType.PRODUCTION && 'Production'}
             </Badge>
-          </div>
-        </div>
 
-        <div className="flex flex-col gap-2 items-start md:hidden">
-          <Link
-            href={`/workspace/${params?.workspace}/projects/${params?.projectName}`}
-            className="text-primary hover:text-primary hover:underline underline-offset-[6px] hover:decoration-2"
-          >
-            <div className="font-semibold text-2xl">{params?.projectName}</div>
-          </Link>
-          {/* // */}
-
-          <div className="font-semibold text-xl ">{params?.env}</div>
-
-          <div
-            className={clsx(['flex items-center'], {
-              'gap-3': selectedEnvironment?.data?.locked,
-            })}
-          >
-            <div>{selectedEnvironment?.data?.locked && <Icons.lock className="h-4 w-4" />}</div>
-
-            <Badge
-              variant="default"
-              className={clsx(['text-[0.725rem] text-gray-200'], {
-                'bg-indigo-600 dark:bg-indigo-800/80 hover:bg-indigo-600 dark:hover:bg-indigo-800/80':
-                  selectedEnvironment?.data?.type === EnvironmentType.DEVELOPMENT,
-                'bg-blue-600 dark:bg-blue-800/80 hover:bg-blue-600 dark:hover:bg-blue-800/80':
-                  selectedEnvironment?.data?.type === EnvironmentType.TESTING,
-                'bg-green-600 dark:bg-green-800/80 hover:bg-green-600 dark:hover:bg-green-800/80':
-                  selectedEnvironment?.data?.type === EnvironmentType.STAGING,
-                'bg-red-600 dark:bg-red-800/80 hover:bg-red-600 dark:hover:bg-red-800/80':
-                  selectedEnvironment?.data?.type === EnvironmentType.PRODUCTION,
-              })}
-            >
-              {selectedEnvironment?.data?.type === EnvironmentType.DEVELOPMENT && 'Development'}
-              {selectedEnvironment?.data?.type === EnvironmentType.TESTING && 'Testing'}
-              {selectedEnvironment?.data?.type === EnvironmentType.STAGING && 'Staging'}
-              {selectedEnvironment?.data?.type === EnvironmentType.PRODUCTION && 'Production'}
-            </Badge>
+            <div className="block md:hidden">
+              {selectedEnvironment?.data?.locked && <Icons.lock className="h-4 w-4" />}
+            </div>
           </div>
         </div>
 
