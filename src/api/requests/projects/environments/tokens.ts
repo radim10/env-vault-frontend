cf49986e46a1e8569dde99ae274bab2b89389425
dimak <rahbussiness@gmail.com>
2023-09-29 21:03:12 +0200

    env token requests

---
+++ src/api/requests/projects/environments/tokens.ts
@@ -0,0 +1,49 @@
+import sendRequest, { APIError } from '@/api/instance'
+import { RenameEnvironmentError } from './environments'
+import { EnvironmentToken } from '@/types/environmentTokens'
+
+export type CreateEnvironmentTokenError = RenameEnvironmentError
+export type CreateEnvironmentTokenResData = { token: string }
+
+export interface CreateEnvironmentTokenArgs {
+  workspaceId: string
+  projectName: string
+  envName: string
+  data: {
+    name: string
+    expiration?: {
+      hours?: number
+      days?: number
+    }
+  }
+}
+export async function createEnvironmentToken(args: CreateEnvironmentTokenArgs) {
+  const { workspaceId, projectName, envName, data } = args
+
+  const response = sendRequest<CreateEnvironmentTokenResData>({
+    method: 'POST',
+    basePath: 'workspaces',
+    path: `${workspaceId}/projects/${projectName}/environments/${envName}/tokens`,
+    body: data,
+  })
+  return await response
+}
+
+export type GetEnvironmentTokensError = RenameEnvironmentError
+export type GetEnvironmentTokensData = EnvironmentToken[]
+
+export type GetEnvironmentTokensArgs = Pick<
+  CreateEnvironmentTokenArgs,
+  'workspaceId' | 'projectName' | 'envName'
+>
+
+export async function getEnvironmentTokens(args: GetEnvironmentTokensArgs) {
+  const { workspaceId, projectName, envName } = args
+
+  const response = sendRequest<GetEnvironmentTokensData>({
+    method: 'GET',
+    basePath: 'workspaces',
+    path: `${workspaceId}/projects/${projectName}/environments/${envName}/tokens`,
+  })
+  return await response
+}
