c5549a0c0a05483888ce52a4e468030c73edee82
dimak <rahbussiness@gmail.com>
2023-09-29 12:35:18 +0200

    projects store - loaded, search

--- src/stores/projects.ts
+++ src/stores/projects.ts
@@ -1,21 +1,51 @@
 import { ProjectSort } from '@/types/projects'
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
+import { immer } from 'zustand/middleware/immer'
 
-export interface ProjectsSortState {
+export interface ProjectsState {
   sort: ProjectSort
-  setSort: (sort: ProjectSort) => void
+  search: string
+  loaded: boolean
 }
 
-export const useProjectsSortStore = create(
+interface ProjectsActions {
+  setLoaded: (loaded: boolean) => void
+  setSort: (sort: ProjectSort) => void
+  setSearch: (search: string) => void
+}
+
+export const useProjectsStore = create(
   devtools(
-    persist<ProjectsSortState>(
-      (set, _) => ({
+    persist(
+      immer<ProjectsState & ProjectsActions>((set, _) => ({
         sort: ProjectSort.CreatedDesc,
-        setSort: (sort) => set({ sort }),
-      }),
+        loaded: false,
+        search: '',
+        setSort: (sort) => {
+          set((state) => {
+            state.sort = sort
+          })
+        },
+        setSearch: (search) => {
+          set((state) => {
+            state.search = search
+          })
+        },
+        setLoaded: (loaded) => {
+          set((state) => {
+            state.loaded = loaded
+          })
+        },
+      })),
       {
-        name: 'projectsSort',
+        name: 'projects',
+        partialize: (state) =>
+          Object.fromEntries(
+            Object.entries(state).filter(
+              ([key]) => !['search'].includes(key) && !['loaded'].includes(key)
+            )
+          ),
       }
     )
   )
