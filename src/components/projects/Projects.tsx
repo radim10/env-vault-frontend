a5648d4cfd4af68ef906ed2ad5ea8756e3cd9373
dimak <rahbussiness@gmail.com>
2023-09-29 11:11:00 +0200

    project - responsive toolbar

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,47 +1,33 @@
+'use client'
+
 import React from 'react'
-import { Icons } from '@/components/icons'
 import TypographyH2 from '@/components/typography/TypographyH2'
-import { Button } from '@/components/ui/button'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
 import ProjectList from '@/components/projects/ProjectList'
 import CreateProject from '@/components/projects/CreateProjectDialog'
+import ProjectsSortSelect from './ProjectsSortSelect'
+import { useProjectsSortStore } from '@/stores/projects'
 
 interface Props {
   workspaceId: string
 }
 
 const Projects: React.FC<Props> = ({ workspaceId }) => {
+  const { sort, setSort } = useProjectsSortStore()
   return (
     <div>
       <div>
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 md:gap-0">
           <div className="flex items-center gap-4">
             <TypographyH2>Projects</TypographyH2>
-
             <CreateProject workspaceId={workspaceId} />
           </div>
-
-          <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Sort by" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="Updated">Updated</SelectItem>
-              <SelectItem value="Created">Created</SelectItem>
-              <SelectItem value="Name">Name</SelectItem>
-              <SelectItem value="Folders">No of folders</SelectItem>
-            </SelectContent>
-          </Select>
+          <div className="flex items-end w-full justify-end">
+            <ProjectsSortSelect sort={sort} setSort={setSort} />
+          </div>
         </div>
 
-        <div className="mt-16">
-          <ProjectList workspace={workspaceId} />
+        <div className="md:mt-16 mt-8">
+          <ProjectList workspace={workspaceId} sort={sort} />
         </div>
       </div>
     </div>
