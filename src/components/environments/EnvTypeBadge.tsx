5c60784096d4ebf2c1d69172203f08015ee3a09e
dimak <rahbussiness@gmail.com>
2023-09-29 13:45:23 +0200

    badge text color

--- src/components/environments/EnvTypeBadge.tsx
+++ src/components/environments/EnvTypeBadge.tsx
@@ -14,7 +14,7 @@ const EnvTypeBadge: React.FC<Props> = ({ className, type }) => {
     <Badge
       variant="default"
       className={cn(
-        clsx(['text-[0.725rem] text-gray-200'], {
+        clsx(['text-[0.725rem] text-white dark:text-gray-200'], {
           'bg-indigo-600 dark:bg-indigo-800/80 hover:bg-indigo-600 dark:hover:bg-indigo-800/80':
             type === EnvironmentType.DEVELOPMENT,
           'bg-blue-600 dark:bg-blue-800/80 hover:bg-blue-600 dark:hover:bg-blue-800/80':
