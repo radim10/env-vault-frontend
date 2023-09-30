25e8751932be7e32578032bbec3a4982e0f577b7
dimak <rahbussiness@gmail.com>
2023-09-30 14:47:32 +0200

    merge prop clasName

--- src/components/typography/TypographyH4.tsx
+++ src/components/typography/TypographyH4.tsx
@@ -1,9 +1,18 @@
+import { cn } from '@/lib/utils'
+
 interface Props {
   children: React.ReactNode
+  className?: string
 }
-function TypographyH4({ children }: Props) {
+
+function TypographyH4({ children, className }: Props) {
   return (
-    <h2 className="dark:text-gray-300 scroll-m-20 text-xl font-semibold tracking-tight transition-colors first:mt-0">
+    <h2
+      className={cn(
+        'dark:text-gray-300 scroll-m-20 text-xl font-semibold tracking-tight transition-colors first:mt-0',
+        className
+      )}
+    >
       {children}
     </h2>
   )
