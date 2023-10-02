17b04779f393e8ea65d8ce6fdfcc5a0690bb535b
dimak <rahbussiness@gmail.com>
2023-10-02 12:54:30 +0200

    create workspace token request

--- src/api/requests/tokens.ts
+++ src/api/requests/tokens.ts
@@ -1,4 +1,4 @@
-import { EnvironmentToken, ReadOnlyEnvToken } from '@/types/environmentTokens'
+import { EnvTokenGrant, ReadOnlyEnvToken } from '@/types/environmentTokens'
 import sendRequest, { APIError } from '../instance'
 
 export type GetEnvTokensError = APIError<'Workspace not found'>
@@ -11,6 +11,35 @@ export async function getEnvTokens(args: { workspaceId: string }) {
     method: 'GET',
     basePath: 'workspaces',
     path: `${workspaceId}/tokens/environments`,
+  })
+  return await response
+}
+
+// NOTE: workspaces
+// create
+export type CreateWorkspaceTokenError = APIError<'Workspace not found'>
+export type CreateWorkspaceTokenResData = { id: string; token: string }
+
+export interface CreateWorkspaceTokenArgs {
+  workspaceId: string
+  data: {
+    name: string
+    grant: EnvTokenGrant
+    expiration?: {
+      hours?: number
+      days?: number
+    }
+  }
+}
+
+export async function createWorkspaceToken(args: CreateWorkspaceTokenArgs) {
+  const { workspaceId } = args
+
+  const response = sendRequest<CreateWorkspaceTokenResData>({
+    method: 'POST',
+    basePath: 'workspaces',
+    path: `${workspaceId}/tokens/workspace`,
+    body: args?.data,
   })
   return await response
 }
