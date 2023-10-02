920571da3a6161ca332a3b1f9c237ad9ef751066
dimak <rahbussiness@gmail.com>
2023-10-02 13:42:54 +0200

    page redirect + metadata

--- src/app/(dashboard)/workspace/[workspace]/tokens/[type]/page.tsx
+++ src/app/(dashboard)/workspace/[workspace]/tokens/[type]/page.tsx
@@ -1,13 +1,28 @@
 import AccessTable from '@/components/environments/access/AccessTable'
 import EnvTokens from '@/components/tokens/EnvTokens'
 import WorkspaceTokens from '@/components/tokens/WorkspaceTokens'
-import { Metadata } from 'next'
+import { Metadata, ResolvingMetadata } from 'next'
+import { redirect } from 'next/navigation'
 
-export const metadata: Metadata = {
-  title: 'Tokens',
+export function generateMetadata({ params }: any, parent: ResolvingMetadata): Metadata {
+  // read route params
+  const type = params.type
+
+  return {
+    title: `Tokens / ${type}`,
+  }
 }
 
 export default function TokensPage({ params }: { params: { workspace: string; type: string } }) {
+  if (
+    params.type !== 'cli' &&
+    params.type !== 'environments' &&
+    params.type !== 'workspace' &&
+    params.type !== 'admin'
+  ) {
+    redirect(`/workspace/${params.workspace}/tokens/cli`)
+  }
+
   return (
     <>
       {params?.type === 'environments' && <EnvTokens workspaceId={params?.workspace} />}
