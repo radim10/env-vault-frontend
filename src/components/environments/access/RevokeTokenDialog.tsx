e2c59adb597e74d27756c4583aecc2fb35c0bdd9
dimak <rahbussiness@gmail.com>
2023-10-02 13:41:29 +0200

    revoke token dialog - workspace token

--- src/components/environments/access/RevokeTokenDialog.tsx
+++ src/components/environments/access/RevokeTokenDialog.tsx
@@ -3,15 +3,19 @@ import { useRevokeEnvironmentToken } from '@/api/mutations/envTokens'
 import { Icons } from '@/components/icons'
 import DeleteDialog from '@/components/DeleteDialog'
 import { Input } from '@/components/ui/input'
+import { useRevokeWorkspaceToken } from '@/api/mutations/tokens'
+import { APIError } from '@/api/instance'
+import { useUpdateEffect } from 'react-use'
 
 interface Props {
   opened: boolean
 
   workspaceId: string
-  projectName: string
-  envName: string
+  projectName?: string
+  envName?: string
   tokenName: string
   tokenId: string
+  description?: string
 
   onClose: () => void
   onSuccess: () => void
@@ -23,30 +27,66 @@ const RevokeTokenDialog: React.FC<Props> = ({
   envName,
   tokenId,
   tokenName,
+  description,
   opened,
   onClose,
   onSuccess,
 }) => {
   const [confirmText, setConfirmText] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<APIError<any> | null>(null)
 
   const {
     mutate: revokeToken,
     isLoading,
-    error,
+    error: err,
   } = useRevokeEnvironmentToken({
     onSuccess: () => onSuccess(),
   })
+
+  const {
+    mutate: revokeWorkspaceToken,
+    isLoading: isLoading2,
+    error: err2,
+  } = useRevokeWorkspaceToken({
+    onSuccess: () => onSuccess(),
+  })
+
+  useUpdateEffect(() => {
+    if (isLoading || isLoading2) {
+      if (!loading) setLoading(true)
+    } else if (!isLoading && !isLoading2 && loading) {
+      setLoading(false)
+    }
+  }, [isLoading, isLoading2])
+
+  useUpdateEffect(() => {
+    if (err) setError(err)
+    if (err2) setError(err2)
+
+    if (error && !err && !err2) setError(null)
+  }, [isLoading, isLoading2])
 
   return (
     <div>
       <DeleteDialog
         opened={opened}
         onClose={onClose}
-        inProgress={isLoading}
+        inProgress={loading}
         disabledConfirm={confirmText !== tokenName}
         title="Revoke this token?"
-        description="If you revoke this token, you will no longer have access to this environment using this token in your applications."
-        onConfirm={() => revokeToken({ workspaceId, projectName, envName, tokenId })}
+        description={
+          description ??
+          'If you revoke this token, you will no longer have access to this environment using this token in your applications.'
+        }
+        onConfirm={() => {
+          if (projectName && envName) {
+            revokeToken({ workspaceId, projectName, envName, tokenId })
+          } else {
+            // revoke workspace token
+            revokeWorkspaceToken({ workspaceId, tokenId })
+          }
+        }}
       >
         <div className="flex flex-col gap-4 p0-4 pb-4">
           <div className="flex flex-col gap-1 mt-3">
@@ -57,7 +97,7 @@ const RevokeTokenDialog: React.FC<Props> = ({
 
             <Input
               placeholder={tokenName}
-              disabled={isLoading}
+              disabled={loading}
               onChange={(e) => setConfirmText(e.target.value)}
             />
           </div>
