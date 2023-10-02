6fdbaab1bacca7811edf5f8de1ba43d69cc40e71
dimak <rahbussiness@gmail.com>
2023-10-02 12:28:46 +0200

    show workspace tokens component

--- src/app/(dashboard)/workspace/[workspace]/tokens/[type]/page.tsx
+++ src/app/(dashboard)/workspace/[workspace]/tokens/[type]/page.tsx
@@ -1,5 +1,6 @@
 import AccessTable from '@/components/environments/access/AccessTable'
 import EnvTokens from '@/components/tokens/EnvTokens'
+import WorkspaceTokens from '@/components/tokens/WorkspaceTokens'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -7,5 +8,10 @@ export const metadata: Metadata = {
 }
 
 export default function TokensPage({ params }: { params: { workspace: string; type: string } }) {
-  return <>{params?.type === 'environments' && <EnvTokens workspaceId={params?.workspace} />}</>
+  return (
+    <>
+      {params?.type === 'environments' && <EnvTokens workspaceId={params?.workspace} />}
+      {params?.type === 'workspace' && <WorkspaceTokens workspaceId={params?.workspace} />}
+    </>
+  )
 }
