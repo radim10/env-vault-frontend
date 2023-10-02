f1ea49e0380f47ba82c3766247116fae74c9291e
dimak <rahbussiness@gmail.com>
2023-10-02 13:42:35 +0200

    get + create workspace tokens queries

--- src/api/requests/tokens.ts
+++ src/api/requests/tokens.ts
@@ -1,5 +1,6 @@
 import { EnvTokenGrant, ReadOnlyEnvToken } from '@/types/environmentTokens'
 import sendRequest, { APIError } from '../instance'
+import { WorkspaceToken } from '@/types/workspaceTokens'
 
 export type GetEnvTokensError = APIError<'Workspace not found'>
 export type GetEnvTokensData = Awaited<ReturnType<typeof getEnvTokens>>
@@ -16,6 +17,21 @@ export async function getEnvTokens(args: { workspaceId: string }) {
 }
 
 // NOTE: workspaces
+// get
+export type GetWorkspaceTokensError = APIError<'Workspace not found'>
+export type GetWorkspaceTokensData = Array<WorkspaceToken>
+
+export async function getWorkspaceTokens(args: { workspaceId: string }) {
+  const { workspaceId } = args
+
+  const response = sendRequest<GetWorkspaceTokensData>({
+    method: 'GET',
+    basePath: 'workspaces',
+    path: `${workspaceId}/tokens/workspace`,
+  })
+  return await response
+}
+
 // create
 export type CreateWorkspaceTokenError = APIError<'Workspace not found'>
 export type CreateWorkspaceTokenResData = { id: string; token: string }
@@ -40,6 +56,28 @@ export async function createWorkspaceToken(args: CreateWorkspaceTokenArgs) {
     basePath: 'workspaces',
     path: `${workspaceId}/tokens/workspace`,
     body: args?.data,
+  })
+  return await response
+}
+
+// revoke (delete)
+
+// revoke (delete)
+export type RevokeWorkspaceTokenError = APIError<'Workspace not found' | 'Token not found'>
+export type RevokeWorkspaceTokenResData = undefined
+
+export type RevokeWorkspaceTokenArgs = {
+  workspaceId: string
+  tokenId: string
+}
+
+export async function revokeWorkspaceToken(args: RevokeWorkspaceTokenArgs) {
+  const { workspaceId, tokenId } = args
+
+  const response = sendRequest<RevokeWorkspaceTokenResData>({
+    method: 'DELETE',
+    basePath: 'workspaces',
+    path: `${workspaceId}/tokens/workspace/${tokenId}`,
   })
   return await response
 }
