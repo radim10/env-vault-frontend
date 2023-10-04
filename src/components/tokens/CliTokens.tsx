3a3a1cc6fff5d8e55cd7cea3e9829e71ecc7b84b
dimak <rahbussiness@gmail.com>
2023-10-04 21:29:55 +0200

    CliTokens component

---
+++ src/components/tokens/CliTokens.tsx
@@ -0,0 +1,134 @@
+'use client'
+
+import { useGetCliTokens } from '@/api/queries/projects/tokens'
+import { Skeleton } from '../ui/skeleton'
+import Error from '@/components/Error'
+import TypographyH4 from '../typography/TypographyH4'
+import { Button } from '../ui/button'
+import { useState } from 'react'
+import { Icons } from '../icons'
+import CliTokensTable from './CliTokensTable'
+import RevokeCliTokenDialog from './RevokeCliTokenDialog'
+import { CliToken } from '@/types/tokens/cli'
+import { useToast } from '../ui/use-toast'
+import { useQueryClient } from '@tanstack/react-query'
+
+interface Props {
+  workspaceId: string
+}
+
+const CliTokens: React.FC<Props> = ({ workspaceId }) => {
+  const { toast } = useToast()
+  const queryClient = useQueryClient()
+
+  const [dialogOpened, setDialogOpened] = useState(false)
+  const [revokeDialog, setRevokeDialog] = useState<{ id: string } | null>(null)
+
+  const { data, isLoading, error } = useGetCliTokens({
+    workspaceId,
+  })
+
+  const getCacheData = (): CliToken[] | undefined => {
+    return queryClient.getQueryData<CliToken[]>([workspaceId, 'cli-tokens'])
+  }
+
+  const handleRevokedToken = (tokenId: string) => {
+    closeRevokeDialog()
+
+    const data = getCacheData()
+
+    if (data) {
+      const newData = [...data].filter(({ id }) => id !== tokenId)
+
+      queryClient.setQueryData<CliToken[]>([workspaceId, 'cli-tokens'], newData)
+    }
+
+    toast({
+      title: 'Token has been revoked!',
+      variant: 'success',
+    })
+  }
+
+  const copyToken = (token: string) => {
+    navigator.clipboard.writeText(token)
+    toast({
+      title: 'Token copied to clipboard!',
+      variant: 'success',
+    })
+  }
+
+  const closeRevokeDialog = () => {
+    if (!revokeDialog) return
+
+    setRevokeDialog({ ...revokeDialog, id: '' })
+    setTimeout(() => {
+      setRevokeDialog(null)
+    }, 150)
+  }
+
+  if (isLoading) {
+    return <Skeleton className="mt-2 border-2 h-72 w-full" />
+  }
+
+  if (error) {
+    return <Error />
+  }
+
+  return (
+    <>
+      {revokeDialog && (
+        <RevokeCliTokenDialog
+          workspaceId={workspaceId}
+          tokenId={revokeDialog?.id}
+          opened={revokeDialog?.id?.length > 0}
+          onClose={closeRevokeDialog}
+          onSuccess={() => handleRevokedToken(revokeDialog.id)}
+        />
+      )}
+      <div>
+        <div className="mt-2 gap-2 rounded-md border-2">
+          <div className="px-3 py-3 md:px-5 md:py-4">
+            <div className="flex items-center justify-between">
+              <div className="gap-3 flex items-center">
+                <TypographyH4>Cli tokens</TypographyH4>
+                <Icons.terminalSquare className="h-5 w-5 opacity-80" />
+              </div>
+
+              <Button
+                variant={'outline'}
+                size={'sm'}
+                className="gap-2 hidden md:flex"
+                onClick={() => setDialogOpened(true)}
+              >
+                <Icons.plus className="h-4 w-4" />
+                Add new
+              </Button>
+            </div>
+            {/* // */}
+            <div className="text-[0.95rem] text-muted-foreground mt-2">
+              Cli tokens are used with cli to access selected worksapace on the behalf of the user
+            </div>
+
+            <Button
+              size={'sm'}
+              variant={'outline'}
+              className="mt-3.5 mb-2 gap-2 md:hidden flex w-full"
+              onClick={() => setDialogOpened(true)}
+            >
+              <Icons.plus className="h-4 w-4" />
+              Add new
+            </Button>
+          </div>
+          {/* // TABLE */}
+          <CliTokensTable
+            data={data}
+            onRevoke={(id) => setRevokeDialog({ id })}
+            onCopyToken={copyToken}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CliTokens
