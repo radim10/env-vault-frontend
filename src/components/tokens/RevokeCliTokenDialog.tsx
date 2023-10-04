eb488b854a63b1688265bff1171c42037eadf2dd
dimak <rahbussiness@gmail.com>
2023-10-04 21:30:20 +0200

    revoke cli token dialog component

---
+++ src/components/tokens/RevokeCliTokenDialog.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Icons } from '@/components/icons'
+import DeleteDialog from '@/components/DeleteDialog'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import { useRevokeCliToken } from '@/api/mutations/tokens/cli'
+
+interface Props {
+  opened: boolean
+
+  workspaceId: string
+  tokenId: string
+
+  onClose: () => void
+  onSuccess: () => void
+}
+
+const RevokeCliTokenDialog: React.FC<Props> = ({
+  workspaceId,
+  tokenId,
+  opened,
+  onClose,
+  onSuccess,
+}) => {
+  const {
+    mutate: revokeToken,
+    isLoading,
+    error,
+  } = useRevokeCliToken({
+    onSuccess: () => onSuccess(),
+  })
+
+  return (
+    <div>
+      <Dialog
+        open={opened}
+        onOpenChange={(e) => {
+          if (!e) {
+            close()
+          }
+        }}
+      >
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Revoke this cli token?</DialogTitle>
+            <DialogDescription>
+              If you revoke this token, you will no longer have access to this workspace using this
+              token with the cli.
+            </DialogDescription>
+
+            {error?.message && (
+              <div className="flex flex-col gap-4 mt-3 pb-1">
+                <div className="text-red-600 text-[0.92rem] flex items-center gap-2 -mt-1">
+                  <Icons.xCircle className="h-4 w-4" />
+                  {error?.message}
+                </div>
+              </div>
+            )}
+          </DialogHeader>
+          <DialogFooter className="mt-4">
+            <Button
+              type="submit"
+              variant="destructive"
+              className="w-full gap-2"
+              loading={isLoading}
+              disabled={isLoading}
+              onClick={() => {
+                revokeToken({ workspaceId, tokenId })
+              }}
+            >
+              <Icons.trash className="h-4 w-4" />
+              <span>Revoke</span>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
+
+export default RevokeCliTokenDialog
