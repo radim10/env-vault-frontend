1f673f79d9ad834c5cf8fd00ff5ed2aa24aff360
dimak <rahbussiness@gmail.com>
2023-10-05 13:34:17 +0200

    create cli token mutation

--- src/api/mutations/tokens/cli.ts
+++ src/api/mutations/tokens/cli.ts
@@ -1,11 +1,24 @@
 import { MutOpt } from '../mutOpt'
 import { useMutation } from '@tanstack/react-query'
 import {
+  CreateCliTokenArgs,
+  CreateCliTokenError,
+  CreateCliTokenResData,
   RevokeCliTokenArgs,
   RevokeCliTokenError,
   RevokeCliTokenResData,
+  createCliToken,
   revokeCliToken,
 } from '@/api/requests/tokens/cli'
+
+// create
+type CreateCliTokenVariables = CreateCliTokenArgs
+
+export const useCreateCliToken = (opt?: MutOpt<CreateCliTokenResData>) =>
+  useMutation<CreateCliTokenResData, CreateCliTokenError, CreateCliTokenVariables>(
+    createCliToken,
+    opt
+  )
 
 // revoke (delete)
 type RevokeCliTokenVariables = RevokeCliTokenArgs
