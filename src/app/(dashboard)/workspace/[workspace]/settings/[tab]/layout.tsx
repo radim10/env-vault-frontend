ef7df9ba76c77250cfd650a61fe948ee03e8b91f
dimak <rahbussiness@gmail.com>
2023-10-12 20:01:42 +0200

    settings layout

---
+++ src/app/(dashboard)/workspace/[workspace]/settings/[tab]/layout.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import SettingsTabs from '@/components/settings/Tabs'
+import TypographyH2 from '@/components/typography/TypographyH2'
+import clsx from 'clsx'
+import React from 'react'
+import { useWindowScroll } from 'react-use'
+
+export default function SettingsLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: { workspace: string }
+}) {
+  const { y } = useWindowScroll()
+
+  return (
+    <>
+      <div
+        className={clsx(
+          [
+            'px-6 lg:px-10  backdrop-blur-xl flex md:flex-row flex-col justify-between md:items-center gap-0 md:gap-0 -mt-2 sticky top-0 bg-transparent pb-2 pt-3 w-full z-10',
+          ],
+          {
+            'border-b-2': y > 104,
+          }
+        )}
+      >
+        <div className="flex md:flex-row flex-col gap-2 md:items-center ">
+          <TypographyH2>Settings</TypographyH2>
+        </div>
+        {/* // */}
+      </div>
+      <div className="mt-5">
+        <SettingsTabs workspaceId={params?.workspace} />
+
+        <div className="mt-4 px-6 lg:px-10">{children}</div>
+      </div>
+    </>
+  )
+}
