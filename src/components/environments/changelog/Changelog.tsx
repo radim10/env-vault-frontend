813e4183211bfdf080ccbe8185d7550b5cbfd7e2
dimak <rahbussiness@gmail.com>
2023-10-13 14:41:13 +0200

    map

--- src/components/environments/changelog/Changelog.tsx
+++ src/components/environments/changelog/Changelog.tsx
@@ -1,4 +1,11 @@
+import dayjs from 'dayjs'
 import React from 'react'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import TypographyH4 from '@/components/typography/TypographyH4'
+import ChangelogItem from './ChangelogItem'
+
+dayjs.extend(relativeTime)
 
 interface Props {
   workspaceId: string
@@ -7,7 +14,19 @@ interface Props {
 }
 
 const Changelog: React.FC<Props> = ({ workspaceId, projectName, envName }) => {
-  return <div>This is changelog list</div>
+  return (
+    <>
+      <div className="mb-4">
+        <TypographyH4>Today</TypographyH4>
+      </div>
+
+      <div className="flex flex-col gap-6 md:gap-7">
+        {Array.from({ length: 2 })?.map((val) => (
+          <ChangelogItem />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Changelog
