5503a3f7a5200e824f461a863ef1be6737aca0ad
dimak <rahbussiness@gmail.com>
2023-10-13 14:08:23 +0200

    metadata + changelog component

--- src/app/(dashboard)/workspace/[workspace]/projects/[projectName]/env/[env]/[tab]/page.tsx
+++ src/app/(dashboard)/workspace/[workspace]/projects/[projectName]/env/[env]/[tab]/page.tsx
@@ -1,6 +1,21 @@
 import { EnvSettings } from '@/components/environments/Settings/EnvSettings'
 import Access from '@/components/environments/access/Access'
+import Changelog from '@/components/environments/changelog/ChangelogList'
+import { Metadata, ResolvingMetadata } from 'next'
 import { redirect } from 'next/navigation'
+
+export function generateMetadata(
+  { params, searchParams }: any,
+  parent: ResolvingMetadata
+): Metadata {
+  // read route params
+  const { projectName, env, tab } = params
+
+  return {
+    title: `${projectName} / ${env} / ${tab}`,
+    // title: `${projectName} / ${env}`,
+  }
+}
 
 export default function EnvTabePage({
   params,
@@ -26,6 +41,13 @@ export default function EnvTabePage({
         />
       )}
       {params.tab === 'settings' && <EnvSettings />}
+      {params.tab === 'changelog' && (
+        <Changelog
+          workspaceId={params.workspace}
+          projectName={params.projectName}
+          envName={params.env}
+        />
+      )}
     </div>
   )
 }
