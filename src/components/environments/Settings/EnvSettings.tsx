09b2f91f29340334229c4d156875d9d68c93def9
dimak <rahbussiness@gmail.com>
2023-10-13 11:10:20 +0200

    use settings list component

--- src/components/environments/Settings/EnvSettings.tsx
+++ src/components/environments/Settings/EnvSettings.tsx
@@ -21,6 +21,7 @@ import { Environment, EnvironmentType } from '@/types/environments'
 import { LucideIcon } from 'lucide-react'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { Skeleton } from '@/components/ui/skeleton'
+import SettingsList from '@/components/SettingsList'
 
 dayjs.extend(relativeTime)
 
@@ -294,84 +295,141 @@ export const EnvSettings = (props: {}) => {
       )}
 
       <div>
-        <div className="mt-2 gap-2 rounded-md border-2">
-          <div className="px-0 py-3 md:px-0 lg:px-0 md:py-4">
-            <div className="flex items-center justify-start gap-3 px-3 md:px-6 lg:px-6">
-              <TypographyH4>General settings</TypographyH4>
-              <Icons.settings2 className="h-5 w-5 opacity-80" />
-            </div>
-            {/* // */}
-            <div className="text-[0.95rem] text-muted-foreground mt-2 md:mt-0 px-3 md:px-6 lg:px-6">
-              Modify/edit this environment
-            </div>
-
-            <div className="mt-7 flex flex-col gap-2.5 text-[0.97rem] ox-0">
-              {generalItems.map(({ label, icon: Icon }, index) => (
+        <SettingsList
+          title="General settings"
+          description={'Edit this environment'}
+          icon={Icons.settings2}
+          items={[
+            {
+              icon: Icons.clock4,
+              label: 'Created at',
+              component: (
                 <>
-                  <div className="flex flex-col md:flex-row md:items-center gap-2 text-md md:justify-between px-4 md:px-10 md:h-8">
-                    <div className="flex items-center gap-2 md:gap-3">
-                      <Icon className="h-4 w-4 opacity-80" />
-                      <span className="font-semibold text-[1.00rem]">{label}</span>
-                    </div>
-                    <div>
-                      {label === 'Created at' && (
-                        <span>
-                          {dayjs(selectedEnv?.createdAt).format('YYYY-MM-DD HH:mm')} (
-                          {dayjs(selectedEnv?.createdAt).fromNow()})
-                        </span>
-                      )}
-                      {label === 'Created by' && <span>@dimak00</span>}
-                      {label === 'Name' && (
-                        <div className="flex justify-between md:justify-start items-center gap-3 md:gap-6">
-                          <div className="flex items-center gap-2">{selectedEnv?.name ?? ''}</div>
-                          <Button
-                            size={'sm'}
-                            variant={'outline'}
-                            disabled={selectedEnv?.locked}
-                            onClick={() => setDialog('rename')}
-                          >
-                            <Icons.pencil className="h-3.5 w-3.5" />
-                          </Button>
-                        </div>
-                      )}
-                      {label === 'Type' && (
-                        <div className="flex justify-between md:justify-start items-center gap-3 md:gap-6">
-                          <div className="flex items-center gap-2">
-                            <EnvTypeBadge type={selectedEnv?.type ?? EnvironmentType.STAGING} />
-                          </div>
-                          <Button
-                            size={'sm'}
-                            variant={'outline'}
-                            disabled={selectedEnv?.locked}
-                            onClick={() => setDialog('changeType')}
-                          >
-                            <Icons.pencil className="h-3.5 w-3.5" />
-                          </Button>
-                        </div>
-                      )}
-                      {label === 'Lock status' && (
-                        <div className="flex justify-between md:justify-start items-center gap-3 md:gap-6">
-                          <div className="flex items-center gap-2">
-                            <span>{selectedEnv?.locked ? 'Locked' : 'Unlocked'}</span>
-                            {selectedEnv?.locked ? (
-                              <Icons.lock className="h-4 w-4" />
-                            ) : (
-                              <Icons.unlock className="h-4 w-4" />
-                            )}
-                          </div>
-                          <Button size={'sm'} variant={'outline'} onClick={() => setDialog('lock')}>
-                            <Icons.pencil className="h-3.5 w-3.5" />
-                          </Button>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  {index !== generalItems.length - 1 && <Separator />}
+                  {dayjs(selectedEnv?.createdAt).format('YYYY-MM-DD HH:mm')} (
+                  {dayjs(selectedEnv?.createdAt).fromNow()})
                 </>
-              ))}
-            </div>
-          </div>
-        </div>
+              ),
+            },
+            {
+              icon: Icons.user,
+              label: 'Created by',
+              value: '@dimak00',
+            },
+            {
+              icon: Icons.fileText,
+              label: 'Name',
+              editBtn: {
+                disabled: selectedEnv?.locked,
+                onClick: () => setDialog('rename'),
+              },
+              component: <div className="flex items-center gap-2">{selectedEnv?.name ?? ''}</div>,
+            },
+            {
+              icon: Icons.tag,
+              label: 'Type',
+              editBtn: {
+                disabled: selectedEnv?.locked,
+                onClick: () => setDialog('changeType'),
+              },
+              component: <EnvTypeBadge type={selectedEnv?.type ?? EnvironmentType.STAGING} />,
+            },
+            {
+              icon: Icons.fileLock2,
+              label: 'Lock status',
+              editBtn: {
+                disabled: selectedEnv?.locked,
+                onClick: () => setDialog('lock'),
+              },
+              component: (
+                <div className="flex items-center gap-2">
+                  <span>{selectedEnv?.locked ? 'Locked' : 'Unlocked'}</span>
+                  {selectedEnv?.locked ? (
+                    <Icons.lock className="h-4 w-4" />
+                  ) : (
+                    <Icons.unlock className="h-4 w-4" />
+                  )}
+                </div>
+              ),
+            },
+          ]}
+        />
+        {/* <div className="mt-2 gap-2 rounded-md border-2"> */}
+        {/*   <div className="px-0 py-3 md:px-0 lg:px-0 md:py-4"> */}
+        {/*     <div className="flex items-center justify-start gap-3 px-3 md:px-6 lg:px-6"> */}
+        {/*       <TypographyH4>General settings</TypographyH4> */}
+        {/*       <Icons.settings2 className="h-5 w-5 opacity-80" /> */}
+        {/*     </div> */}
+        {/*     <div className="text-[0.95rem] text-muted-foreground mt-2 md:mt-0 px-3 md:px-6 lg:px-6"> */}
+        {/*       Modify/edit this environment */}
+        {/*     </div> */}
+        {/**/}
+        {/*     <div className="mt-7 flex flex-col gap-2.5 text-[0.97rem] ox-0"> */}
+        {/*       {generalItems.map(({ label, icon: Icon }, index) => ( */}
+        {/*         <> */}
+        {/*           <div className="flex flex-col md:flex-row md:items-center gap-2 text-md md:justify-between px-4 md:px-10 md:h-8"> */}
+        {/*             <div className="flex items-center gap-2 md:gap-3"> */}
+        {/*               <Icon className="h-4 w-4 opacity-80" /> */}
+        {/*               <span className="font-semibold text-[1.00rem]">{label}</span> */}
+        {/*             </div> */}
+        {/*             <div> */}
+        {/*               {label === 'Created at' && ( */}
+        {/*                 <span> */}
+        {/*                   {dayjs(selectedEnv?.createdAt).format('YYYY-MM-DD HH:mm')} ( */}
+        {/*                   {dayjs(selectedEnv?.createdAt).fromNow()}) */}
+        {/*                 </span> */}
+        {/*               )} */}
+        {/*               {label === 'Created by' && <span>@dimak00</span>} */}
+        {/*               {label === 'Name' && ( */}
+        {/*                 <div className="flex justify-between md:justify-start items-center gap-3 md:gap-6"> */}
+        {/*                   <div className="flex items-center gap-2">{selectedEnv?.name ?? ''}</div> */}
+        {/*                   <Button */}
+        {/*                     size={'sm'} */}
+        {/*                     variant={'outline'} */}
+        {/*                     disabled={selectedEnv?.locked} */}
+        {/*                     onClick={() => setDialog('rename')} */}
+        {/*                   > */}
+        {/*                     <Icons.pencil className="h-3.5 w-3.5" /> */}
+        {/*                   </Button> */}
+        {/*                 </div> */}
+        {/*               )} */}
+        {/*               {label === 'Type' && ( */}
+        {/*                 <div className="flex justify-between md:justify-start items-center gap-3 md:gap-6"> */}
+        {/*                   <div className="flex items-center gap-2"> */}
+        {/*                     <EnvTypeBadge type={selectedEnv?.type ?? EnvironmentType.STAGING} /> */}
+        {/*                   </div> */}
+        {/*                   <Button */}
+        {/*                     size={'sm'} */}
+        {/*                     variant={'outline'} */}
+        {/*                     disabled={selectedEnv?.locked} */}
+        {/*                     onClick={() => setDialog('changeType')} */}
+        {/*                   > */}
+        {/*                     <Icons.pencil className="h-3.5 w-3.5" /> */}
+        {/*                   </Button> */}
+        {/*                 </div> */}
+        {/*               )} */}
+        {/*               {label === 'Lock status' && ( */}
+        {/*                 <div className="flex justify-between md:justify-start items-center gap-3 md:gap-6"> */}
+        {/*                   <div className="flex items-center gap-2"> */}
+        {/*                     <span>{selectedEnv?.locked ? 'Locked' : 'Unlocked'}</span> */}
+        {/*                     {selectedEnv?.locked ? ( */}
+        {/*                       <Icons.lock className="h-4 w-4" /> */}
+        {/*                     ) : ( */}
+        {/*                       <Icons.unlock className="h-4 w-4" /> */}
+        {/*                     )} */}
+        {/*                   </div> */}
+        {/*                   <Button size={'sm'} variant={'outline'} onClick={() => setDialog('lock')}> */}
+        {/*                     <Icons.pencil className="h-3.5 w-3.5" /> */}
+        {/*                   </Button> */}
+        {/*                 </div> */}
+        {/*               )} */}
+        {/*             </div> */}
+        {/*           </div> */}
+        {/*           {index !== generalItems.length - 1 && <Separator />} */}
+        {/*         </> */}
+        {/*       ))} */}
+        {/*     </div> */}
+        {/*   </div> */}
+        {/* </div> */}
         {/* // DANGER ZONE */}
         <div className="mt-7 gap-2 rounded-md border-2">
           <div className="px-0 py-3 md:px-0 lg:px-0 md:py-4">
