f530f6bfa7fad3f224afb17ba78566d9086c292b
dimak <rahbussiness@gmail.com>
2023-10-13 17:42:54 +0200

    changes array

--- src/components/environments/changelog/ChangelogItem.tsx
+++ src/components/environments/changelog/ChangelogItem.tsx
@@ -10,12 +10,14 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/comp
 import clsx from 'clsx'
 
 interface Props {
-  secretKey: string
-  newValue?: string
-  oldValue?: string
+  changes: Array<{
+    secretKey: string
+    newValue?: string
+    oldValue?: string
+  }>
 }
 
-const ChangelogItem: React.FC<Props> = ({ secretKey, newValue, oldValue }) => {
+const ChangelogItem: React.FC<Props> = ({ changes }) => {
   const [hidden, setHidden] = useState(true)
 
   const toggleHide = () => setHidden(!hidden)
@@ -71,67 +73,71 @@ const ChangelogItem: React.FC<Props> = ({ secretKey, newValue, oldValue }) => {
         </div>
         {/* // Secrets changes */}
         <div className="ml-[3.75rem] bg-red-400x">
-          <div className="w-full h-fit bg-red-400X my-2 text-sm rounded-md border border-input">
-            <div className="flex flex-row gap-2 items-center">
-              <div
-                className={clsx(['h-10 flex gap-3.5 px-3.5 items-center'], {
-                  'w-1/2': (oldValue && !newValue) || (!oldValue && newValue),
-                  'w-1/3': !((oldValue && !newValue) || (!oldValue && newValue)),
-                })}
-              >
-                <input
-                  readOnly
-                  className="w-full opacity-80 font-bold outline-none bg-transparent"
-                  value={secretKey}
-                />
+          {changes?.map(({ secretKey, newValue, oldValue }) => (
+            <>
+              <div className="w-full h-fit bg-red-400X my-2 text-sm rounded-md border border-input">
+                <div className="flex flex-row gap-2 items-center">
+                  <div
+                    className={clsx(['h-10 flex gap-3.5 px-3.5 items-center'], {
+                      'w-1/2': (oldValue && !newValue) || (!oldValue && newValue),
+                      'w-1/3': !((oldValue && !newValue) || (!oldValue && newValue)),
+                    })}
+                  >
+                    <input
+                      readOnly
+                      className="w-full opacity-80 font-bold outline-none bg-transparent"
+                      value={secretKey}
+                    />
+                  </div>
+
+                  {oldValue && (
+                    <div
+                      className={clsx(['h-10 pr-2 flex gap-3.5 items-center'], {
+                        'w-1/2': !newValue,
+                        'w-1/3': newValue,
+                      })}
+                    >
+                      <div className="px-2 h-full flex justify-center items-center bg-red-600/5 dark:bg-red-600/10 border-red-600/40 dark:border-red-600/20 border-x-[1.5px]">
+                        <Icons.minus className="opacity-70 h-3.5 w-3.5 text-red-600" />
+                      </div>
+
+                      {hidden ? (
+                        <span className="block opacity-85">•••••••••••</span>
+                      ) : (
+                        <input
+                          readOnly
+                          className="w-full text-red-600 dark:text-red-600/80 outline-none bg-transparent"
+                          value={oldValue}
+                        />
+                      )}
+                    </div>
+                  )}
+
+                  {newValue && (
+                    <div
+                      className={clsx(['h-10 flex gap-3.5 items-center'], {
+                        'w-1/2': !oldValue,
+                        'w-1/3': oldValue,
+                      })}
+                    >
+                      <div className="px-2 h-full flex justify-center items-center bg-green-600/5 dark:bg-green-600/10 border-green-600/40 dark:border-green-600/20 border-x-[1.5px]">
+                        <Icons.minus className="opacity-70 h-3.5 w-3.5 text-green-600" />
+                      </div>
+                      {hidden ? (
+                        <span className="block opacity-85 font-bold ">•••••••••••</span>
+                      ) : (
+                        <input
+                          readOnly
+                          className="w-full text-green-600 dark:text-green-600/90 outline-none bg-transparent"
+                          value={newValue}
+                        />
+                      )}
+                    </div>
+                  )}
+                </div>
               </div>
-
-              {oldValue && (
-                <div
-                  className={clsx(['h-10 pr-2 flex gap-3.5 items-center'], {
-                    'w-1/2': !newValue,
-                    'w-1/3': newValue,
-                  })}
-                >
-                  <div className="px-2 h-full flex justify-center items-center bg-red-600/5 dark:bg-red-600/10 border-red-600/40 dark:border-red-600/20 border-x-[1.5px]">
-                    <Icons.minus className="opacity-70 h-3.5 w-3.5 text-red-600" />
-                  </div>
-
-                  {hidden ? (
-                    <span className="block opacity-85">•••••••••••</span>
-                  ) : (
-                    <input
-                      readOnly
-                      className="w-full text-red-600 dark:text-red-600/90 outline-none bg-transparent"
-                      value={oldValue}
-                    />
-                  )}
-                </div>
-              )}
-
-              {newValue && (
-                <div
-                  className={clsx(['h-10 flex gap-3.5 items-center'], {
-                    'w-1/2': !oldValue,
-                    'w-1/3': oldValue,
-                  })}
-                >
-                  <div className="px-2 h-full flex justify-center items-center bg-green-600/5 dark:bg-green-600/10 border-green-600/40 dark:border-green-600/20 border-x-[1.5px]">
-                    <Icons.minus className="opacity-70 h-3.5 w-3.5 text-green-600" />
-                  </div>
-                  {hidden ? (
-                    <span className="block opacity-85 font-bold ">•••••••••••</span>
-                  ) : (
-                    <input
-                      readOnly
-                      className="w-full text-green-600 dark:text-green-600/90 outline-none bg-transparent"
-                      value={newValue}
-                    />
-                  )}
-                </div>
-              )}
-            </div>
-          </div>
+            </>
+          ))}
 
           <div className="mt-1">
             <div className="flex items-center text-[0.90rem] opacity-80">
