255fabd69c1b6d72cfa7ecc66baedcf73feb2664
dimak <rahbussiness@gmail.com>
2023-10-14 11:09:14 +0200

    infinite query

--- src/components/environments/changelog/Changelog.tsx
+++ src/components/environments/changelog/Changelog.tsx
@@ -1,10 +1,13 @@
+'use client'
+
 import dayjs from 'dayjs'
-import React from 'react'
+import React, { useState } from 'react'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import TypographyH4 from '@/components/typography/TypographyH4'
 import ChangelogItem from './ChangelogItem'
 import { Separator } from '@/components/ui/separator'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { getEnvChangelog } from '@/api/requests/envChangelog'
 
 dayjs.extend(relativeTime)
 
@@ -15,51 +18,50 @@ interface Props {
 }
 
 const Changelog: React.FC<Props> = ({ workspaceId, projectName, envName }) => {
+  const [hasMore, setHasMore] = useState(true)
+
+  const { data, error, isFetching, isFetchingNextPage, fetchNextPage, isRefetching, refetch } =
+    useInfiniteQuery(
+      ['changelog', workspaceId, projectName, envName],
+      async ({ pageParam = undefined }) => {
+        const res = await getEnvChangelog({
+          workspaceId,
+          projectName,
+          envName,
+        })
+
+        setHasMore(res?.hasMore ?? false)
+
+        return res.data
+      },
+      {
+        enabled: true,
+        getPreviousPageParam: (firstPage) => {
+          return {
+            id: firstPage?.[0]?.createdAt ?? undefined,
+            date: firstPage?.[0]?.createdAt ?? undefined,
+          }
+        },
+        getNextPageParam: (lastPage) => {
+          return {
+            id: lastPage?.[lastPage.length - 1]?.id ?? undefined,
+            date: lastPage?.[lastPage.length - 1]?.createdAt ?? undefined,
+          }
+        },
+      }
+    )
+
   return (
     <>
       <div className="mb-6">
         <TypographyH4>Today</TypographyH4>
       </div>
 
-      <div className="flex flex-col gap-6 md:gap-8">
-        {[
-          {
-            changes: [
-              {
-                secretKey: 'KEY_2',
-                newValue: 'new_value_2',
-                oldValue: 'old_value_2',
-              },
-              {
-                secretKey: 'KEY_3',
-                newValue: 'new_value_3',
-              },
-              {
-                secretKey: 'VERY_SECRET',
-                oldValue: 'old_value',
-              },
-            ],
-          },
-          {
-            changes: [
-              {
-                secretKey: 'KEY_2',
-                newValue: 'new_value_2',
-              },
-              {
-                secretKey: 'KEY_3',
-                oldValue: 'new_value_3',
-              },
-              {
-                secretKey: 'VERY_SECRET',
-                oldValue: 'old_value',
-              },
-            ],
-          },
-        ]?.map((val, index) => (
+      <div className="flex flex-col gap-6 md:gap-12">
+        {data?.pages?.flat(1)?.map((val, index) => (
           <>
-            <ChangelogItem {...val} />
-            {index !== 2 && <Separator />}
+            <ChangelogItem changes={val?.secretsChanges ?? []} />
+            {/* {index !== 2 && <Separator />} */}
           </>
         ))}
       </div>
