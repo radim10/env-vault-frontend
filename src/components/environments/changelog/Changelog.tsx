be7df54b9b761e16fbcd6269b2cb89134b733c46
dimak <rahbussiness@gmail.com>
2023-10-14 16:57:14 +0200

    update query data

--- src/components/environments/changelog/Changelog.tsx
+++ src/components/environments/changelog/Changelog.tsx
@@ -5,13 +5,15 @@ import React, { useState } from 'react'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import TypographyH4 from '@/components/typography/TypographyH4'
 import ChangelogItem from './ChangelogSecretsItem'
-import { useInfiniteQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
 import { getEnvChangelog, rollbackEnvChangelog } from '@/api/requests/envChangelog'
 import { Button } from '@/components/ui/button'
 import { Icons } from '@/components/icons'
 import ChangelogItemSkeleton from './ChangelogItemSkeleton'
 import { Skeleton } from '@/components/ui/skeleton'
 import RollbackDialog from './RollbackDialog'
+import { EnvChangelogItem } from '@/types/envChangelog'
+import { useToast } from '@/components/ui/use-toast'
 
 dayjs.extend(relativeTime)
 
@@ -22,6 +24,9 @@ interface Props {
 }
 
 const Changelog: React.FC<Props> = ({ workspaceId, projectName, envName }) => {
+  const { toast } = useToast()
+  const queryClient = useQueryClient()
+
   const [hasMore, setHasMore] = useState(true)
   const [rollbackDialogChangeId, setRollbackDialogChangeId] = useState<string | null>(null)
 
@@ -68,6 +73,41 @@ const Changelog: React.FC<Props> = ({ workspaceId, projectName, envName }) => {
     }
   )
 
+  const handleRollbackData = (newItem: EnvChangelogItem) => {
+    const key = ['changelog', workspaceId, projectName, envName]
+    const existingData = queryClient?.getQueryData<{ pages: Array<EnvChangelogItem[]> }>(key)
+
+    if (existingData) {
+      const firstPage = existingData?.pages?.[0]
+      if (!firstPage) return
+
+      const updatedPage = [newItem, ...firstPage]
+      const updatedData = { ...existingData }
+
+      updatedData.pages[0] = updatedPage
+
+      queryClient.setQueryData(key, updatedData)
+    }
+
+    closeDialog()
+
+    toast({
+      title: 'Rollback has been completed',
+      variant: 'success',
+    })
+  }
+
+  const handleNoChangesToRollback = () => {
+    toast({
+      title: 'No changes to rollback',
+      description: 'This is current secrets state',
+      variant: 'info',
+    })
+    closeDialog()
+  }
+
+  const closeDialog = () => setRollbackDialogChangeId(null)
+
   if (isLoading || isRefetching) {
     return (
       <>
@@ -86,7 +126,10 @@ const Changelog: React.FC<Props> = ({ workspaceId, projectName, envName }) => {
         changeId={rollbackDialogChangeId ?? ''}
         opened={rollbackDialogChangeId !== null}
         onClose={() => setRollbackDialogChangeId(null)}
-        onSuccess={() => setRollbackDialogChangeId(null)}
+        onSuccess={(item) => {
+          if (item) handleRollbackData(item)
+          if (!item) handleNoChangesToRollback()
+        }}
       />
       <div className="flex flex-col gap-6 md:gap-8">
         {data?.pages?.flat(1)?.map((val, index) => (
