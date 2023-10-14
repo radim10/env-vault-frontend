aac0de5ec75f14d61a4e8e3544721f8e7c56f6be
dimak <rahbussiness@gmail.com>
2023-10-14 20:03:03 +0200

    loader style condition

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,6 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
+import clsx from 'clsx'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -43,7 +44,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
-        {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+        {loading && <Loader2 className={clsx(["h-4 w-4 animate-spin"],{
+          'mr-2': props?.children
+        })} />}
         {props.children}
       </Comp>
     )
