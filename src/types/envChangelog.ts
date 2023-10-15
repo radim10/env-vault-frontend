9990bd7646f110e2f9ee1bcc02217dc795ffe4d7
dimak <rahbussiness@gmail.com>
2023-10-15 11:10:22 +0200

    EnvChange type

--- src/types/envChangelog.ts
+++ src/types/envChangelog.ts
@@ -1,7 +1,10 @@
+import { EnvironmentType } from './environments'
+
 export interface EnvChangelogItem {
   id: string
   createdAt: string
   secretsChanges?: SecretsChange[]
+  change?: EnvChange
   // TODO: user
 }
 
@@ -9,4 +12,23 @@ export interface SecretsChange {
   secretKey: string
   newValue: string | null
   oldValue: string | null
+}
+
+export type EnvChange = RenamedEnvChange | LockedEnvChange | EnvTypeChange
+
+export type RenamedEnvChange = {
+  action: 'renamed'
+  new: string
+  old: string
+}
+
+export type LockedEnvChange = {
+  action: 'lock'
+  locked: boolean
+}
+
+export type EnvTypeChange = {
+  action: 'type'
+  old: EnvironmentType
+  new: EnvironmentType
 }
