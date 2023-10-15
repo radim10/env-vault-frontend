d115495ec86efd6f2cd615dd6d7f75504d75f125
dimak <rahbussiness@gmail.com>
2023-10-15 11:10:38 +0200

    show changelog items

--- src/components/environments/changelog/Changelog.tsx
+++ src/components/environments/changelog/Changelog.tsx
@@ -4,7 +4,7 @@ import dayjs from 'dayjs'
 import React, { useState } from 'react'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import TypographyH4 from '@/components/typography/TypographyH4'
-import ChangelogItem from './ChangelogSecretsItem'
+import ChangelogSecretsItem from './ChangelogSecretsItem'
 import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
 import { getEnvChangelogItems } from '@/api/requests/envChangelog'
 import { Button } from '@/components/ui/button'
@@ -15,6 +15,7 @@ import RollbackDialog from './RollbackDialog'
 import { EnvChangelogItem, SecretsChange } from '@/types/envChangelog'
 import { useToast } from '@/components/ui/use-toast'
 import Error from '@/components/Error'
+import ChangelogItem from './ChangelogItem'
 
 dayjs.extend(relativeTime)
 
@@ -192,36 +193,49 @@ const Changelog: React.FC<Props> = ({ workspaceId, projectName, envName }) => {
               )}
             </div>
 
-            <ChangelogItem
-              key={val?.id}
-              workspaceId={workspaceId}
-              projectName={projectName}
-              envName={envName}
-              changeId={val?.id}
-              valuesLoaded={queryClient?.getQueryData(['changelog-secrets', val?.id]) !== undefined}
-              changes={
-                queryClient?.getQueryData(['changelog-secrets', val?.id]) ??
-                val?.secretsChanges ??
-                []
-              }
-              createdAt={`${dayjs(val?.createdAt).format('HH:mm')} (${dayjs(
-                val?.createdAt
-              ).fromNow()})`}
-              onRollback={() => setRollbackDialogChangeId(val?.id)}
-              onValuesLoaded={(values) =>
-                handleFetchedSecretValues({
-                  page: Math.ceil((index + 1) / 5) - 1,
-                  changeId: val?.id,
-                  secrets: values,
-                })
-              }
-              onError={() => {
-                toast({
-                  title: 'Something went wrong',
-                  variant: 'destructive',
-                })
-              }}
-            />
+            {val?.secretsChanges && (
+              <ChangelogSecretsItem
+                key={val?.id}
+                workspaceId={workspaceId}
+                projectName={projectName}
+                envName={envName}
+                changeId={val?.id}
+                valuesLoaded={
+                  queryClient?.getQueryData(['changelog-secrets', val?.id]) !== undefined
+                }
+                changes={
+                  queryClient?.getQueryData(['changelog-secrets', val?.id]) ??
+                  val?.secretsChanges ??
+                  []
+                }
+                createdAt={`${dayjs(val?.createdAt).format('HH:mm')} (${dayjs(
+                  val?.createdAt
+                ).fromNow()})`}
+                onRollback={() => setRollbackDialogChangeId(val?.id)}
+                onValuesLoaded={(values) =>
+                  handleFetchedSecretValues({
+                    page: Math.ceil((index + 1) / 5) - 1,
+                    changeId: val?.id,
+                    secrets: values,
+                  })
+                }
+                onError={() => {
+                  toast({
+                    title: 'Something went wrong',
+                    variant: 'destructive',
+                  })
+                }}
+              />
+            )}
+            {val?.change && (
+              <ChangelogItem
+                change={val.change}
+                createdAt={`${dayjs(val?.createdAt).format('HH:mm')} (${dayjs(
+                  val?.createdAt
+                ).fromNow()})`}
+                onRollback={() => {}}
+              />
+            )}
             {/* {index !== 2 && <Separator />} */}
           </div>
         ))}
