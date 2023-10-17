22838e33d27f3fb75d835ba89040563f6f452317
dimak <rahbussiness@gmail.com>
2023-10-17 13:01:05 +0200

    env changelog types

--- src/types/envChangelog.ts
+++ src/types/envChangelog.ts
@@ -1,20 +1,44 @@
 import { EnvironmentType } from './environments'
 
+// export interface EnvChangelogItem {
+//   id: string
+//   createdAt: string
+//   secretsChanges?: SecretsChange[]
+//   change?: EnvChange
+//   // TODO: user
+// }
+//
+//
+// export interface SecretsChange {
+//   secretKey: string
+//   newValue: string | null
+//   oldValue: string | null
+// }
+//
+// export type EnvChange = RenamedEnvChange | LockedEnvChange | EnvTypeChange
+
 export interface EnvChangelogItem {
   id: string
   createdAt: string
-  secretsChanges?: SecretsChange[]
-  change?: EnvChange
+  change: EnvChange
   // TODO: user
 }
 
-export interface SecretsChange {
-  secretKey: string
-  newValue: string | null
-  oldValue: string | null
+export type EnvChange = SecretsEnvChage | RenamedEnvChange | LockedEnvChange | EnvTypeChange
+
+export type SecretsEnvChage = {
+  action: 'secrets'
+  data: Array<SecretsChange>
 }
 
-export type EnvChange = RenamedEnvChange | LockedEnvChange | EnvTypeChange
+export interface SecretsChange {
+  key: string
+  old?: string
+  new?: string
+  // only renamed
+  newKey?: string
+  value?: string
+}
 
 export type RenamedEnvChange = {
   action: 'renamed'
