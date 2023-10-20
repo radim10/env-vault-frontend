d835fbea904f968af4e56e267e388531ede85486
dimak <rahbussiness@gmail.com>
2023-10-20 14:48:42 +0200

    env tabs - changelog filter query path

--- src/components/environments/EnvTabs.tsx
+++ src/components/environments/EnvTabs.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useSelectedEnvironmentStore } from '@/stores/selectedEnv'
 import clsx from 'clsx'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
@@ -20,13 +21,18 @@ const items = [
 ]
 
 const EnvTabs: React.FC<Props> = ({ workspaceId, projectName, envName }) => {
+  const envChagelogFilter = useSelectedEnvironmentStore((state) => state?.changelogFilter)
   const params = useParams<{ workspace: string; projectName: string; env: string; tab?: string }>()
 
   return (
     <div className="px-6 lg:px-10 dark:text-gray-400 flex items-center gap-2 md:gap-4 overflow-y-auto text-[1rem] border-b-[1px] pb-0 md:px-3">
       {items.map(({ label, href }, index) => (
         <Link
-          href={`/workspace/${workspaceId}/projects/${projectName}/env/${envName}/${href}`}
+          href={
+            label === 'Changelog' && envChagelogFilter === 'secrets'
+              ? `/workspace/${workspaceId}/projects/${projectName}/env/${envName}/${href}?only-secrets=true`
+              : `/workspace/${workspaceId}/projects/${projectName}/env/${envName}/${href}`
+          }
           className={clsx(
             [
               'border-b-[3px] flex items-center gap-2 h-full hover:dark:text-gray-200 ease duration-150 px-2 lg:px-3 pb-3',
