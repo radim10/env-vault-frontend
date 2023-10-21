fcccbcf8bd2d63502c602d7d053729b7afcf79cd
dimak <rahbussiness@gmail.com>
2023-10-21 13:02:42 +0200

    fullComponent prop

--- src/components/SettingsList.tsx
+++ src/components/SettingsList.tsx
@@ -10,6 +10,7 @@ interface Item {
   label: String
   icon: LucideIcon
   component?: ReactNode
+  fullComponent?: ReactNode
   value?: String
   editBtn?: {
     disabled?: boolean
@@ -42,38 +43,86 @@ const SettingsList: React.FC<Props> = ({ title, description, icon: Icon, items }
           )}
 
           <div className="mt-7 flex flex-col gap-2.5 text-[0.96rem]">
-            {items.map(({ label, icon: Icon, value, component, editBtn }, index) => (
+            {items.map(({ label, icon: Icon, value, component, fullComponent, editBtn }, index) => (
               <>
-                <div className="flex flex-col md:flex-row md:items-center gap-2 text-md md:justify-between px-4 md:px-10 md:h-8">
-                  <div className="flex items-center gap-2 md:gap-3">
-                    <Icon className="h-4 w-4 opacity-80" />
-                    <span className="font-semibold text-[0.96rem]">{label}</span>
-                  </div>
-                  <div>
-                    {value && <div>{value}</div>}
-                    {component && (
-                      <div
-                        className={clsx(['flex justify-between md:justify-start items-center'], {
-                          'gap-3 md:gap-6': editBtn,
-                        })}
-                      >
-                        <div>{component}</div>
-                        <div>
-                          {editBtn && (
-                            <Button
-                              size={'sm'}
-                              variant={'outline'}
-                              disabled={editBtn?.disabled}
-                              onClick={() => editBtn.onClick()}
-                            >
-                              <Icons.pencil className="h-3.5 w-3.5" />
-                            </Button>
-                          )}
+                {(value || component) && (
+                  <div className="flex flex-col md:flex-row md:items-center gap-2 text-md md:justify-between px-4 md:px-10 md:h-8">
+                    <div className="flex items-center gap-2 md:gap-3">
+                      <Icon className="h-4 w-4 opacity-80" />
+                      <span className="font-semibold text-[0.96rem]">{label}</span>
+                    </div>
+                    <div>
+                      {value && <div>{value}</div>}
+                      {component && (
+                        <div
+                          className={clsx(['flex justify-between md:justify-start items-center'], {
+                            'gap-3 md:gap-6': editBtn,
+                          })}
+                        >
+                          <div>{component}</div>
+                          <div>
+                            {editBtn && (
+                              <Button
+                                size={'sm'}
+                                variant={'outline'}
+                                disabled={editBtn?.disabled}
+                                onClick={() => editBtn.onClick()}
+                              >
+                                <Icons.pencil className="h-3.5 w-3.5" />
+                              </Button>
+                            )}
+                          </div>
                         </div>
-                      </div>
-                    )}
+                      )}
+                    </div>
                   </div>
-                </div>
+                )}
+                {fullComponent && (
+                  <>
+                    <div className="px-4 md:px-10">
+                      <div className="flex justify-between items-center">
+                        <div className="flex flex-col gap-2">
+                          <div className="flex items-center gap-2 md:gap-3">
+                            <Icon className="h-4 w-4 opacity-80" />
+                            <span className="font-semibold text-[0.96rem]">{label}</span>
+                          </div>
+
+                          <div>{fullComponent}</div>
+                        </div>
+
+                        {editBtn && (
+                          <Button
+                            size={'sm'}
+                            variant={'outline'}
+                            disabled={editBtn?.disabled}
+                            onClick={() => editBtn.onClick()}
+                          >
+                            <Icons.pencil className="h-3.5 w-3.5" />
+                          </Button>
+                        )}
+                      </div>
+                    </div>
+                    {/*   <div className="px-4 md:px-10 bg-red-600"> */}
+                    {/*     <div className="flex flex-col md:flex-row md:items-center gap-2 text-md md:justify-between md:h-8"> */}
+                    {/*       <div className="flex items-center gap-2 md:gap-3"> */}
+                    {/*         <Icon className="h-4 w-4 opacity-80" /> */}
+                    {/*         <span className="font-semibold text-[0.96rem]">{label}</span> */}
+                    {/*       </div> */}
+                    {/*       {editBtn && ( */}
+                    {/*         <Button */}
+                    {/*           size={'sm'} */}
+                    {/*           variant={'outline'} */}
+                    {/*           disabled={editBtn?.disabled} */}
+                    {/*           onClick={() => editBtn.onClick()} */}
+                    {/*         > */}
+                    {/*           <Icons.pencil className="h-3.5 w-3.5" /> */}
+                    {/*         </Button> */}
+                    {/*       )} */}
+                    {/*     </div> */}
+                    {/*     {fullComponent} */}
+                    {/*   </div> */}
+                  </>
+                )}
                 {index !== items.length - 1 && <Separator />}
               </>
             ))}
