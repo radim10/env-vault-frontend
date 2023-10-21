b512e927f032ebc52fa57405e718c95447fbfff2
dimak <rahbussiness@gmail.com>
2023-10-21 12:35:43 +0200

    handle remove project

--- src/components/projects/ProjectSettings.tsx
+++ src/components/projects/ProjectSettings.tsx
@@ -2,10 +2,15 @@
 
 import dayjs from 'dayjs'
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { Icons } from '../icons'
 import SettingsList from '../SettingsList'
 import DangerZone from '../DangerZone'
+import DeleteProjectDialog from './DeleteProjectDialog'
+import { useQueryClient } from '@tanstack/react-query'
+import { Project } from '@/types/projects'
+import { toast } from '../ui/use-toast'
 
 dayjs.extend(relativeTime)
 
@@ -15,10 +20,52 @@ interface Props {
 }
 
 const ProjectSettings: React.FC<Props> = ({ workspaceId, projectName }) => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
   const [dialog, setDialog] = useState<'rename' | 'delete' | 'lock' | 'changeType' | null>(null)
+
+  const closeDialog = () => {
+    setDialog(null)
+  }
+
+  const handleRemovedProject = () => {
+    setDialog(null)
+
+    const projectData = queryClient.getQueryData<Project>(['project', workspaceId, projectName])
+    // update project list
+
+    if (projectData) {
+      queryClient.setQueryData(['project', workspaceId, projectName], null)
+    }
+
+    const projects = queryClient.getQueryData<Project[]>(['projects', workspaceId])
+
+    if (projects) {
+      queryClient.setQueryData(
+        ['projects', workspaceId],
+        projects.filter((item) => item.name !== projectName)
+      )
+    }
+
+    toast({
+      title: 'Project has been deleted',
+      variant: 'success',
+    })
+
+    router.push(`/workspace/${workspaceId}/projects`)
+  }
 
   return (
     <>
+      <DeleteProjectDialog
+        workspaceId={workspaceId}
+        projectName={projectName}
+        opened={dialog === 'delete'}
+        onClose={closeDialog}
+        onSuccess={() => handleRemovedProject()}
+      />
+      {/* */}
+
       <div className="flex flex-col gap-7">
         <SettingsList
           title="General settings"
@@ -62,7 +109,7 @@ const ProjectSettings: React.FC<Props> = ({ workspaceId, projectName }) => {
 
         <DangerZone
           btn={{
-            onClick: () => { },
+            onClick: () => setDialog('delete'),
             disabled: false,
           }}
           title="Delete project"
