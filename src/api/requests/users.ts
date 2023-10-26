99fe72db14d2653d6759f12462522448c14135d3
dimak <rahbussiness@gmail.com>
2023-10-26 15:58:19 +0200

    get workspace users

---
+++ src/api/requests/users.ts
@@ -0,0 +1,28 @@
+import sendRequest, { APIError } from '../instance'
+import { User } from '@/types/users'
+
+type UsersErrorCode = 'workspace_not_found'
+export type UsersError<T extends UsersErrorCode | void> = APIError<T>
+
+export function workspacesErrorMsgFromCode(code: UsersErrorCode): string {
+  let msg = ''
+
+  if (code === 'workspace_not_found') {
+    msg = 'Workspace has been deleted'
+  }
+
+  return msg
+}
+
+// NOTE: requests
+export type GetWorkspaceUsersError = UsersError<undefined>
+export type GetWorkspaceUsersData = User
+
+export async function getWorkspaceUsers(id: string) {
+  const response = sendRequest<GetWorkspaceUsersData>({
+    method: 'GET',
+    basePath: `workspaces`,
+    path: `${id}/users`,
+  })
+  return await response
+}
