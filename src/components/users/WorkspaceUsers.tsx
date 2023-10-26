1f98b5f7393396a6eceaa31ba9cce9d0f0a8206a
dimak <rahbussiness@gmail.com>
2023-10-26 20:59:21 +0200

    data

--- src/components/users/WorkspaceUsers.tsx
+++ src/components/users/WorkspaceUsers.tsx
@@ -3,19 +3,35 @@
 import { useGetWorkspaceUsers } from '@/api/queries/users'
 import { columns } from './table/Columns'
 import UsersDataTable from './table/DataTable'
+import { WorkspaceUser, WorkspaceUserRole } from '@/types/users'
 
 interface Props {
   workspaceId: string
 }
 
 const WorkspaceUsers: React.FC<Props> = ({ workspaceId }) => {
-  // const { data } = useGetWorkspaceUsers(workspaceId)
+  const { data } = useGetWorkspaceUsers(workspaceId)
 
-  const dummyData =[{"id":"915b7fa0-f194-47cb-8b47-479a4d88f0b4","name":"radim","avatarUrl":"https://avatars.githubusercontent.com/u/62602887?v=4"},{"id":"8afba311-8706-472d-836b-5d62a1eb8e32","name":"John Doe John Doe John Doe John Doe dasdsadasd","avatarUrl":null}] 
-
+  // const dummyData: WorkspaceUser[] = [
+  //   {
+  //     id: '915b7fa0-f194-47cb-8b47-479a4d88f0b4',
+  //     name: 'radim',
+  //     avatarUrl: 'https://avatars.githubusercontent.com/u/62602887?v=4',
+  //     role: WorkspaceUserRole.ADMIN,
+  //   },
+  //   {
+  //     id: '8afba311-8706-472d-836b-5d62a1eb8e32',
+  //     name: 'John Doe John Doe John Doe John Doe dasdsadasd',
+  //     avatarUrl: null,
+  //     role: WorkspaceUserRole.MEMBER,
+  //
+  //   },
+  // ]
+  //
   return (
     <div>
-      <UsersDataTable columns={columns as any} data={dummyData} />
+      {/* <UsersDataTable columns={columns as any} data={dummyData} /> */}
+      <UsersDataTable columns={columns as any} data={data ?? []} />
     </div>
   )
 }
