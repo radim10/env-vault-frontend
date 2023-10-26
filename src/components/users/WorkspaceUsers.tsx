354d57e8691d58a957e05f99498a6eb18577c898
dimak <rahbussiness@gmail.com>
2023-10-26 20:21:01 +0200

    dummy data

--- src/components/users/WorkspaceUsers.tsx
+++ src/components/users/WorkspaceUsers.tsx
@@ -9,11 +9,13 @@ interface Props {
 }
 
 const WorkspaceUsers: React.FC<Props> = ({ workspaceId }) => {
-  const { data } = useGetWorkspaceUsers(workspaceId)
+  // const { data } = useGetWorkspaceUsers(workspaceId)
+
+  const dummyData =[{"id":"915b7fa0-f194-47cb-8b47-479a4d88f0b4","name":"radim","avatarUrl":"https://avatars.githubusercontent.com/u/62602887?v=4"},{"id":"8afba311-8706-472d-836b-5d62a1eb8e32","name":"John Doe John Doe John Doe John Doe dasdsadasd","avatarUrl":null}] 
 
   return (
     <div>
-      <UsersDataTable columns={columns as any} data={data ?? []} />
+      <UsersDataTable columns={columns as any} data={dummyData} />
     </div>
   )
 }
