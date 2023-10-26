e555427ed67f52d423de52c70849bf34f261bf1f
dimak <rahbussiness@gmail.com>
2023-10-26 18:07:42 +0200

    show component

--- src/app/(dashboard)/workspace/[workspace]/users/[tab]/page.tsx
+++ src/app/(dashboard)/workspace/[workspace]/users/[tab]/page.tsx
@@ -1,9 +1,10 @@
+import WorkspaceUsers from '@/components/users/WorkspaceUsers'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Users',
 }
 
-export default function UsersPage({ params }: { params: { workspace: string } }) {
-  return <>Users page</>
+export default function UsersPage({ params }: { params: { workspace: string; tab: string } }) {
+  return <>{params.tab === 'workspace' && <WorkspaceUsers workspaceId={params.workspace} />}</>
 }
