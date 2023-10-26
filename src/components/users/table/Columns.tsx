1527e14418f181c3791207edefc2ebe853ed22c7
dimak <rahbussiness@gmail.com>
2023-10-26 20:21:10 +0200

    cols

--- src/components/users/table/Columns.tsx
+++ src/components/users/table/Columns.tsx
@@ -14,54 +14,105 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Icons } from '@/components/icons'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import UserRoleBadge from '../UserRoleBadge'
+
+dayjs.extend(relativeTime)
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
 export const columns: ColumnDef<User>[] = [
   {
-    accessorKey: 'name',
-    // header: () => <div className="text-left">User</div>,
+    accessorKey: 'avatarUrl',
     header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          User
-          {column.getIsSorted() === 'asc' ? <Icons.arrowUp className="ml-2 h-4 w-4" /> : <Icons.arrowDown className="ml-2 h-4 w-4" />}
-        </Button>
-      )
+      
+      return <></>
     },
     cell: ({ row }) => {
-      const name = row.getValue('name') as string
-
       return (
-        <div className="flex flex-row gap-2 md:gap-3">
-          <div>
-            <Avatar className="w-10 h-10">
-              <AvatarImage src={row.getValue('avatarUrl')} />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
-
-          <div className="flex flex-col">
-            <div className="text-left font-bold">{name}</div>
-
-            <div className="text-left text-muted-foreground">email@example.com</div>
-          </div>
-        </div>
+        <Avatar className="w-10 h-10">
+          <AvatarImage src={row.getValue('avatarUrl')} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
       )
     },
   },
 
   {
-    accessorKey: 'role',
-    header: () => <div className="text-left">Role</div>,
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <button
+          className="flex items-center gap-1"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Name
+          {column.getIsSorted() && (
+            <>
+              {column.getIsSorted() === 'asc' ? (
+                <Icons.arrowUpDown className="ml-2 h-4 w-4" />
+              ) : (
+                <Icons.arrowDown className="ml-2 h-4 w-4" />
+              )}
+            </>
+          )}
+        </button>
+      )
+    },
     cell: ({ row }) => {
       const name = row.getValue('name') as string
 
-      return <div>Admin</div>
+      return <div className="text-left font-bold">{name}</div>
+    },
+  },
+
+  {
+    accessorKey: 'email',
+    header: ({ column }) => {
+      return (
+        <button
+          className="flex items-center gap-1"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Email
+          {column.getIsSorted() && (
+            <>
+              {column.getIsSorted() === 'asc' ? (
+                <Icons.arrowUp className="ml-2 h-4 w-4" />
+              ) : (
+                <Icons.arrowDown className="ml-2 h-4 w-4" />
+              )}
+            </>
+          )}
+        </button>
+      )
+    },
+    cell: ({ row }) => {
+      // const email = row.getValue('name') as string
+      const email = 'email@example.com'
+      return <div className="text-left">{email}</div>
+    },
+  },
+
+  {
+    accessorKey: 'role',
+    header: () => <div className="text-start ml-2">Role</div>,
+    cell: ({ row }) => {
+      const name = row.getValue('name') as string
+
+      return <div><UserRoleBadge role='admin'/></div>
+    },
+  },
+
+  {
+    id: 'joined',
+    header: () => <div className="text-left">Joined</div>,
+    cell: ({ row }) => {
+      const relativeDate = dayjs().fromNow()
+
+      return <div>{relativeDate}</div>
     },
   },
 
@@ -71,23 +122,25 @@ export const columns: ColumnDef<User>[] = [
       const payment = row.original
 
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.id)}>
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="w-full flex justify-end items-center pr-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.id)}>
+                Copy payment ID
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>View customer</DropdownMenuItem>
+              <DropdownMenuItem>View payment details</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       )
     },
   },
