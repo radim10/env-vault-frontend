fa2e52432c3b37c58bbd0a5cd04429646e0b2728
dimak <rahbussiness@gmail.com>
2023-10-27 12:45:30 +0200

    pagination

--- src/components/users/table/DataTable.tsx
+++ src/components/users/table/DataTable.tsx
@@ -1,12 +1,13 @@
 'use client'
 
+import { useMemo, useState } from 'react'
+import clsx from 'clsx'
 import {
   ColumnDef,
+  PaginationState,
   SortingState,
   flexRender,
   getCoreRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
 
@@ -19,32 +20,96 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import { Button } from '@/components/ui/button'
-import { useState } from 'react'
-import clsx from 'clsx'
+import { Icons } from '@/components/icons'
+import { useGetWorkspaceUsers } from '@/api/queries/users'
+import { WorkspaceUser } from '@/types/users'
+import { Skeleton } from '@/components/ui/skeleton'
+import TableToolbar from '../TableToolbar'
 
-interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[]
+interface DataTableProps {
+  workspaceId: string
+  columns: ColumnDef<WorkspaceUser>[]
 }
 
-function UsersDataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+function UsersDataTable({ columns, workspaceId }: DataTableProps) {
+  // const [pagesLoaded, setPagesLoaded] = useState<number[]>([])
+  const [totalCount, setTotalCount] = useState<number>(0)
+
+  const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 5,
+  })
+
+  const pagination = useMemo(
+    () => ({
+      pageIndex,
+      pageSize,
+    }),
+    [pageIndex, pageSize]
+  )
+
+  const defaultData = useMemo(() => [], [])
+
   const [sorting, setSorting] = useState<SortingState>([])
 
+  const fetchDataOptions = {
+    page: pageIndex,
+    sort: (sorting?.[0]?.id === 'joinedAt' ? 'joined' : sorting?.[0]?.id) as any,
+    desc: sorting?.[0]?.desc ?? undefined,
+  }
+
+  const { data, isLoading, isFetching } = useGetWorkspaceUsers(
+    { workspaceId, ...fetchDataOptions },
+    {
+      keepPreviousData: false,
+      staleTime: Infinity,
+      onSuccess: (data) => {
+        if (totalCount !== data?.totalCount) {
+          setTotalCount(data?.totalCount)
+        }
+      },
+      //   onSuccess: () =>
+      //     setPagesLoaded((prev) => {
+      //       const unique = [...prev, pageIndex]
+      //       return Array.from(new Set(unique))
+      //     }),
+    }
+  )
+
+  // useUpdateEffect(() => {
+  //   if (pagesLoaded?.length === table.getPageCount() && pagesLoaded?.length > 0) {
+  //     alert('all pages loaded')
+  //   }
+  // }, [pagesLoaded])
+
+  // useUpdateEffect(() => {
+  //   refetch()
+  // }, [pageIndex, sorting])
+
   const table = useReactTable({
-    pageCount: 1 ?? undefined,
-    data,
+    pageCount: totalCount ? Math.ceil(totalCount / 5) : undefined,
+    data: data?.data ?? defaultData,
     columns,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
+
+    // getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
-    getSortedRowModel: getSortedRowModel(),
+    onPaginationChange: setPagination,
+    // getSortedRowModel: getSortedRowModel(),
+    manualPagination: true,
+    manualSorting: true,
+    debugTable: true,
+    enableSorting: !isFetching,
     state: {
       sorting,
+      pagination,
     },
   })
 
   return (
     <div>
+      <TableToolbar userCount={totalCount} />
+
       <div className="rounded-md border">
         <Table>
           <TableHeader>
@@ -57,7 +122,7 @@ function UsersDataTable<TData, TValue>({ columns, data }: DataTableProps<TData, 
                       className={clsx({
                         'bg-red-500X  w-8': index === 0,
                         'md:w-[27%]': index === 2 || index === 1,
-                        'md:w-36 bg-red-300X': index === 3,
+                        'md:w-36 2xl:w-56 bg-red-300X': index === 3,
                       })}
                     >
                       {header.isPlaceholder
@@ -70,51 +135,116 @@ function UsersDataTable<TData, TValue>({ columns, data }: DataTableProps<TData, 
             ))}
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                  {row.getVisibleCells().map((cell, index) => (
-                    <TableCell
-                      key={cell.id}
-                      className={clsx(['py-2 md:py-3'], {
-                        'pr-0': index === 0,
-                      })}
-                    >
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
-                  No results.
-                </TableCell>
-              </TableRow>
+            {isLoading && (
+              <>
+                {Array.from({ length: 5 }).map((row) => (
+                  <TableRow className="h-16 w-full bg-red-400X hover:bg-transparent">
+                    {Array.from({ length: 6 }).map((cell, index) => (
+                      <TableCell
+                        key={index}
+                        className={clsx(['py-2 md:py-3'], {
+                          'pr-0': index === 0,
+                        })}
+                      >
+                        <Skeleton
+                          className={clsx(['w-full'], {
+                            'h-6 w-full': index !== 0,
+                            'w-10 h-10 rounded-full': index === 0,
+                          })}
+                        />
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </>
+            )}
+            {!isLoading && (
+              <>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                      {row.getVisibleCells().map((cell, index) => (
+                        <TableCell
+                          key={cell.id}
+                          className={clsx(['py-2 md:py-3'], {
+                            'pr-0': index === 0,
+                          })}
+                        >
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <>
+                    {!isLoading && (
+                      <TableRow>
+                        <TableCell colSpan={columns.length} className="h-24 text-center">
+                          No results.
+                        </TableCell>
+                      </TableRow>
+                    )}
+
+                    {isLoading && (
+                      <TableRow>
+                        <TableCell colSpan={columns.length} className="h-24 text-center">
+                          Loading
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </>
+                )}
+              </>
             )}
           </TableBody>
         </Table>
       </div>
       <div className="flex justify-end items-center gap-4">
-        <div className="text-muted-foreground text-sm">Pages: {Math.ceil(2 / 5)}/1</div>
+        {/* <div className="text-muted-foreground text-sm">Pages: {Math.ceil(2 / 5)}/1</div> */}
+        {table.getPageCount() !== 0 && (
+          <span className="flex items-center gap-1 text-sm text-muted-foreground">
+            <div>Page</div>
+            <span className="">
+              {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+            </span>
+          </span>
+        )}
+
         <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <Icons.chevronsLeft className="h-4 w-4" />
+          </Button>
+
           <Button
             variant="outline"
             size="sm"
             onClick={() => table.previousPage()}
             disabled={!table.getCanPreviousPage()}
           >
-            Previous
+            <Icons.chevronLeft className="h-4 w-4" />
           </Button>
 
           <Button
             variant="outline"
             size="sm"
-            className="px-5"
             onClick={() => table.nextPage()}
             disabled={!table.getCanNextPage()}
           >
-            Next
+            <Icons.chevronRight className="h-4 w-4" />
+          </Button>
+
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <Icons.chevronsRight className="h-4 w-4" />
           </Button>
         </div>
       </div>
