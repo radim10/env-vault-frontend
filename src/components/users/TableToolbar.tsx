610d74a22d486f5b61b205ed386cc3cbc0e0bc76
dimak <rahbussiness@gmail.com>
2023-10-27 14:55:11 +0200

    search

--- src/components/users/TableToolbar.tsx
+++ src/components/users/TableToolbar.tsx
@@ -6,9 +6,11 @@ import { Input } from '../ui/input'
 
 interface Props {
   userCount: number
+  search: string
+  onSearch: (search: string) => void
 }
 
-const TableToolbar: React.FC<Props> = ({ userCount }) => {
+const TableToolbar: React.FC<Props> = ({ userCount, search, onSearch }) => {
   return (
     <div>
       <div className="flex items-center justify-between mb-3">
@@ -19,10 +21,10 @@ const TableToolbar: React.FC<Props> = ({ userCount }) => {
         <div className="flex items-center w-fit justify-end gap-3">
           <div className="relative md:w-[12rem] lg:w-[16rem]">
             <Icons.search className="h-4 w-4 pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-3" />
-            {false && (
+            {search?.length > 0 && (
               <button
                 className="absolute top-1/2 transform -translate-y-1/2 right-4 opacity-60 hover:opacity-100"
-                onClick={() => { }}
+                onClick={() => onSearch('')}
               >
                 <Icons.x className="h-4 w-4" />
               </button>
@@ -31,8 +33,8 @@ const TableToolbar: React.FC<Props> = ({ userCount }) => {
             <Input
               placeholder="Search by name or email"
               className="pl-10 pr-10 -mr-10"
-              value={''}
-              onChange={(e) => { }}
+              value={search ?? undefined}
+              onChange={(e) => onSearch(e.target.value)}
             />
           </div>
 
