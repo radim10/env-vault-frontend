1bc653b4577aed7667c2a812920af210ed9aea34
dimak <rahbussiness@gmail.com>
2023-10-28 14:34:42 +0200

    role

--- src/components/users/UserRoleBadge.tsx
+++ src/components/users/UserRoleBadge.tsx
@@ -17,18 +17,18 @@ const UserRoleBadge: React.FC<Props> = ({ className, role }) => {
       className={cn(
         clsx(['text-[0.725rem] text-white dark:text-gray-200'], {
           'bg-blue-600 dark:bg-blue-800/80 hover:bg-blue-600 dark:hover:bg-blue-800/80 dark:text-blue-200 text-blue-100':
-            role === 'member',
+            role === WorkspaceUserRole.MEMBER,
           'bg-green-600 dark:bg-green-800/80 hover:bg-green-600 dark:hover:bg-green-800/80 dark:text-green-200 text-green-100':
-            role === 'admin',
+            role === WorkspaceUserRole.ADMIN,
           'bg-red-600 dark:bg-red-800/80 hover:bg-red-600 dark:hover:bg-red-800/80 dark:text-red-200 text-red-100':
-            role === 'owner',
+            role === WorkspaceUserRole.OWNER,
         }),
         className
       )}
     >
-      {role === 'member' && 'Member'}
-      {role === 'admin' && 'Admin'}
-      {role === 'owner' && 'Owner'}
+      {role === WorkspaceUserRole.MEMBER && 'Member'}
+      {role === WorkspaceUserRole.ADMIN && 'Admin'}
+      {role === WorkspaceUserRole.OWNER && 'Owner'}
     </Badge>
   )
 }
