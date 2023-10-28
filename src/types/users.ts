b30585fb80d0d9d0fb9790bc55f3cfe27bbdec5e
dimak <rahbussiness@gmail.com>
2023-10-28 14:34:39 +0200

    workspace user role

--- src/types/users.ts
+++ src/types/users.ts
@@ -8,9 +8,9 @@ export interface User {
 }
 
 export enum WorkspaceUserRole {
-  ADMIN = 'admin',
-  MEMBER = 'member',
-  OWNER = 'owner',
+  MEMBER = 'MEMBER',
+  ADMIN = 'ADMIN',
+  OWNER = 'OWNER',
 }
 
 export type WorkspaceUser = User & { role: WorkspaceUserRole; joinedAt: Date }
