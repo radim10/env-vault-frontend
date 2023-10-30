bea42fb5cc03a1c8e0ece6e727dd65edd7e33272
dimak <rahbussiness@gmail.com>
2023-10-30 21:10:49 +0100

    invitations store

---
+++ src/stores/invitations.ts
@@ -0,0 +1,38 @@
+import { createStore } from 'zustand/vanilla'
+
+export interface InvitationsState {
+  resendingIds: string[]
+  resentIds: string[]
+  errorIds: string[]
+}
+
+export interface InvitationsActions {
+  setResendingIds: (resendingIds: string[]) => void
+  setResentIds: (resendingIds: string[]) => void
+}
+
+// export const useInvitationsStore = create(
+//   devtools(
+//     immer<InvitationsState & InvitationsActions>((set) => ({
+//       resendingIds: [],
+//       setResendingIds: (resendingIds) => {
+//         set((state) => {
+//           state.resendingIds = resendingIds
+//         })
+//       },
+//     }))
+//   )
+// )
+//
+
+const invitationsStore = createStore<InvitationsState & InvitationsActions>((set) => ({
+  resendingIds: [],
+  resentIds: [],
+  errorIds: [],
+  setResendingIds: (updated) => set((state) => updated as any),
+  setResentIds: (updated) => set((state) => updated as any),
+}))
+
+const { getState, setState } = invitationsStore
+
+export { invitationsStore, getState, setState }
