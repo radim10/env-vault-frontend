b1586cdc2af0dacfb9df9f3ed263afbd9a73d36e
dimak <rahbussiness@gmail.com>
2023-10-30 21:11:27 +0100

    colored text

--- src/components/environments/EnvTypeBadge.tsx
+++ src/components/environments/EnvTypeBadge.tsx
@@ -15,13 +15,13 @@ const EnvTypeBadge: React.FC<Props> = ({ className, type }) => {
       variant="default"
       className={cn(
         clsx(['text-[0.725rem] text-white dark:text-gray-200'], {
-          'bg-indigo-600 dark:bg-indigo-800/80 hover:bg-indigo-600 dark:hover:bg-indigo-800/80':
+          'bg-indigo-600 dark:bg-indigo-800/80 hover:bg-indigo-600 dark:hover:bg-indigo-800/80 dark:text-indigo-200 text-indigo-100':
             type === EnvironmentType.DEVELOPMENT,
-          'bg-blue-600 dark:bg-blue-800/80 hover:bg-blue-600 dark:hover:bg-blue-800/80':
+          'bg-blue-600 dark:bg-blue-800/80 hover:bg-blue-600 dark:hover:bg-blue-800/80 dark:text-blue-200 text-blue-100':
             type === EnvironmentType.TESTING,
-          'bg-green-600 dark:bg-green-800/80 hover:bg-green-600 dark:hover:bg-green-800/80':
+          'bg-green-600 dark:bg-green-800/80 hover:bg-green-600 dark:hover:bg-green-800/80 dark:text-green-200 text-green-100':
             type === EnvironmentType.STAGING,
-          'bg-red-600 dark:bg-red-800/80 hover:bg-red-600 dark:hover:bg-red-800/80':
+          'bg-red-600 dark:bg-red-800/80 hover:bg-red-600 dark:hover:bg-red-800/80 dark:text-red-200 text-red-100':
             type === EnvironmentType.PRODUCTION,
         }),
         className
