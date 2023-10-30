815d9a4f09b39c71dfb16e7e876d6fb1a211b31a
dimak <rahbussiness@gmail.com>
2023-10-30 17:45:22 +0100

    props

--- src/components/users/TableToolbar.tsx
+++ src/components/users/TableToolbar.tsx
@@ -6,6 +6,7 @@ import { Input } from '../ui/input'
 import { Skeleton } from '../ui/skeleton'
 
 interface Props {
+  isInvitations?: boolean
   userCount: number | null
   isSearchCount?: boolean
   loading: boolean
@@ -15,6 +16,7 @@ interface Props {
 }
 
 const TableToolbar: React.FC<Props> = ({
+  isInvitations,
   userCount,
   search,
   isSearchCount,
@@ -32,35 +34,47 @@ const TableToolbar: React.FC<Props> = ({
             </>
           ) : (
             <div className="text-muted-foreground font-medium">
-              {!isSearchCount ? 'Total users' : 'Found users'}: {userCount}
+              {!isInvitations && (
+                <>
+                  {!isSearchCount ? 'Total users' : 'Found users'}: {userCount}
+                </>
+              )}
+
+              {isInvitations && (
+                <>
+                  {!isSearchCount ? 'Total invitations' : 'Found invitations'}: {userCount}
+                </>
+              )}
             </div>
           )}
         </div>
 
         <div className="flex items-center w-full md:w-fit justify-end gap-3">
-          <div className="relative w-full md:w-[12rem] lg:w-[16rem]">
-            <Icons.search className="h-4 w-4 pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-3" />
-            {search?.length > 0 && (
-              <button
-                className="absolute top-1/2 transform -translate-y-1/2 right-4 opacity-60 hover:opacity-100"
-                onClick={() => onSearch('')}
-              >
-                <Icons.x className="h-4 w-4" />
-              </button>
-            )}
+          {!isInvitations && (
+            <div className="relative w-full md:w-[12rem] lg:w-[16rem]">
+              <Icons.search className="h-4 w-4 pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-3" />
+              {search?.length > 0 && (
+                <button
+                  className="absolute top-1/2 transform -translate-y-1/2 right-4 opacity-60 hover:opacity-100"
+                  onClick={() => onSearch('')}
+                >
+                  <Icons.x className="h-4 w-4" />
+                </button>
+              )}
 
-            <Input
-              // readOnly={loading}
-              placeholder="Search by name or email"
-              className="pl-10 pr-10 -mr-10"
-              value={search ?? undefined}
-              onChange={(e) => onSearch(e.target.value)}
-            />
-          </div>
+              <Input
+                // readOnly={loading}
+                placeholder="Search by name or email"
+                className="pl-10 pr-10 -mr-10"
+                value={search ?? undefined}
+                onChange={(e) => onSearch(e.target.value)}
+              />
+            </div>
+          )}
 
-          <Button size={'sm'} onClick={onInviteUser}>
-            <Icons.plus />
-            <span className="md:block hidden">Add user</span>
+          <Button size={'sm'} onClick={onInviteUser} className="flex gap-1.5">
+            <Icons.plus className="h-5 w-5" />
+            <span className="md:block hidden">Invite user</span>
           </Button>
         </div>
       </div>
