d9dc6118f6a669141b97bb42d2b632c9470f787d
dimak <rahbussiness@gmail.com>
2023-10-31 18:55:18 +0100

    conditions

--- src/components/users/TableToolbar.tsx
+++ src/components/users/TableToolbar.tsx
@@ -50,27 +50,25 @@ const TableToolbar: React.FC<Props> = ({
         </div>
 
         <div className="flex items-center w-full md:w-fit justify-end gap-3">
-          {!isInvitations && (
-            <div className="relative w-full md:w-[12rem] lg:w-[16rem]">
-              <Icons.search className="h-4 w-4 pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-3" />
-              {search?.length > 0 && (
-                <button
-                  className="absolute top-1/2 transform -translate-y-1/2 right-4 opacity-60 hover:opacity-100"
-                  onClick={() => onSearch('')}
-                >
-                  <Icons.x className="h-4 w-4" />
-                </button>
-              )}
+          <div className="relative w-full md:w-[12rem] lg:w-[16rem]">
+            <Icons.search className="h-4 w-4 pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-3" />
+            {search?.length > 0 && (
+              <button
+                className="absolute top-1/2 transform -translate-y-1/2 right-4 opacity-60 hover:opacity-100"
+                onClick={() => onSearch('')}
+              >
+                <Icons.x className="h-4 w-4" />
+              </button>
+            )}
 
-              <Input
-                // readOnly={loading}
-                placeholder="Search by name or email"
-                className="pl-10 pr-10 -mr-10"
-                value={search ?? undefined}
-                onChange={(e) => onSearch(e.target.value)}
-              />
-            </div>
-          )}
+            <Input
+              // readOnly={loading}
+              placeholder={isInvitations ? "Search by email" : "Search by name or email"}
+              className="pl-10 pr-10 -mr-10"
+              value={search ?? undefined}
+              onChange={(e) => onSearch(e.target.value)}
+            />
+          </div>
 
           <Button size={'sm'} onClick={onInviteUser} className="flex gap-1.5">
             <Icons.plus className="h-5 w-5" />
