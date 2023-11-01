b42257483c7036be93544c9267ba5a20c75728ca
dimak <rahbussiness@gmail.com>
2023-11-01 19:33:20 +0100

    get teams req

---
+++ src/api/requests/teams.ts
@@ -0,0 +1,64 @@
+import { ListTeam } from '@/types/teams'
+import sendRequest, { APIError } from '../instance'
+import { User, WorkspaceInvitation, WorkspaceUser, WorkspaceUserRole } from '@/types/users'
+
+type UsersErrorCode =
+  | 'workspace_not_found'
+  | 'user_not_found'
+  | 'team_already_exists'
+  | 'team_not_found'
+export type UsersError<T extends UsersErrorCode | void> = APIError<T>
+
+export function usersErrorMsgFromCode(code: UsersErrorCode): string {
+  let msg = ''
+
+  if (code === 'workspace_not_found') {
+    msg = 'Workspace has been deleted'
+  }
+
+  if (code === 'user_not_found') {
+    msg = 'User not found in current workspace'
+  }
+
+  if (code === 'team_already_exists') {
+    msg = 'Team already exists'
+  }
+
+  if (code === 'team_not_found') {
+    msg = 'Team not found'
+  }
+
+  return msg
+}
+
+// NOTE: requests
+export type GetTeamsError = UsersError<undefined>
+export type GetTeamsData = ListTeam[]
+
+export type GetTeamsArgs = {
+  workspaceId: string
+  // pageSize?: number
+  // page?: number
+  // sort?: 'name' | 'email' | 'joined' | 'role'
+  // desc?: boolean
+  // search?: string
+}
+
+export async function getTeams(args: GetTeamsArgs) {
+  const { workspaceId } = args
+
+  const response = sendRequest<GetTeamsData>({
+    method: 'GET',
+    basePath: `workspaces`,
+    path: `${workspaceId}/teams`,
+    //   params: {
+    //     page,
+    //     pageSize,
+    //     sort,
+    //     desc,
+    //     search,
+    //   },
+  })
+
+  return await response
+}
