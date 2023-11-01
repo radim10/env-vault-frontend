363e32253be3d890e2a6314ac18985dd2614dc2e
dimak <rahbussiness@gmail.com>
2023-11-01 19:33:56 +0100

    show teams component

--- src/app/(dashboard)/workspace/[workspace]/users/[tab]/page.tsx
+++ src/app/(dashboard)/workspace/[workspace]/users/[tab]/page.tsx
@@ -1,4 +1,5 @@
-import WorkspaceInvitations from '@/components/users/WorkspaceInvitations';
+import WorkspaceTeams from '@/components/teams/WorkspaceTeams'
+import WorkspaceInvitations from '@/components/users/WorkspaceInvitations'
 import WorkspaceUsers from '@/components/users/WorkspaceUsers'
 import { Metadata } from 'next'
 
@@ -13,9 +14,11 @@ export default function UsersPage({
   params: { workspace: string; tab: string }
   searchParams: { [key: string]: string | string[] | undefined }
 }) {
-  return <>
-    {params.tab === 'workspace' && <WorkspaceUsers workspaceId={params.workspace} />}
-    {params.tab === 'invitations' && <WorkspaceInvitations workspaceId={params.workspace} />}
-
-  </>
+  return (
+    <>
+      {params.tab === 'workspace' && <WorkspaceUsers workspaceId={params.workspace} />}
+      {params.tab === 'invitations' && <WorkspaceInvitations workspaceId={params.workspace} />}
+      {params.tab === 'teams' && <WorkspaceTeams workspaceId={params.workspace} />}
+    </>
+  )
 }
