880a12456ace7beac41c29e65352a7067e7e210c
dimak <rahbussiness@gmail.com>
2023-11-01 19:33:38 +0100

    WorkspaceTeams component

---
+++ src/components/teams/WorkspaceTeams.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useGetTeams } from '@/api/queries/teams'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { Icons } from '../icons'
+import TeamsTable from './table/TeamsTable'
+import { teamsColumns } from './table/Columns'
+import { useQueryClient } from '@tanstack/react-query'
+import CreateTeamDrawer from './CreateTeamDrawer'
+
+interface Props {
+  workspaceId: string
+}
+
+const WorkspaceTeams: React.FC<Props> = ({ workspaceId }) => {
+  const queryClient = useQueryClient()
+  const [createdDrawerOpened, setCreatedDrawerOpened] = useState(false)
+
+  return (
+    <div>
+      <CreateTeamDrawer
+        opened={createdDrawerOpened}
+        onClose={() => setCreatedDrawerOpened(false)}
+      />
+      <TeamsTable
+        onCreateTeam={() => setCreatedDrawerOpened(true)}
+        workspaceId={workspaceId}
+        columns={teamsColumns}
+        queryClient={queryClient}
+      />
+    </div>
+  )
+}
+
+export default WorkspaceTeams
