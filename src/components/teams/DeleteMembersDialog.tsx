6ec7d0ebca5fd6276a9ac5241467ae1c9325d7d8
dimak <rahbussiness@gmail.com>
2023-11-03 18:35:58 +0100

    DeleteMembers dialog

---
+++ src/components/teams/DeleteMembersDialog.tsx
@@ -0,0 +1,68 @@
+import { useUpdateEffect } from 'react-use'
+import DialogComponent from '../Dialog'
+import { useUpdateTeamMembers } from '@/api/mutations/teams'
+import { teamsErrorMsgFromCode } from '@/api/requests/teams'
+
+interface Props {
+  workspaceId: string
+  teamId: string
+  opened: boolean
+  userIds: string[]
+  onClose: () => void
+  onSuccess: () => void
+}
+
+const DeleteMembersDialog: React.FC<Props> = ({
+  workspaceId,
+  teamId,
+  userIds,
+  opened,
+  onClose,
+  onSuccess,
+}) => {
+  const {
+    mutate: deleteUsers,
+    isLoading,
+    error,
+    reset,
+  } = useUpdateTeamMembers({
+    onSuccess: () => onSuccess(),
+  })
+
+  useUpdateEffect(() => {
+    if (!opened && error) {
+      setTimeout(() => reset(), 150)
+    }
+  }, [opened])
+
+  return (
+    <DialogComponent
+      opened={opened}
+      title={'Delete users'}
+      submit={{
+        text: 'Delete',
+        disabled: false,
+        wFull: true,
+        variant: 'destructive',
+      }}
+      error={
+        error
+          ? error?.code
+            ? teamsErrorMsgFromCode(error.code)
+            : 'Something went wrong'
+          : undefined
+      }
+      descriptionComponent={
+        <span>
+          Are you sure you want to remove <span className="font-bold">{userIds?.length}</span> users
+          from this team?
+        </span>
+      }
+      loading={isLoading}
+      onSubmit={() => deleteUsers({ workspaceId, teamId, data: { removed: userIds } })}
+      onClose={onClose}
+    />
+  )
+}
+
+export default DeleteMembersDialog
