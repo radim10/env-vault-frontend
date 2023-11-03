3e3ff50cf53e1dae3cc9dcf2071274f94de350e5
dimak <rahbussiness@gmail.com>
2023-11-03 14:29:01 +0100

    state

--- src/components/teams/TeamMembers.tsx
+++ src/components/teams/TeamMembers.tsx
@@ -4,6 +4,8 @@ import { useQueryClient } from '@tanstack/react-query'
 import { useState } from 'react'
 import TeamMembersTable from './membersTable/Table'
 import { columns } from './membersTable/Columns'
+import CreateTeamDrawer from './CreateTeamDrawer'
+import { useToast } from '../ui/use-toast'
 
 interface Props {
   workspaceId: string
@@ -11,19 +13,44 @@ interface Props {
 }
 
 const TeamMembers: React.FC<Props> = ({ workspaceId, teamId }) => {
+  const { toast } = useToast()
   const queryClient = useQueryClient()
-  const [inviteUserDialog, setInviteUserDialog] = useState(false)
+  const [addMembersDrawerOpened, setAddMembersDrawerOpened] = useState(false)
+
+  const handleAddedMembers = () => {
+    queryClient.invalidateQueries(['workspace', workspaceId, 'team-members', teamId], {
+      exact: false,
+    })
+
+    toast({
+      title: 'Team members have been added',
+      variant: 'success',
+    })
+
+    setAddMembersDrawerOpened(false)
+  }
 
   return (
-    <div className="flex flex-col gap-6">
-      <TeamMembersTable
-        workspaceId={workspaceId}
-        teamId={teamId}
+    <>
+      <CreateTeamDrawer
         queryClient={queryClient}
-        columns={columns}
-        onInviteUser={() => {}}
+        teamId={teamId}
+        workspaceId={workspaceId}
+        opened={addMembersDrawerOpened}
+        onClose={() => setAddMembersDrawerOpened(false)}
+        onCreated={() => {}}
+        onAddedMembers={handleAddedMembers}
       />
-    </div>
+      <div className="flex flex-col gap-6">
+        <TeamMembersTable
+          workspaceId={workspaceId}
+          teamId={teamId}
+          queryClient={queryClient}
+          columns={columns}
+          onAddMembers={() => setAddMembersDrawerOpened(true)}
+        />
+      </div>
+    </>
   )
 }
 
