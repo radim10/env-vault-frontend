73ace1ece158c69a494317db7bc9037895aae279
dimak <rahbussiness@gmail.com>
2023-11-03 14:19:16 +0100

    add team members

--- src/api/mutations/teams.ts
+++ src/api/mutations/teams.ts
@@ -1,4 +1,13 @@
-import { CreateTeamArgs, CreateTeamError, CreateTeamResData, createTeam } from '../requests/teams'
+import {
+  AddTeamMembersArgs,
+  AddTeamMembersError,
+  AddTeamMembersResData,
+  CreateTeamArgs,
+  CreateTeamError,
+  CreateTeamResData,
+  addTeamMembers,
+  createTeam,
+} from '../requests/teams'
 import { MutOpt } from './mutOpt'
 import { useMutation } from '@tanstack/react-query'
 
@@ -7,3 +16,11 @@ type UseCreateTeamVariables = CreateTeamArgs
 
 export const useCreateTeam = (opt?: MutOpt<CreateTeamResData>) =>
   useMutation<CreateTeamResData, CreateTeamError, UseCreateTeamVariables>(createTeam, opt)
+
+// add team members
+type UseAddTeamMembersArgs = AddTeamMembersArgs
+export const useAddTeamMembers = (opt?: MutOpt<AddTeamMembersResData>) =>
+  useMutation<AddTeamMembersResData, AddTeamMembersError, UseAddTeamMembersArgs>(
+    addTeamMembers,
+    opt
+  )
