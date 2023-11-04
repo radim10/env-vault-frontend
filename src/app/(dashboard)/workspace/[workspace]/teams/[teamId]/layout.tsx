a48b0918ccb83e8441fd2c73609425d99665b85e
dimak <rahbussiness@gmail.com>
2023-11-04 11:26:48 +0100

    style

--- src/app/(dashboard)/workspace/[workspace]/teams/[teamId]/layout.tsx
+++ src/app/(dashboard)/workspace/[workspace]/teams/[teamId]/layout.tsx
@@ -5,8 +5,6 @@ import { Icons } from '@/components/icons'
 import NotFound from '@/components/projects/NotFound'
 import ProjectSkeleton from '@/components/projects/ProjectSkeleton'
 import TeamTabs from '@/components/teams/TeamTabs'
-import TypographyH2 from '@/components/typography/TypographyH2'
-import UsersTabs from '@/components/users/UsersTabs'
 import clsx from 'clsx'
 import Error from '@/components/Error'
 import Link from 'next/link'
@@ -71,29 +69,36 @@ export default function TeamLayout({
 
   return (
     <>
-      <div>
-        <div className="flex justify-between items-center px-6 lg:px-10 mt-1 pb-2">
-          <div className="flex gap-2 items-center">
-            <Link
-              href={`/workspace/${params.workspace}/users/teams`}
-              className="text-primary hover:text-primary hover:underline underline-offset-4 underline-offset-[6px] hover:decoration-2"
-            >
-              <div className="font-semibold text-2xl">Teams</div>
-            </Link>
-            {/* // */}
-            <div className="dark:text-gray-400">
-              <Icons.chevronRight className="mt-1" />
-            </div>
-            <div className="font-semibold text-2xl ">{data?.name}</div>
-          </div>
-          {/* // FLEX END */}
-        </div>
-        {data?.description && (
-          <div className="text-muted-foreground px-6 lg:px-10 line-clamp-1 md:max-w-[50%]">
-            {data?.description}
-          </div>
+      <div
+        className={clsx(
+          [
+            'px-6 lg:px-10  backdrop-blur-xl flex md:flex-row flex-col justify-between md:items-center gap-0 md:gap-0 -mt-1 sticky top-0 bg-transparent pb-2 pt-2 w-full z-10',
+          ],
+          {
+            'border-b-2': y > 108,
+          }
         )}
+      >
+        <div className="flex gap-2 items-center">
+          <Link
+            href={`/workspace/${params.workspace}/users/teams`}
+            className="text-primary hover:text-primary hover:underline underline-offset-4 underline-offset-[6px] hover:decoration-2"
+          >
+            <div className="font-semibold text-2xl">Teams</div>
+          </Link>
+          {/* // */}
+          <div className="dark:text-gray-400">
+            <Icons.chevronRight className="mt-1" />
+          </div>
+          <div className="font-semibold text-2xl ">{selectedTeam?.data?.name}</div>
+        </div>
+        {/* // FLEX END */}
       </div>
+      {selectedTeam?.data?.description && (
+        <div className="text-muted-foreground px-6 lg:px-10 line-clamp-1 md:max-w-[50%]">
+          {selectedTeam?.data?.description}
+        </div>
+      )}
 
       <div className="mt-5">
         <TeamTabs workspaceId={params?.workspace} team={params.teamId} />
