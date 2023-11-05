880b3f57c7f39d67560ab7b56de14e0baf6ecc7d
dimak <rahbussiness@gmail.com>
2023-11-05 21:21:59 +0100

    show table

--- src/components/projectAccess/teams/AccessTeams.tsx
+++ src/components/projectAccess/teams/AccessTeams.tsx
@@ -1,5 +1,27 @@
-const AccessTeams = (props: {}) => {
-  return <>Teams</>
+'use client'
+
+import { useGetProjectAccessTeams } from '@/api/queries/projectAccess'
+import { userTeamsColumns } from '@/components/singleUser/table/UserTeamsColumns'
+import AccessTeamsTable from './table/AccessTeamsTable'
+import { accessTeamsColumns } from './table/AccessTeamsColumns'
+import TypographyH4 from '@/components/typography/TypographyH4'
+
+interface Props {
+  workspaceId: string
+  projectName: string
+}
+
+const AccessTeams: React.FC<Props> = ({ workspaceId, projectName }) => {
+  return (
+    <>
+      <TypographyH4>Teams</TypographyH4>
+      <AccessTeamsTable
+        workspaceId={workspaceId}
+        projectName={projectName}
+        columns={accessTeamsColumns}
+      />
+    </>
+  )
 }
 
 export default AccessTeams
