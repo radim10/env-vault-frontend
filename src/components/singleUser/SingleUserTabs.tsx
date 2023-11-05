2a548d5816090fab60f1c000e53ea6dcf11b84c3
dimak <rahbussiness@gmail.com>
2023-11-05 12:44:52 +0100

    hrefs

--- src/components/singleUser/SingleUserTabs.tsx
+++ src/components/singleUser/SingleUserTabs.tsx
@@ -9,19 +9,19 @@ interface Props {
 }
 
 const items = [
-  { label: 'Profile', href: 'details' },
+  { label: 'Profile', href: 'profile' },
   { label: 'Access', href: 'access' },
   { label: 'Teams', href: 'teams' },
 ]
 
 const SingleUserTabs: React.FC<Props> = ({ workspaceId }) => {
-  const params = useParams<{ workspace: string; projectName: string; env: string; tab?: string }>()
+  const params = useParams<{ workspace: string; userId: string; tab: string }>()
 
   return (
     <div className="px-6 lg:px-10 dark:text-gray-400 flex items-center gap-2 md:gap-4 overflow-y-auto text-[1rem] border-b-[1px] pb-0 md:px-3">
       {items.map(({ label, href }, index) => (
         <Link
-          href={`/workspace/${workspaceId}/teams/${href}`}
+          href={`/workspace/${workspaceId}/user/${params.userId}/${href}`}
           className={clsx(
             [
               'border-b-[3px] flex items-center gap-2 h-full hover:dark:text-gray-200 ease duration-150 px-2 lg:px-3 pb-3',
