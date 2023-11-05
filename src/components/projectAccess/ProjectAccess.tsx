81a27c0a06868088b7fc4a5666c3a13341f3bc7b
dimak <rahbussiness@gmail.com>
2023-11-05 21:21:45 +0100

    layout

--- src/components/projectAccess/ProjectAccess.tsx
+++ src/components/projectAccess/ProjectAccess.tsx
@@ -15,26 +15,20 @@ const ProjectAccess: React.FC<Props> = ({ projectName, workspaceId }) => {
 
   return (
     <div className="mt-4 flex flex-col gap-3 px-6 lg:px-10">
-      <div className="py-2 pl-4 pr-2 flex items-center justify-between rounded-md border w-full">
-        <div className="dark:text-gray-400 text-gray-700 font-bold">Edit access</div>
-        <div>
-          <Button variant="default" size={'sm'}>
-            Edit user access
-          </Button>
-        </div>
-      </div>
+      {/* <div className="py-2 pl-4 pr-2 flex items-center justify-between rounded-md border w-full"> */}
+      {/*   <div className="dark:text-gray-400 text-gray-700 font-bold">Edit access</div> */}
+      {/*   <div> */}
+      {/*     <Button variant="default" size={'sm'}> */}
+      {/*       Edit user access */}
+      {/*     </Button> */}
+      {/*   </div> */}
+      {/* </div> */}
+      {/**/}
 
-      <div>TABLE WITH USERS</div>
+      {/* <div>TABLE WITH USERS</div> */}
       <div>
-        <AccessTeams />
+        <AccessTeams workspaceId={workspaceId} projectName={projectName} />
       </div>
-      <AddTeamAccessDrawer
-        workspaceId={workspaceId}
-        opened={false}
-        onClose={function (): void {
-          throw new Error('Function not implemented.')
-        }}
-      />
     </div>
   )
 }
