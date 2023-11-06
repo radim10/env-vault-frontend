73aca243e6627de2e9ad7b5754925b9ec6f06851
dimak <rahbussiness@gmail.com>
2023-11-06 20:04:26 +0100

    select team role

--- src/components/projectAccess/teams/AddTeamAccessDrawer.tsx
+++ src/components/projectAccess/teams/AddTeamAccessDrawer.tsx
@@ -6,7 +6,20 @@ import { ListTeam } from '@/types/teams'
 import { useQueryClient } from '@tanstack/react-query'
 import TeamsSearchCombobox from '@/components/teams/TeamsSearchCombobox'
 import { useUpdateProjectAccessTeams } from '@/api/mutations/projectAccess'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { ProjectRole } from '@/types/projectAccess'
+import UserRoleBadge from '@/components/users/UserRoleBadge'
+import { WorkspaceUserRole } from '@/types/users'
+import { Label } from '@/components/ui/label'
 import { UpdateProjectAccessTeamsData } from '@/api/requests/projectAccess'
+
+const roles: ProjectRole[] = [ProjectRole.MEMBER, ProjectRole.ADMIN, ProjectRole.OWNER]
 
 interface Props {
   workspaceId: string
@@ -25,6 +38,7 @@ const AddTeamAccessDrawer: React.FC<Props> = ({
 }) => {
   const queryClient = useQueryClient()
   const [selectedTeams, setSelectedTeams] = useState<ListTeam[]>([])
+  const [selectedRole, setSelectedRole] = useState<ProjectRole>(ProjectRole.MEMBER)
 
   const { mutate, isLoading, error } = useUpdateProjectAccessTeams({
     onSuccess: () => {
@@ -45,8 +59,12 @@ const AddTeamAccessDrawer: React.FC<Props> = ({
           loading: isLoading,
           onSubmit: () => {
             const teamIds = selectedTeams.map((team) => team.id)
+
             const payload: UpdateProjectAccessTeamsData = {
-              add: teamIds,
+              add: {
+                ids: teamIds,
+                role: selectedRole,
+              },
             }
 
             mutate({
@@ -57,7 +75,32 @@ const AddTeamAccessDrawer: React.FC<Props> = ({
           },
         }}
       >
-        <div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <div>
+              <Label htmlFor="Role" className="">
+                <span className="">Project role</span>
+              </Label>
+            </div>
+            <Select onValueChange={(value) => setSelectedRole(value as ProjectRole)}>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select role" />
+              </SelectTrigger>
+              <SelectContent>
+                {roles.map((role) => (
+                  <SelectItem
+                    value={role}
+                    key={role}
+                    className="px-10"
+                    onFocus={(e) => e.stopPropagation()}
+                  >
+                    <UserRoleBadge role={role as any as WorkspaceUserRole} className="px-4" />
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+
           <TeamsSearchCombobox
             queryClient={queryClient}
             onSelect={(teams) => {
