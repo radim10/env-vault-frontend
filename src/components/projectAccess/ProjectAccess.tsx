066af33151ca6bcea6919e68292bccd2169d452a
dimak <rahbussiness@gmail.com>
2023-11-06 13:14:00 +0100

    layout

--- src/components/projectAccess/ProjectAccess.tsx
+++ src/components/projectAccess/ProjectAccess.tsx
@@ -4,6 +4,8 @@ import { useQueryClient } from '@tanstack/react-query'
 import { Button } from '../ui/button'
 import AccessTeams from './teams/AccessTeams'
 import AddTeamAccessDrawer from './teams/AddTeamAccessDrawer'
+import AccessUsers from './users/AccessUsers'
+import { Separator } from '../ui/separator'
 
 interface Props {
   workspaceId: string
@@ -14,7 +16,7 @@ const ProjectAccess: React.FC<Props> = ({ projectName, workspaceId }) => {
   const queryClient = useQueryClient()
 
   return (
-    <div className="mt-4 flex flex-col gap-3 px-6 lg:px-10">
+    <div className="mt-4 flex flex-col gap-6 px-6 lg:px-10">
       {/* <div className="py-2 pl-4 pr-2 flex items-center justify-between rounded-md border w-full"> */}
       {/*   <div className="dark:text-gray-400 text-gray-700 font-bold">Edit access</div> */}
       {/*   <div> */}
@@ -26,7 +28,14 @@ const ProjectAccess: React.FC<Props> = ({ projectName, workspaceId }) => {
       {/**/}
 
       {/* <div>TABLE WITH USERS</div> */}
+
       <div>
+        <AccessUsers workspaceId={workspaceId} projectName={projectName} />
+      </div>
+
+      <Separator />
+
+      <div className="">
         <AccessTeams workspaceId={workspaceId} projectName={projectName} />
       </div>
     </div>
