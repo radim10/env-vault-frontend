c47aa73db288156643681f76f9d6640345ab4933
dimak <rahbussiness@gmail.com>
2023-11-06 21:11:15 +0100

    removed imports

--- src/components/projectAccess/teams/AccessTeams.tsx
+++ src/components/projectAccess/teams/AccessTeams.tsx
@@ -1,10 +1,7 @@
 'use client'
 
-import { useGetProjectAccessTeams } from '@/api/queries/projectAccess'
-import { userTeamsColumns } from '@/components/singleUser/table/UserTeamsColumns'
 import AccessTeamsTable from './table/AccessTeamsTable'
 import { accessTeamsColumns } from './table/AccessTeamsColumns'
-import TypographyH4 from '@/components/typography/TypographyH4'
 
 interface Props {
   workspaceId: string
