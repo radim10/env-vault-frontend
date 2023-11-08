28ef4497e9f8602405c2816afb700d94ab7ed3c1
dimak <rahbussiness@gmail.com>
2023-11-08 13:15:45 +0100

    change role meta

--- src/components/projectAccess/teams/table/AccessTeamsColumns.tsx
+++ src/components/projectAccess/teams/table/AccessTeamsColumns.tsx
@@ -6,9 +6,10 @@ import { Button } from '@/components/ui/button'
 import { Icons } from '@/components/icons'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import clsx from 'clsx'
-import { ProjectAccessTeam } from '@/types/projectAccess'
+import { ProjectAccessTeam, ProjectRole } from '@/types/projectAccess'
 import UserRoleBadge from '@/components/users/UserRoleBadge'
 import { WorkspaceUserRole } from '@/types/users'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 
 dayjs.extend(relativeTime)
 
@@ -110,29 +111,62 @@ export const accessTeamsColumns: ColumnDef<ProjectAccessTeam>[] = [
       const meta = table.options.meta as {
         goto: (id: string) => void
         delete: (args: { id: string; name: string }) => void
+        changeRole: (args: {
+          id: string
+          name: string
+          role: ProjectRole
+          membersCount: number
+        }) => void
       }
 
       return (
         <div className="w-full flex justify-start items-center gap-1.5 bg-red-400X">
-          <Button
-            size="sm"
-            variant={'ghost'}
-            onClick={() => meta.goto(row.original.id)}
-            className="opacity-70 hover:opacity-100"
-          >
-            <Icons.arrowRight className="h-4 w-4" />
-          </Button>
+          <TooltipProvider>
+            <Button
+              size="sm"
+              variant={'ghost'}
+              onClick={() => meta.goto(row.original.id)}
+              className="opacity-70 hover:opacity-100"
+            >
+              <Icons.arrowRight className="h-4 w-4" />
+            </Button>
 
-          <Button
-            onClick={() => meta.delete({ id: row.original.id, name: row.original.name })}
-            size={'sm'}
-            variant="ghost"
-            className={clsx([
-              'opacity-70 hover:opacity-100 dark:hover:text-red-500 Xdark:text-red-500 xtext-red-600 hover:text-red-600',
-            ])}
-          >
-            <Icons.trash className="h-4 w-4" />
-          </Button>
+            {/* // change role */}
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  size="sm"
+                  variant={'ghost'}
+                  disabled={false}
+                  onClick={() => {
+                    meta.changeRole({
+                      id: row.original.id,
+                      name: row.original.name,
+                      membersCount: row.original.membersCount,
+                      role: row.original.role,
+                    })
+                  }}
+                  className={clsx([''], {
+                    'opacity-70 hover:opacity-100': true,
+                  })}
+                >
+                  <Icons.userCog className="h-4 w-4" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Change role</TooltipContent>
+            </Tooltip>
+
+            <Button
+              onClick={() => meta.delete({ id: row.original.id, name: row.original.name })}
+              size={'sm'}
+              variant="ghost"
+              className={clsx([
+                'opacity-70 hover:opacity-100 dark:hover:text-red-500 Xdark:text-red-500 xtext-red-600 hover:text-red-600',
+              ])}
+            >
+              <Icons.trash className="h-4 w-4" />
+            </Button>
+          </TooltipProvider>
         </div>
       )
     },
