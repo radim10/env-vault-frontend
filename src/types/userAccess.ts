1e579cb4287b9cd411528bb0100be52342a911c5
dimak <rahbussiness@gmail.com>
2023-11-08 17:16:57 +0100

    UserAccessTeamProject type

--- src/types/userAccess.ts
+++ src/types/userAccess.ts
@@ -4,3 +4,9 @@ import { Project } from './projects'
 export type UserAccessProject = Pick<Project, 'name'> & {
   role: ProjectRole
 }
+
+export type UserAccessTeamProject = {
+  name: string
+  role: ProjectRole
+  teamName: string
+}
