2a539e5f05f8c4817c7797b9273b81f28e5b8321
dimak <rahbussiness@gmail.com>
2023-11-08 20:12:25 +0100

    table footer - className prop

--- src/components/tables/TableFooter.tsx
+++ src/components/tables/TableFooter.tsx
@@ -1,9 +1,11 @@
+import { cn } from '@/lib/utils'
 import TablePageSizeSelect, { TablePageSizeProps } from './TablePageSize'
 import TablePagination, { TablePaginationProps } from './TablePagination'
 
 interface Props {
   pagination: TablePaginationProps
   pageSize: TablePageSizeProps
+  className?: string
   page: {
     current: number
     total: number
@@ -11,9 +13,9 @@ interface Props {
   }
 }
 
-const TableFooter: React.FC<Props> = ({ pagination, pageSize, page }) => {
+const TableFooter: React.FC<Props> = ({ className, pagination, pageSize, page }) => {
   return (
-    <div className="flex justify-end items-center gap-4 md:gap-6">
+    <div className={cn('flex justify-end items-center gap-4 md:gap-6', className)}>
       {page && !page?.hidden && (
         <span className="flex items-center gap-1 text-sm text-muted-foreground ease duration-200">
           <div>Page</div>
