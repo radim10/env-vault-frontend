1e3c34d6b8dfe2655a22ea7b59bff36a1db0d911
dimak <rahbussiness@gmail.com>
2023-11-08 20:27:31 +0100

    gap

--- src/components/projectAccess/ProjectAccess.tsx
+++ src/components/projectAccess/ProjectAccess.tsx
@@ -16,7 +16,7 @@ const ProjectAccess: React.FC<Props> = ({ projectName, workspaceId }) => {
   const queryClient = useQueryClient()
 
   return (
-    <div className="mt-4 flex flex-col gap-6 px-6 lg:px-10">
+    <div className="mt-4 flex flex-col gap-6 md:gap-8 px-6 lg:px-10">
       {/* <div className="py-2 pl-4 pr-2 flex items-center justify-between rounded-md border w-full"> */}
       {/*   <div className="dark:text-gray-400 text-gray-700 font-bold">Edit access</div> */}
       {/*   <div> */}
@@ -33,7 +33,7 @@ const ProjectAccess: React.FC<Props> = ({ projectName, workspaceId }) => {
         <AccessUsers workspaceId={workspaceId} projectName={projectName} />
       </div>
 
-      <Separator />
+      {/* <Separator /> */}
 
       <div className="">
         <AccessTeams workspaceId={workspaceId} projectName={projectName} />
