73ab02fdbbd6ece831613c9e3b8088447b5100e3
dimak <rahbussiness@gmail.com>
2023-11-08 21:18:40 +0100

    TeamAccess component

---
+++ src/components/teams/TeamAccess.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  workspaceId: string
+  teamId: string
+}
+
+const TeamAccess: React.FC<Props> = ({ workspaceId, teamId }) => {
+  return <>Team access</>
+}
+
+export default TeamAccess
