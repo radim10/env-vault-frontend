b7cd97083b412afedde4e2541c49cf731587a613
dimak <rahbussiness@gmail.com>
2023-11-09 13:39:20 +0100

    TeamProjects table component

---
+++ src/components/teams/projectsTable/TeamProjectsTable.tsx
@@ -0,0 +1,287 @@
+'use client'
+
+import clsx from 'clsx'
+import { produce } from 'immer'
+import { useMemo, useState } from 'react'
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  PaginationState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Skeleton } from '@/components/ui/skeleton'
+import { ListTeam, TeamProjectAccess } from '@/types/teams'
+import { useRouter } from 'next/navigation'
+import TableFooter from '@/components/tables/TableFooter'
+import { useQueryClient } from '@tanstack/react-query'
+import { useToast } from '@/components/ui/use-toast'
+import TableToolbar from '@/components/users/TableToolbar'
+import { useGetUserAccessProjects, useGetUserAccessTeamProjects } from '@/api/queries/users'
+import { UserAccessProject, UserAccessTeamProject } from '@/types/userAccess'
+import TypographyH4 from '@/components/typography/TypographyH4'
+import { Icons } from '@/components/icons'
+import { useGetTeamProjects } from '@/api/queries/teams'
+
+interface Props {
+  workspaceId: string
+  teamId: string
+  columns: ColumnDef<TeamProjectAccess>[]
+}
+
+const TeamProjectsTable: React.FC<Props> = ({ columns, teamId, workspaceId }) => {
+  const { toast } = useToast()
+  const queryClient = useQueryClient()
+
+  const router = useRouter()
+  const defaultData = useMemo(() => [], [])
+
+  const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 5,
+  })
+
+  const pagination = useMemo(
+    () => ({
+      pageIndex,
+      pageSize,
+    }),
+    [pageIndex, pageSize]
+  )
+
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+
+  const [sorting, setSorting] = useState<SortingState>([
+    {
+      id: 'name',
+      desc: false,
+    },
+  ])
+
+  const { data, isLoading, error } = useGetTeamProjects({
+    workspaceId,
+    teamId,
+  })
+
+  // TODO:
+  const getCurrentKey = () => ['workspace', workspaceId, 'user', teamId, 'access-user']
+
+  const handleGotoProject = (projectName: string) => {
+    router.push(`/workspace/${workspaceId}/projects/${projectName}/environments`)
+  }
+
+  const handleGotoTeam = (teamId: string) => {
+    router.push(`/workspace/${workspaceId}/teams/${teamId}/members`)
+  }
+
+  const sortTeams = (
+    teams: ListTeam[],
+    sort: 'membersCount' | 'name',
+    desc: boolean
+  ): ListTeam[] => {
+    if (sort === 'membersCount') {
+      return teams.sort(function (a, b) {
+        const countCompare = desc
+          ? b.membersCount - a.membersCount
+          : a.membersCount - b.membersCount
+        return countCompare || a.name.localeCompare(b.name)
+      })
+    } else {
+      return teams.sort(function (a, b) {
+        return desc ? b.name.localeCompare(a.name) : a.name.localeCompare(b.name)
+      })
+    }
+  }
+
+  const table = useReactTable({
+    pageCount: data ? Math.ceil(data?.length / pageSize) : undefined,
+    data: data ?? defaultData,
+    columns,
+    meta: {
+      gotoProject: handleGotoProject,
+      gotoTeam: handleGotoTeam,
+    },
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: (sorting) => {
+      setSorting(sorting)
+    },
+    onPaginationChange: setPagination,
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    enableSorting: !isLoading,
+    state: {
+      sorting,
+      pagination,
+      columnFilters,
+    },
+  })
+
+  return (
+    <div className="">
+      <TableToolbar
+        hideSubmit
+        count={table.getRowModel().rows.length ?? null}
+        loading={isLoading}
+        entity="user-access"
+        disabledSearch={isLoading}
+        isSearchCount={(table.getColumn('name')?.getFilterValue() as string)?.length > 0}
+        search={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+        onSearch={(value) => {
+          table.getColumn('name')?.setFilterValue(value)
+        }}
+      />
+
+      <div className="rounded-md border">
+        <Table>
+          {/* <TableHeader> */}
+          <TableHeader className="bg-gray-100/60 hover:bg-gray-100/60 dark:bg-gray-900/80 hover:dark:bg-gray-900/80 sticky">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header, index) => {
+                  return (
+                    <TableHead
+                      key={header.id}
+                      className={clsx([''], {
+                        'md:w-[45%]': index === 0 || index === 1,
+                        'pl-7 bg-red-400X': table.getRowModel().rows?.length === 0 && !isLoading,
+                      })}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {isLoading ? (
+              <>
+                {Array.from({ length: pageSize }).map((_) => (
+                  <TableRow className="h-16 w-full bg-red-400X hover:bg-transparent">
+                    {Array.from({ length: 3 }).map((_, index) => (
+                      <TableCell
+                        key={index}
+                        className={clsx(['py-2 md:py-3'], {
+                          'md:w-[45%]': index === 0 || index === 1,
+                          'pl-7 bg-red-400X': table.getRowModel().rows?.length === 0 && !isLoading,
+                        })}
+                      >
+                        <Skeleton
+                          className={clsx(['w-full h-6'], {
+                            // 'w-36': index === 1,
+                          })}
+                        />
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </>
+            ) : (
+              <>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row, _) => (
+                    <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                      {row.getVisibleCells().map((cell, index) => (
+                        <TableCell
+                          key={cell.id}
+                          className={clsx(['py-2 md:py-3'], {
+                            'pr-0': index === 0,
+                          })}
+                        >
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <>
+                    {!isLoading && (
+                      <TableRow className="hover:bg-transparent">
+                        <TableCell colSpan={columns.length} className="h-24 text-center">
+                          No teams
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </>
+                )}
+              </>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+
+      <TableFooter
+        className="py-3"
+        pagination={{
+          toStart: {
+            onClick: () => table.setPageIndex(0),
+            disabled:
+              !table.getCanPreviousPage() ||
+              table.getState().pagination.pageIndex + 1 >
+                Math.ceil(table.getFilteredRowModel().rows.length / pageSize),
+          },
+          toEnd: {
+            onClick: () => table.setPageIndex(table.getPageCount() - 1),
+            disabled:
+              !table.getCanNextPage() ||
+              table.getState().pagination.pageIndex + 1 >=
+                Math.ceil(table.getFilteredRowModel().rows.length / pageSize),
+          },
+          prev: {
+            onClick: () => table.previousPage(),
+            disabled:
+              !table.getCanPreviousPage() ||
+              table.getState().pagination.pageIndex + 1 >
+                Math.ceil(table.getFilteredRowModel().rows.length / pageSize),
+          },
+          next: {
+            onClick: () => table.nextPage(),
+            disabled:
+              !table.getCanNextPage() ||
+              table.getState().pagination.pageIndex + 1 >=
+                Math.ceil(table.getFilteredRowModel().rows.length / pageSize),
+          },
+        }}
+        page={{
+          hidden: isLoading || table.getFilteredRowModel().rows.length <= 5,
+          current: table.getState().pagination.pageIndex + 1,
+          total: table.getPageCount(),
+        }}
+        pageSize={{
+          value: pageSize,
+          disabled: isLoading || table.getFilteredRowModel().rows.length <= 5,
+          onChange: (pageSize) => {
+            setPagination((p) => {
+              const currentPage = p.pageIndex + 1
+
+              if (pageSize === 10 && currentPage === table.getPageCount()) {
+                return { pageIndex: p.pageIndex - 1, pageSize }
+              } else {
+                return { pageIndex: p.pageIndex, pageSize }
+              }
+            })
+          },
+        }}
+      />
+    </div>
+  )
+}
+
+export default TeamProjectsTable
