7a59537c3812f8494f06f7bc7f299643e0c6f81b
dimak <rahbussiness@gmail.com>
2023-11-09 20:18:28 +0100

    sheet

--- src/components/secrects/ImportSecretsDrawer.tsx
+++ src/components/secrects/ImportSecretsDrawer.tsx
@@ -12,6 +12,7 @@ import { Textarea } from '../ui/textarea'
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
 import { useUpdateEffect } from 'react-use'
+import Drawer from '../Drawer'
 
 interface Props {
   opened: boolean
@@ -68,53 +69,41 @@ const ImportSecretsDrawer: React.FC<Props> = ({ opened, onClose, onConfirm }) =>
 
   return (
     <>
-      <Sheet
-        open={opened}
-        onOpenChange={(e) => {
-          if (!e) onClose()
+      <Drawer
+        opened={opened}
+        className="w-screen sm:w-[500px] md:w-[650px] lg:w-[940px] h-full md:px-8 md:py-4 py-3 px-4"
+        title="Import secrets"
+        description="Paste secrets in .env format"
+        onClose={() => {
+          onClose()
         }}
       >
-        <SheetContent
-          className="w-screen sm:w-[500px] md:w-[650px] lg:w-[940px] h-full md:px-8 md:py-4 py-3 px-4"
-          onPointerDownOutside={(e) => e.preventDefault()}
-          onEscapeKeyDown={(e) => {
-            e.preventDefault()
-          }}
-        >
-          <SheetHeader>
-            <SheetTitle className="text-[1.1rem]">Import secrets</SheetTitle>
-            <SheetDescription className="text-[0.95rem]">
-              Paste secrets in .env format
-            </SheetDescription>
-
-            <div className="mt-4 md:mt-1">
-              <div className="mb-3 flex flex-col md:flex-row items-center justify-end gap-2 md:gap-4">
-                <Input
-                  id="file"
-                  type="file"
-                  // accept="text/plain, .env, .json"
-                  accept="text/plain, .env"
-                  className="w-full md:w-[40%]"
-                  onChange={handleFileChange}
-                />
-                <Button
-                  className="w-full md:w-fit gap-2"
-                  disabled={value.trim().length === 0}
-                  onClick={() => handleConfirm(value)}
-                >
-                  Confirm import
-                </Button>
-              </div>
-              <Textarea
-                placeholder="MY_SECRET=MY_SECRET_VALUE"
-                className="h-[70vh] resize-none text-[0.95rem]"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-              />
-            </div>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
+        <div className="-mt-3">
+          <div className="mb-3 flex flex-col md:flex-row items-center justify-end gap-2 md:gap-4">
+            <Input
+              id="file"
+              type="file"
+              // accept="text/plain, .env, .json"
+              accept="text/plain, .env"
+              className="w-full md:w-[40%]"
+              onChange={handleFileChange}
+            />
+            <Button
+              className="w-full md:w-fit gap-2"
+              disabled={value.trim().length === 0}
+              onClick={() => handleConfirm(value)}
+            >
+              Confirm import
+            </Button>
+          </div>
+          <Textarea
+            placeholder="MY_SECRET=MY_SECRET_VALUE"
+            className="h-[70vh] resize-none text-[0.95rem]"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
+      </Drawer>
     </>
   )
 }
