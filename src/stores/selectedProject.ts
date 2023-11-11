f6865c638b81d6d196f1dced49960ebc9f5c9aba
dimak <rahbussiness@gmail.com>
2023-11-11 11:42:08 +0100

    store functions

--- src/stores/selectedProject.ts
+++ src/stores/selectedProject.ts
@@ -1,5 +1,5 @@
-import { Project } from '@/types/projects'
 import { create } from 'zustand'
+import { Project } from '@/types/projects'
 import { devtools } from 'zustand/middleware'
 import { immer } from 'zustand/middleware/immer'
 
@@ -12,17 +12,29 @@ export interface SelectedProjectState {
 interface SelectedProjectAction {
   set: (project: SelectedProject) => void
   update: (data: Partial<SelectedProject>) => void
+  isOwnerRole: () => boolean | null
+  isAdminRole: () => boolean | null
+  isMemberRole: () => boolean | null
   reset: () => void
 }
 
 export const useSelectedProjectStore = create(
   devtools(
-    immer<SelectedProjectState & SelectedProjectAction>((set) => ({
+    immer<SelectedProjectState & SelectedProjectAction>((set, get) => ({
       data: null,
       set: (project) => set({ data: project }),
       reset: () => set({ data: null }),
       update: (data) => {
         // TODO :???
+      },
+      isMemberRole: () => {
+        return get()?.data?.userRole === 'MEMBER' ?? null
+      },
+      isAdminRole: () => {
+        return get()?.data?.userRole === 'ADMIN' ?? null
+      },
+      isOwnerRole: () => {
+        return get()?.data?.userRole === 'OWNER' ?? null
       },
     }))
   )
