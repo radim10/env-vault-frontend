54b4b620035f1c7a7eb509a993c6b7716b38d624
dimak <rahbussiness@gmail.com>
2023-11-13 14:08:02 +0100

    role condition

--- src/components/users/WorkspaceInvitations.tsx
+++ src/components/users/WorkspaceInvitations.tsx
@@ -7,6 +7,7 @@ import InvitationsTable from './invitationsTable/InvitationsTable'
 import { useUnmount } from 'react-use'
 import { useInvitationsStore } from '@/stores/invitations'
 import useInvitationsTableColumns from './invitationsTable/Columns'
+import useCurrentUserStore from '@/stores/user'
 
 interface Props {
   workspaceId: string
@@ -16,6 +17,7 @@ const WorkspaceInvitations: React.FC<Props> = ({ workspaceId }) => {
   const queryClient = useQueryClient()
   const [inviteUserDialog, setInviteUserDialog] = useState(false)
   const invitationsStore = useInvitationsStore()
+  const { isMemberRole } = useCurrentUserStore()
 
   useUnmount(() => {
     invitationsStore.reset()
@@ -35,7 +37,7 @@ const WorkspaceInvitations: React.FC<Props> = ({ workspaceId }) => {
         workspaceId={workspaceId}
         columns={useInvitationsTableColumns()}
         queryClient={queryClient}
-        onInviteUser={() => setInviteUserDialog(true)}
+        onInviteUser={isMemberRole() ? undefined : () => setInviteUserDialog(true)}
       />
     </div>
   )
