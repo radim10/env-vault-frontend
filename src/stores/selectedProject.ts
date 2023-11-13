62d482aa731bc408b8e80370be497f7da9a16e14
dimak <rahbussiness@gmail.com>
2023-11-13 14:25:43 +0100

    selected project store

--- src/stores/selectedProject.ts
+++ src/stores/selectedProject.ts
@@ -1,4 +1,4 @@
-import { create, createStore, useStore } from 'zustand'
+import { create } from 'zustand'
 import { Project } from '@/types/projects'
 import { devtools } from 'zustand/middleware'
 import { immer } from 'zustand/middleware/immer'
@@ -18,52 +18,29 @@ interface SelectedProjectAction {
   reset: () => void
 }
 
-const selectedProjectStore = createStore<SelectedProjectState & SelectedProjectAction>(
-  (set, get) => ({
-    data: null,
-    set: (project) => set({ data: project }),
-    reset: () => set({ data: null }),
-    update: (data) => {
-      // TODO :???
-    },
-    isMemberRole: () => {
-      return get()?.data?.userRole === 'MEMBER' ?? null
-    },
-    isAdminRole: () => {
-      return get()?.data?.userRole === 'ADMIN' ?? null
-    },
-    isOwnerRole: () => {
-      return get()?.data?.userRole === 'OWNER' ?? null
-    },
-  })
-)
-
-const { getState, setState } = selectedProjectStore
-export { selectedProjectStore, getState, setState }
-
 // const useSelectedProjectStore = (selector) =>
 //   useStore<SelectedProjectState & SelectedProjectAction>(selectedProjectStore, selector)
 //
 // export { useSelectedProjectStore }
 
-// export const useSelectedProjectStore = create(
-//   devtools(
-//     immer<SelectedProjectState & SelectedProjectAction>((set, get) => ({
-//       data: null,
-//       set: (project) => set({ data: project }),
-//       reset: () => set({ data: null }),
-//       update: (data) => {
-//         // TODO :???
-//       },
-//       isMemberRole: () => {
-//         return get()?.data?.userRole === 'MEMBER' ?? null
-//       },
-//       isAdminRole: () => {
-//         return get()?.data?.userRole === 'ADMIN' ?? null
-//       },
-//       isOwnerRole: () => {
-//         return get()?.data?.userRole === 'OWNER' ?? null
-//       },
-//     }))
-//   )
-// )
+export const useSelectedProjectStore = create(
+  devtools(
+    immer<SelectedProjectState & SelectedProjectAction>((set, get) => ({
+      data: null,
+      set: (project) => set({ data: project }),
+      reset: () => set({ data: null }),
+      update: (data) => {
+        // TODO :???
+      },
+      isMemberRole: () => {
+        return get()?.data?.userRole === 'MEMBER' ?? null
+      },
+      isAdminRole: () => {
+        return get()?.data?.userRole === 'ADMIN' ?? null
+      },
+      isOwnerRole: () => {
+        return get()?.data?.userRole === 'OWNER' ?? null
+      },
+    }))
+  )
+)
