f58fe1134bcf5a00a098499c8f8aacf1ce69457f
dimak <rahbussiness@gmail.com>
2023-11-19 13:32:00 +0100

    get env token query

--- src/api/queries/projects/environments/tokens.ts
+++ src/api/queries/projects/environments/tokens.ts
@@ -1,6 +1,9 @@
 import {
+  GetEnvironmentTokenData,
+  GetEnvironmentTokenError,
   GetEnvironmentTokensData,
   GetEnvironmentTokensError,
+  getEnvironmentToken,
   getEnvironmentTokens,
 } from '@/api/requests/projects/environments/tokens'
 import { UseQueryOptions, useQuery } from '@tanstack/react-query'
@@ -17,6 +20,23 @@ export const useGetEnvironmentTokens = (
     [args.workspaceId, args.projectName, args.envName, 'tokens'],
     () => {
       return getEnvironmentTokens(args)
+    },
+    opt
+  )
+
+export const useGetEnvironmentToken = (
+  args: {
+    workspaceId: string
+    projectName: string
+    envName: string
+    tokenId: string
+  },
+  opt?: UseQueryOptions<GetEnvironmentTokenData, GetEnvironmentTokenError>
+) =>
+  useQuery<GetEnvironmentTokenData, GetEnvironmentTokenError>(
+    [args.workspaceId, args.projectName, args.envName, 'tokens', args.tokenId],
+    () => {
+      return getEnvironmentToken(args)
     },
     opt
   )
