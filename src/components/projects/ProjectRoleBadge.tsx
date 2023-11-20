e8eb1edeaa09118fa9a48a667a63e5331b36b69b
dimak <rahbussiness@gmail.com>
2023-11-20 17:23:08 +0100

    padding, icon

--- src/components/projects/ProjectRoleBadge.tsx
+++ src/components/projects/ProjectRoleBadge.tsx
@@ -14,7 +14,7 @@ const ProjectRoleBadge: React.FC<Props> = ({ className, role }) => {
     <Badge
       variant="default"
       className={cn(
-        clsx(['text-[0.725rem] text-white dark:text-gray-200'], {
+        clsx(['text-[0.725rem] text-white dark:text-gray-200 pl-2'], {
           'bg-blue-600 dark:bg-blue-800/80 hover:bg-blue-600 dark:hover:bg-blue-800/80 dark:text-blue-200 text-blue-100':
             role === ProjectRole.VIEWER,
           'bg-green-600 dark:bg-green-800/80 hover:bg-green-600 dark:hover:bg-green-800/80 dark:text-green-200 text-green-100':
@@ -33,7 +33,7 @@ const ProjectRoleBadge: React.FC<Props> = ({ className, role }) => {
       )}
       {role === ProjectRole.EDITOR && (
         <div className="flex items-center gap-1.5">
-          <Icons.penSquare className="w-3.5 h-3.5" />
+          <Icons.penLine className="w-3.5 h-3.5" />
           <span>Editor</span>
         </div>
       )}
