b286ff2e34e1a536acab4c582d7bbe3d1132896c
dimak <rahbussiness@gmail.com>
2023-11-23 21:46:05 +0100

    delete session action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,13 +1,18 @@
 'use server'
 
+import { UserSession } from '@/types/session'
 import { createSession } from '@/utils/auth/session'
 import { cookies } from 'next/headers'
 
-export async function saveSession(accessToken: string) {
-  const session = await createSession(accessToken)
+export async function saveSession(data: UserSession) {
+  const session = await createSession(data)
 
   cookies().set('session', session, {
     httpOnly: true,
     maxAge: 60 * 60,
   })
+}
+
+export const deleteSession = () => {
+  cookies().delete('session')
 }
