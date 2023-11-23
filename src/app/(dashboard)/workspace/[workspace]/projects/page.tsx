465a5dc98efd48ca5c953cb160213075491774e8
dimak <rahbussiness@gmail.com>
2023-11-23 17:27:26 +0100

    validate server session

--- src/app/(dashboard)/workspace/[workspace]/projects/page.tsx
+++ src/app/(dashboard)/workspace/[workspace]/projects/page.tsx
@@ -1,11 +1,13 @@
 import { Metadata } from 'next'
 import Projects from '@/components/projects/Projects'
+import { validateServerSession } from '@/utils/auth/session'
 
 export const metadata: Metadata = {
   title: 'Projects',
 }
 
-export default function Home({ params }: { params: { workspace: string } }) {
+export default async function Home({ params }: { params: { workspace: string } }) {
+  await validateServerSession('/login')
   return (
     <>
       <Projects workspaceId={params?.workspace} />
