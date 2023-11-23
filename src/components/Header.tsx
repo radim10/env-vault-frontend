aba6c21eb2b53ba04b3d67b9c9b6d5c67b81c5ed
dimak <rahbussiness@gmail.com>
2023-11-23 21:45:52 +0100

    show name + email

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import {
   DropdownMenu,
@@ -12,6 +12,9 @@ import clsx from 'clsx'
 import { Icons } from './icons'
 import { ThemeToggle } from './ui/theme-toggle'
 import { AuthContext } from './SessionProvider'
+import { useRouter } from 'next/navigation'
+import sessionStore from '@/stores/session'
+import { deleteSession } from '@/app/actions'
 
 const dropdownItems = [
   { text: 'Account', icon: Icons.user },
@@ -19,13 +22,16 @@ const dropdownItems = [
 ]
 
 const Header = () => {
-  const session = useContext(AuthContext)
+  const router = useRouter()
+  const auth = useContext(AuthContext)
+  const session = sessionStore?.getState().data
 
   const handleSignOut = async () => {
-    await fetch('/api/logout', { method: 'POST' })
+    deleteSession()
+    router.replace('/login', { scroll: false })
   }
 
-  console.log(session)
+  console.log(auth)
 
   return (
     <div className="flex flex-row justify-between items-center">
@@ -33,33 +39,39 @@ const Header = () => {
       <div className="flex items-center gap-2 md:gap-3">
         <ThemeToggle />
         {session?.accessToken}
-        <DropdownMenu>
-          <DropdownMenuTrigger className="flex gap-2 items-center">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="mt-2.5 mr-8 text-md">
-            {dropdownItems?.map((item, index) => (
-              <>
-                <DropdownMenuItem
-                  onClick={() => {
-                    if (index === 1) {
-                      handleSignOut()
-                    }
-                  }}
-                  className={clsx(['flex gap-3 items-center'], {
-                    'hover:text-red-500 text-red-500': index === 1,
-                  })}
-                >
-                  <item.icon className=" h-4 w-4 opacity-70" />
-                  <>{item.text}</>
-                </DropdownMenuItem>
-              </>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex items-center gap-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="flex gap-2 items-center">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="mt-2.5 mr-8 text-md">
+              {dropdownItems?.map((item, index) => (
+                <>
+                  <DropdownMenuItem
+                    onClick={() => {
+                      if (index === 1) {
+                        handleSignOut()
+                      }
+                    }}
+                    className={clsx(['flex gap-3 items-center'], {
+                      'hover:text-red-500 text-red-500': index === 1,
+                    })}
+                  >
+                    <item.icon className=" h-4 w-4 opacity-70" />
+                    <>{item.text}</>
+                  </DropdownMenuItem>
+                </>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <div className="flex flex-col">
+            <div className="text-[0.9rem] font-medium">{auth?.name}</div>
+            <div className="text-sm">{auth?.email}</div>
+          </div>
+        </div>
       </div>
     </div>
   )
