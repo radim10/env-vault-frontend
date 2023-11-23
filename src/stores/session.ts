1acfee71cdb07548c6f18bea71cee417bf298421
dimak <rahbussiness@gmail.com>
2023-11-23 21:45:44 +0100

    session store

--- src/stores/session.ts
+++ src/stores/session.ts
@@ -1,15 +1,25 @@
 import { UserSession } from '@/types/session'
 import { create } from 'zustand'
 
-export interface UseSessionState {
+export interface SessionState {
   data: UserSession | null
 }
 
-export interface UseSessionActions {
+export interface SessionActions {
   set: (user: UserSession | null) => void
 }
 
-export const useSession = create<UseSessionState & UseSessionActions>((set) => ({
+// export const useSession = create<UseSessionState & UseSessionActions>((set) => ({
+//   data: null,
+//   set: (user) => set({ data: user }),
+// }))
+
+import { createStore } from 'zustand/vanilla'
+
+const sessionStore = createStore<SessionState & SessionActions>((set) => ({
   data: null,
   set: (user) => set({ data: user }),
 }))
+const { getState, setState, subscribe } = sessionStore
+
+export default sessionStore
