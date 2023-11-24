7823cb7aeacc4fe3c5ad3b26ff26f2df9996021e
dimak <rahbussiness@gmail.com>
2023-11-24 17:45:55 +0100

    redirect

--- src/app/(dashboard)/workspace/page.tsx
+++ src/app/(dashboard)/workspace/page.tsx
@@ -1,10 +1,33 @@
+import { UserSession } from '@/types/session'
 import { validateServerSession } from '@/utils/auth/session'
 import { redirect } from 'next/navigation'
+
+// TODO: check session -> refresh
+const getDefaultWorkspace = async (session?: UserSession) => {
+  const res = await fetch(`http://localhost:8080/api/v1/me/default-workspace`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store',
+  })
+
+  console.log(res.status)
+  if (!res.ok) return undefined
+  let body = (await res.json()) as { id: string }
+
+  return body
+}
 
 // TODO: get default workspace and redirect
 const WorkspacePage = async (props: {}) => {
   await validateServerSession('/login')
-  redirect(`/workspace/4ef8a291-024e-4ed8-924b-1cc90d01315e/projects`)
+
+  const workspaceData = await getDefaultWorkspace()
+
+  redirect(`/workspace/${workspaceData?.id}/projects`)
+
+  // redirect(`/workspace/4ef8a291-024e-4ed8-924b-1cc90d01315e/projects`)
 }
 
 export default WorkspacePage
