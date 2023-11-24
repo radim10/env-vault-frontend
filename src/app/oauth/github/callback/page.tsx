14938db1ffdcbd864035e6f8af6d203c4b96a686
dimak <rahbussiness@gmail.com>
2023-11-24 14:47:26 +0100

    page

--- src/app/oauth/github/callback/page.tsx
+++ src/app/oauth/github/callback/page.tsx
@@ -21,12 +21,21 @@ async function getGithubUser(code: string) {
   return body
 }
 
+async function getDefaultWorkspace() {
+  const { data } = await new Promise((resolve) => setTimeout(resolve, 250)).then(() => ({
+    data: { id: '4ef8a291-024e-4ed8-924b-1cc90d01315e' },
+  }))
+
+  return data
+}
+
 export default async function Page({ searchParams }: { searchParams: { code: string } }) {
   // setCookie('key', 'value')
   // console.log(searchParams)
   // console.log(searchParams?.code)
   //
   const res = (await getGithubUser(searchParams?.code)) as UserSession
+  const workspaceData = await getDefaultWorkspace()
   // console.log(res)
   // const res = await fetch('http://localhost:3000/api', { method: 'POST' })
   // console.log(res)
@@ -63,5 +72,5 @@ export default async function Page({ searchParams }: { searchParams: { code: str
   //     // redirect("/")
   //   }
 
-  return <CookieAuth data={res} />
+  return <CookieAuth data={res} workspaceId={workspaceData?.id} />
 }
