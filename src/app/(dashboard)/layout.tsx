0584b33dbb96a13bd489d24b9c84a962da9d3921
dimak <rahbussiness@gmail.com>
2023-11-24 13:44:58 +0100

    Dashboard layout - session + provider

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,23 +1,40 @@
 import Header from '@/components/Header'
 import ScrollToTopButton from '@/components/ScrollToTopBtn'
+import AuthProvider from '@/components/SessionProvider'
 import Sidebar from '@/components/Sidebar'
+import { UserSession } from '@/types/session'
+import { getSession } from '@/utils/auth/session'
+import { redirect } from 'next/navigation'
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  console.log('dashboard layout running')
+  const session = await getSession()
+  console.log(session)
+
+  if (!session) {
+    redirect('/login')
+  }
+
   return (
     <>
-      <div className="flex md:flex-row flex-col">
-        <div className="md:w-[280px] lg:w-[320px] md:sticky top-0 md:h-screen w-screen">
-          <Sidebar />
-        </div>
-        {/* <div className="h-full w-full flex-grow px-6 py-6 lg:px-10"> */}
-        <div className="h-full w-full flex-grow">
-          <div className="md:block hidden px-6 pt-6 lg:px-10">
-            <Header />
+      <AuthProvider
+        // user={{ email: userEmail as string, name: name as string }}
+        session={session as UserSession}
+      >
+        <div className="flex md:flex-row flex-col">
+          <div className="md:w-[280px] lg:w-[320px] md:sticky top-0 md:h-screen w-screen">
+            <Sidebar />
           </div>
-          <div className="md:pt-8 pt-5 pb-20">{children}</div>
+          {/* <div className="h-full w-full flex-grow px-6 py-6 lg:px-10"> */}
+          <div className="h-full w-full flex-grow">
+            <div className="md:block hidden px-6 pt-6 lg:px-10">
+              <Header />
+            </div>
+            <div className="md:pt-8 pt-5 pb-20">{children}</div>
+          </div>
         </div>
-      </div>
-      <ScrollToTopButton />
+        <ScrollToTopButton />
+      </AuthProvider>
     </>
   )
 }
