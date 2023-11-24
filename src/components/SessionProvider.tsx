62d4eded183006835a96b09c960ce57d63909977
dimak <rahbussiness@gmail.com>
2023-11-24 13:19:08 +0100

    fetch current user

--- src/components/SessionProvider.tsx
+++ src/components/SessionProvider.tsx
@@ -1,10 +1,11 @@
 'use client'
 
+import { getCurrentUserData } from '@/app/actions'
 import sessionStore from '@/stores/session'
 import useCurrentUserStore from '@/stores/user'
 import { UserSession } from '@/types/session'
 import { WorkspaceUserRole } from '@/types/users'
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 import { useMount } from 'react-use'
 
 // interface Props {
@@ -13,19 +14,22 @@ import { useMount } from 'react-use'
 // }
 interface Props {
   session: UserSession
-  user: {
-    email: string
-    name: string
-  }
+  // user: {
+  //   email: string
+  //   name: string
+  // }
   children: React.ReactNode
 }
 
 export const AuthContext = createContext<{ email: string; name: string } | null>(null)
 
-const AuthProvider: React.FC<Props> = ({ user, session, children }) => {
-  const { set } = useCurrentUserStore()
+const AuthProvider: React.FC<Props> = ({ session, children }) => {
+  const { set, data } = useCurrentUserStore()
+  const [user, setUser] = useState<{ email: string; name: string }>()
 
-  useMount(() => {
+  useMount(async () => {
+    const user = await getCurrentUserData()
+    console.log(user)
     set({
       email: user?.email,
       role: WorkspaceUserRole.ADMIN,
@@ -35,10 +39,18 @@ const AuthProvider: React.FC<Props> = ({ user, session, children }) => {
   })
 
   // TODO: get current user from database and set state
-  // if (true) {
-  //   return <>Loading</>
-  // }
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
+  if (data === null) {
+    return (
+      <>
+        <div className="h-screen w-screen flex justify-center items-center">
+          <div className="w-8 h-8 rounded-full animate-spin border-4 border border-solid border-primary border-t-transparent"></div>
+        </div>
+      </>
+    )
+  }
+  //
+  return <>{children}</>
+  // return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
 }
 
 export default AuthProvider
