b3b46686f49129063c7cbf66fe4fed7c25763964
dimak <rahbussiness@gmail.com>
2023-11-25 18:53:45 +0100

    get workspace

--- src/app/oauth/google/callback/page.tsx
+++ src/app/oauth/google/callback/page.tsx
@@ -20,18 +20,38 @@ async function handleGoogleAuth(code: string) {
   return body
 }
 
-async function getDefaultWorkspace() {
-  const { data } = await new Promise((resolve) => setTimeout(resolve, 250)).then(() => ({
-    data: { id: '4ef8a291-024e-4ed8-924b-1cc90d01315e' },
-  }))
+// async function getDefaultWorkspace() {
+//   const { data } = await new Promise((resolve) => setTimeout(resolve, 250)).then(() => ({
+//     data: { id: '4ef8a291-024e-4ed8-924b-1cc90d01315e' },
+//   }))
+//
+//   return data
+// }
+//
+const getDefaultWorkspace = async (session?: UserSession) => {
+  const res = await fetch(`http://localhost:8080/api/v1/me/default-workspace`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store',
+  })
 
-  return data
+  console.log(res.status)
+  if (!res.ok) return undefined
+  let body = (await res.json()) as { id: string | null }
+
+  return body
 }
 
 export default async function Page({ searchParams }: { searchParams: { code: string } }) {
   console.log('Code: ', searchParams?.code)
   const res = (await handleGoogleAuth(searchParams?.code)) as UserSession
   const workspaceData = await getDefaultWorkspace()
+
+  if (!workspaceData) {
+    return <>Something went wrong</>
+  }
 
   return <CookieAuth data={res} workspaceId={workspaceData?.id} />
 }
