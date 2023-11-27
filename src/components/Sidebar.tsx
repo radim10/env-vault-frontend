1c17ccde534edd2d53baa1132aff9adaf542a890
dimak <rahbussiness@gmail.com>
2023-11-27 11:31:21 +0100

    sidebar - workspace dialog state

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import clsx from 'clsx'
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { Icons } from './icons'
 import { Separator } from './ui/separator'
 import WorkspaceSelect from './WorkspaceSelect'
-import { usePathname, useParams } from 'next/navigation'
+import { usePathname, useParams, useRouter } from 'next/navigation'
 import { useLockBodyScroll, useToggle } from 'react-use'
 import useCurrentUserStore from '@/stores/user'
+import CreateWorkspaceDialog from './CreateWorkspaceDialog'
+import { useToast } from './ui/use-toast'
 
 const navItems = [
   { label: 'Projects', href: 'projects', icon: Icons.folder },
@@ -25,9 +27,15 @@ const helpNavItems = [
 ]
 
 const Sidebar = () => {
+  const { toast } = useToast()
+  const router = useRouter()
   const currentUser = useCurrentUserStore((state) => state.data)
-  const [opened, setOpened] = React.useState(false)
+  const [opened, setOpened] = useState(false)
   const [scrollLocked, setSrcollLocked] = useToggle(false)
+
+  const [createWorkspaceDialog, setCreateWorkspaceDialog] = useState<{ opened: boolean } | null>(
+    null
+  )
 
   const pathname = usePathname()
   const params = useParams()
@@ -39,91 +47,120 @@ const Sidebar = () => {
 
   useLockBodyScroll(scrollLocked)
 
+  const handleCloseDialog = () => {
+    setCreateWorkspaceDialog({ opened: false })
+    setTimeout(() => {
+      setCreateWorkspaceDialog(null)
+    }, 200)
+  }
+
+  const handleNewWorkspace = (data: { id: string; name: string }) => {
+    handleCloseDialog()
+
+    toast({
+      variant: 'success',
+      title: 'Workspace created',
+      description: 'Your workspace has been created',
+    })
+
+    router.push(`/workspace/${data.id}/projects`)
+  }
+
   return (
-    <div className="h-full md:border-r-[1.2px]  border-b-[1.2px] dark:border-gray-800 bg-background z-50">
-      <div className="md:pt-10 pt-4 pb-4 md:pb-0 md:px-9 px-6">
-        <div className="hidden md:block">LOGO</div>
-        <button className="block md:hidden" onClick={toggle}>
-          {opened ? <Icons.x /> : <Icons.menu />}
-        </button>
-      </div>
-      <div
-        className={clsx(
-          [
-            'shadow-lg dark:shadow-gray-900 md:shadow-none w-screen md:w-auto fixed md:relative pt-6 md:pt-16 bg-background z-[1000] border-b-[1.2px] dark:border-gray-800 md:border-0 rounded-b-lg md:rounded-b-none',
-          ],
-          {
-            hidden: !opened,
-            block: opened,
-            'md:block': true,
-          }
-        )}
-      >
-        <div className="-ml-1 pr-4">
-          <div className="pl-6 pr-1">
-            <WorkspaceSelect
-              currentWorkspace={
-                currentUser?.workspaces?.find((val) => val?.selected === true) as any
-              }
-              allWorkspaces={currentUser?.workspaces as any}
-            />
-          </div>
+    <>
+      {createWorkspaceDialog !== null && (
+        <CreateWorkspaceDialog
+          opened={createWorkspaceDialog?.opened === true}
+          onSuccess={handleNewWorkspace}
+          onClose={handleCloseDialog}
+        />
+      )}
+      <div className="h-full md:border-r-[1.2px]  border-b-[1.2px] dark:border-gray-800 bg-background z-50">
+        <div className="md:pt-10 pt-4 pb-4 md:pb-0 md:px-9 px-6">
+          <div className="hidden md:block">LOGO</div>
+          <button className="block md:hidden" onClick={toggle}>
+            {opened ? <Icons.x /> : <Icons.menu />}
+          </button>
         </div>
-        <div className="mt-5 py-5 pl-9 pr-6">
-          <div className="bg-red-40 flex flex-col gap-3">
-            {navItems.map((item, index) => (
-              <>
-                <Link
-                  onClick={() => {
-                    if (opened) setOpened(false)
-                  }}
-                  href={`/workspace/${params.workspace}/${item.href}`}
-                  className={clsx(
-                    [
-                      'flex gap-4 items-center dark:hover:text-primary dark:text-gray-300 ease duration-150',
-                    ],
-                    {
-                      // 'text-primary dark:text-primary': pathname === item.href,
-                      'text-primary dark:text-primary':
-                        pathname?.split('/')?.[3] === item.href ||
-                        (pathname?.split('/')?.[3] === item.href?.split('/')[0] && index === 3) ||
-                        (pathname?.split('/')?.[3] === item.href?.split('/')[0] && index === 2) ||
-                        (pathname?.split('/')?.[3] === item.href?.split('/')[0] && index === 4) ||
-                        (pathname?.split('/')?.[3] === 'teams' && index === 2) ||
-                        (pathname?.split('/')?.[3] === 'user' && index === 2) ||
-                        (pathname?.split('/')?.[3] === 'create-project' && index === 0),
-                    }
-                  )}
+        <div
+          className={clsx(
+            [
+              'shadow-lg dark:shadow-gray-900 md:shadow-none w-screen md:w-auto fixed md:relative pt-6 md:pt-16 bg-background z-[1000] border-b-[1.2px] dark:border-gray-800 md:border-0 rounded-b-lg md:rounded-b-none',
+            ],
+            {
+              hidden: !opened,
+              block: opened,
+              'md:block': true,
+            }
+          )}
+        >
+          <div className="-ml-1 pr-4">
+            <div className="pl-6 pr-1">
+              <WorkspaceSelect
+                currentWorkspace={
+                  currentUser?.workspaces?.find((val) => val?.selected === true) as any
+                }
+                allWorkspaces={currentUser?.workspaces as any}
+                onCreate={() => setCreateWorkspaceDialog({ opened: true })}
+              />
+            </div>
+          </div>
+          <div className="mt-5 py-5 pl-9 pr-6">
+            <div className="bg-red-40 flex flex-col gap-3">
+              {navItems.map((item, index) => (
+                <>
+                  <Link
+                    onClick={() => {
+                      if (opened) setOpened(false)
+                    }}
+                    href={`/workspace/${params.workspace}/${item.href}`}
+                    className={clsx(
+                      [
+                        'flex gap-4 items-center dark:hover:text-primary dark:text-gray-300 ease duration-150',
+                      ],
+                      {
+                        // 'text-primary dark:text-primary': pathname === item.href,
+                        'text-primary dark:text-primary':
+                          pathname?.split('/')?.[3] === item.href ||
+                          (pathname?.split('/')?.[3] === item.href?.split('/')[0] && index === 3) ||
+                          (pathname?.split('/')?.[3] === item.href?.split('/')[0] && index === 2) ||
+                          (pathname?.split('/')?.[3] === item.href?.split('/')[0] && index === 4) ||
+                          (pathname?.split('/')?.[3] === 'teams' && index === 2) ||
+                          (pathname?.split('/')?.[3] === 'user' && index === 2) ||
+                          (pathname?.split('/')?.[3] === 'create-project' && index === 0),
+                      }
+                    )}
+                  >
+                    {/* {pathname === item.href} */}
+                    <item.icon className=" h-5 w-5 opacity-70" />
+                    <div className="text-[1.14rem]">{item.label}</div>
+                  </Link>
+
+                  {/* {pathname.split('/')?.[3] == 'teams' && index === 2 && ( */}
+                  {/*   <div className=" ml-5 flex gap-4 items-center dark:hover:text-primary dark:text-gray-300 ease duration-150"> */}
+                  {/*     <Icons.users2 className="h-4 w-4 opacity-70" /> */}
+                  {/*     <div className="text-[1.1rem]">Teams</div> */}
+                  {/*   </div> */}
+                  {/* )} */}
+                </>
+              ))}
+            </div>
+            <Separator className="my-6" />
+            <div className="bg-red-40 flex flex-col gap-3">
+              {helpNavItems.map((item) => (
+                <a
+                  href={item.href}
+                  className="flex gap-4 items-center dark:hover:text-primary dark:text-gray-300 ease duration-150"
                 >
-                  {/* {pathname === item.href} */}
                   <item.icon className=" h-5 w-5 opacity-70" />
                   <div className="text-[1.14rem]">{item.label}</div>
-                </Link>
-
-                {/* {pathname.split('/')?.[3] == 'teams' && index === 2 && ( */}
-                {/*   <div className=" ml-5 flex gap-4 items-center dark:hover:text-primary dark:text-gray-300 ease duration-150"> */}
-                {/*     <Icons.users2 className="h-4 w-4 opacity-70" /> */}
-                {/*     <div className="text-[1.1rem]">Teams</div> */}
-                {/*   </div> */}
-                {/* )} */}
-              </>
-            ))}
-          </div>
-          <Separator className="my-6" />
-          <div className="bg-red-40 flex flex-col gap-3">
-            {helpNavItems.map((item) => (
-              <a
-                href={item.href}
-                className="flex gap-4 items-center dark:hover:text-primary dark:text-gray-300 ease duration-150"
-              >
-                <item.icon className=" h-5 w-5 opacity-70" />
-                <div className="text-[1.14rem]">{item.label}</div>
-              </a>
-            ))}
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 export default Sidebar
