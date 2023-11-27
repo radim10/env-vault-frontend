f1a5d922b76b3201ee22f2f922fb5330c4c68592
dimak <rahbussiness@gmail.com>
2023-11-27 17:28:57 +0100

    personal settings tabs

---
+++ src/components/personalSettings/Tabs.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import clsx from 'clsx'
+import Link from 'next/link'
+import { useParams } from 'next/navigation'
+
+interface Props {
+  workspaceId: string
+}
+
+const items = [
+  { label: 'General', href: 'general' },
+  { label: 'Authentication', href: 'authentication' },
+  { label: 'preferences', href: 'preferences' },
+]
+
+const SettingsTabs: React.FC<Props> = ({ workspaceId }) => {
+  const params = useParams<{ workspace: string; tab: string }>()
+
+  return (
+    <div className="px-6 lg:px-10 dark:text-gray-400 flex items-center gap-2 md:gap-4 overflow-y-auto text-[1rem] border-b-[1px] pb-0 md:px-3">
+      {items.map(({ label, href }) => (
+        <Link
+          href={`/workspace/${workspaceId}/personal-settings/${href}`}
+          className={clsx(
+            [
+              'border-b-[3px] flex items-center gap-2 h-full hover:dark:text-gray-200 ease duration-150 px-2 lg:px-3 pb-3',
+            ],
+            {
+              'border-primary dark:text-gray-200 font-medium': params?.tab === href?.toLowerCase(),
+              'border-b-transparent text-gray-700 dark:text-gray-400':
+                params?.tab !== href?.toLowerCase(),
+            }
+          )}
+        >
+          <div>{label}</div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default SettingsTabs
