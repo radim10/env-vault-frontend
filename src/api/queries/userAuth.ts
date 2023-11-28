d33aae2731a75d2c7e5fca7799e1bcff6e12f7d8
dimak <rahbussiness@gmail.com>
2023-11-28 21:42:39 +0100

    comment

--- src/api/queries/userAuth.ts
+++ src/api/queries/userAuth.ts
@@ -1,6 +1,7 @@
 import { UseQueryOptions, useQuery } from '@tanstack/react-query'
 import { ListUserSessionsData, ListUserSessionsError, listUserSessions } from '../requests/userAuth'
 
+// TODO: key with user id?
 export const useListUserSessions = (
   opt?: UseQueryOptions<ListUserSessionsData, ListUserSessionsError>
 ) => useQuery<ListUserSessionsData, ListUserSessionsError>(['sessions'], listUserSessions, opt)
