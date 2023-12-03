80ae7547f53e830c0cf196d3c8436c3b97552127
dimak <rahbussiness@gmail.com>
2023-12-03 18:50:31 +0100

    no referrer policy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
   },
+
+  async headers() {
+    return [
+      {
+        source: '/reset-password',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
