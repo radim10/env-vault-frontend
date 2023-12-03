5ade4e04113ed4ff2b221092c151b66e8510f516
dimak <rahbussiness@gmail.com>
2023-12-03 18:50:19 +0100

    auth utils

---
+++ src/utils/auth/auth.ts
@@ -0,0 +1,31 @@
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+const upperCaseRegex = /[A-Z]/
+const lowerCaseRegex = /[a-z]/
+const numberRegex = /\d/
+
+export const passwordRules = [
+  {
+    name: 'Minimum 10 characters',
+    check: (password: string) => password.length >= 10,
+  },
+  {
+    name: 'At least 1 uppercase letter',
+    check: (password: string) => upperCaseRegex.test(password),
+  },
+  {
+    name: 'At least 1 lowercase letter',
+    check: (password: string) => lowerCaseRegex.test(password),
+  },
+  {
+    name: 'At least 1 number',
+    check: (password: string) => numberRegex.test(password),
+  },
+]
+
+export const authRegex = {
+  email: emailRegex,
+  passwordUpperCase: upperCaseRegex,
+  passwordLowerCase: lowerCaseRegex,
+  passwordNumber: numberRegex,
+}
