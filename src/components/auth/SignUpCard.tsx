4908bd6eb6a4f4f556de7dd145c5c6f8e388a8c2
dimak <rahbussiness@gmail.com>
2023-12-04 21:13:39 +0100

    styling, checkbox

--- src/components/auth/SignUpCard.tsx
+++ src/components/auth/SignUpCard.tsx
@@ -4,12 +4,19 @@ import { Icons } from '@/components/icons'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
 import { useGetGithubUrl, useGetGoogleUrl } from '@/api/queries/auth'
+import { Checkbox } from '../ui/checkbox'
+import { useState } from 'react'
+import clsx from 'clsx'
+import GoogleIcon from './GoogleIcon'
 
 interface SignUpCardProps {
   onEmailSelect: () => void
 }
 
 const SignUpCard: React.FC<SignUpCardProps> = ({ onEmailSelect }) => {
+  const [checked, setChecked] = useState(false)
+  const [animateCheck, setAnimateCheck] = useState(false)
+
   const { refetch: getGoogleLink, isRefetching: getGoogleLinkLoading } = useGetGoogleUrl(null, {
     enabled: false,
 
@@ -25,6 +32,25 @@ const SignUpCard: React.FC<SignUpCardProps> = ({ onEmailSelect }) => {
     },
   })
 
+  const handleButtonClick = (type: 'github' | 'google' | 'email') => {
+    if (!checked) {
+      setAnimateCheck(true)
+      setTimeout(() => {
+        setAnimateCheck(false)
+      }, 400)
+
+      return
+    }
+
+    if (type === 'github') {
+      getGithubUrl()
+    } else if (type === 'google') {
+      getGoogleLink()
+    } else if (type === 'email') {
+      onEmailSelect()
+    }
+  }
+
   return (
     <div>
       <Card className="sm:w-[420px] w-[90vw]">
@@ -35,44 +61,90 @@ const SignUpCard: React.FC<SignUpCardProps> = ({ onEmailSelect }) => {
               <Icons.rocket className="w-4 h-4 mt-1" />
             </div>
 
-            <Button
-              size={'sm'}
-              onClick={onEmailSelect}
-              disabled={getGithubUrlLoading || getGoogleLinkLoading}
-            >
-              Sign up with email
-            </Button>
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
-              </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
-              </div>
-            </div>
+            {/* <Button */}
+            {/*   size={'default'} */}
+            {/*   onClick={onEmailSelect} */}
+            {/*   disabled={getGithubUrlLoading || getGoogleLinkLoading} */}
+            {/* > */}
+            {/*   Sign up with email */}
+            {/* </Button> */}
+            {/* <div className="relative"> */}
+            {/*   <div className="absolute inset-0 flex items-center"> */}
+            {/*     <span className="w-full border-t" /> */}
+            {/*   </div> */}
+            {/*   <div className="relative flex justify-center text-xs uppercase"> */}
+            {/*     <span className="bg-background px-2 text-muted-foreground">Or continue with</span> */}
+            {/*   </div> */}
+            {/* </div> */}
+
             <div className="flex flex-col gap-2">
               <Button
-                size={'sm'}
+                size={'default'}
                 variant="outline"
                 type="button"
+                className="gap-3"
                 disabled={getGithubUrlLoading || getGoogleLinkLoading}
                 loading={getGithubUrlLoading}
-                onClick={() => getGithubUrl()}
+                onClick={() => handleButtonClick('github')}
               >
-                <Icons.github className="mr-2 h-4 w-4" />
-                Github
+                <Icons.github className="h-4 w-4 opacity-70" />
+                Continue with Github
               </Button>
 
               <Button
                 disabled={getGoogleLinkLoading || getGithubUrlLoading}
                 loading={getGoogleLinkLoading}
-                onClick={() => getGoogleLink()}
+                onClick={() => handleButtonClick('google')}
                 variant="outline"
-                size={'sm'}
+                size={'default'}
                 type="button"
+                className="gap-3"
               >
-                Google
+                <GoogleIcon />
+                <span>Continue with Google</span>
               </Button>
+            </div>
+
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">OR</span>
+              </div>
+            </div>
+            <Button
+              variant="outline"
+              size={'default'}
+              onClick={() => handleButtonClick('email')}
+              disabled={getGithubUrlLoading || getGoogleLinkLoading}
+              className="gap-3"
+            >
+              <Icons.mail className="h-4 w-4" />
+              Continue with email
+            </Button>
+
+            <div className={clsx(['flex items-center w-full justify-end  mt-2'], {})}>
+              <div
+                className={clsx(['flex space-x-3'], {
+                  shake: animateCheck === true && checked === false,
+                })}
+              >
+                <label
+                  htmlFor="terms"
+                  className="cursor-pointer text-sm font-normal leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Accept terms and conditions
+                </label>
+                <Checkbox
+                  id="terms"
+                  checked={checked}
+                  onCheckedChange={() => setChecked(!checked)}
+                  className={clsx(['dark:border-gray-600 border-gray-800'], {
+                    'border-primary dark:border-primary': checked,
+                  })}
+                />
+              </div>
             </div>
           </div>
         </CardContent>
