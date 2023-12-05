4ce213a50114e31461c16558d543d910511a2e23
dimak <rahbussiness@gmail.com>
2023-12-05 09:29:52 +0100

    state

--- src/components/Invitation.tsx
+++ src/components/Invitation.tsx
@@ -11,6 +11,8 @@ import { Card, CardContent } from '@/components/ui/card'
 import { useGetGithubUrl, useGetGoogleUrl } from '@/api/queries/auth'
 import clsx from 'clsx'
 import { Checkbox } from './ui/checkbox'
+import EmailSignUp from './auth/EmailSignUp'
+import GoogleIcon from './auth/GoogleIcon'
 
 interface InvitationProps {
   id: string
@@ -19,6 +21,7 @@ interface InvitationProps {
 }
 
 export const Invitation: React.FC<InvitationProps> = ({ id, workspace, role }) => {
+  const [emailSelected, setEmailSelected] = useState(false)
   const [checked, setChecked] = useState(false)
   const [animateCheck, setAnimateCheck] = useState(false)
 
@@ -65,11 +68,11 @@ export const Invitation: React.FC<InvitationProps> = ({ id, workspace, role }) =
               ZenEnv invitation
             </h1>
             <div className="text-muted-foreground">
-              Don't have an account?{' '}
+              Already have an account?
               <span>
-                <Link href="/signup">
+                <Link href="/login">
                   <Button variant="link" className="pl-0.5">
-                    Sign up
+                    Login
                   </Button>
                 </Link>
               </span>
@@ -77,115 +80,120 @@ export const Invitation: React.FC<InvitationProps> = ({ id, workspace, role }) =
           </div>
           {/* // FORM */}
           <div>
-            <Card className="sm:w-[420px] w-[90vw]">
-              <CardContent className="pt-5">
-                <div className="flex flex-col gap-5">
-                  <div className="flex flex-row items-center justify-center gap-2 w-full opacity-80">
-                    You have been invited to ZenEnv
-                    <Icons.userPlus className="w-4 h-4" />
-                  </div>
-
-                  <Separator />
-                  <div className="flex flex-col gap-1.5 mb-2">
-                    <div className="flex flex-row gap-2 flex-wrap">
-                      <div className="font-medium">Workspace:</div>
-                      <div>{workspace}</div>
+            {emailSelected && (
+              <EmailSignUp invitation={id} onCancel={() => setEmailSelected(false)} />
+            )}
+            {!emailSelected && (
+              <Card className="sm:w-[420px] w-[90vw]">
+                <CardContent className="pt-5">
+                  <div className="flex flex-col gap-4">
+                    <div className="flex flex-row items-center justify-center gap-2 w-full opacity-80">
+                      You have been invited to ZenEnv
+                      <Icons.userPlus className="w-4 h-4" />
                     </div>
 
-                    <div className="flex flex-row gap-2">
-                      <div className="font-medium">User role:</div>
-                      <div>
-                        <UserRoleBadge role={role} />
+                    <Separator />
+                    <div className="flex flex-col gap-1.5 mb-2">
+                      <div className="flex flex-row gap-2 flex-wrap">
+                        <div className="font-medium">Workspace:</div>
+                        <div>{workspace}</div>
+                      </div>
+
+                      <div className="flex flex-row gap-2">
+                        <div className="font-medium">User role:</div>
+                        <div>
+                          <UserRoleBadge role={role} />
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* <Button disabled={getGithubUrlLoading || getGoogleLinkLoading} size={'sm'}> */}
+                    {/*   Login with email */}
+                    {/* </Button> */}
+                    {/* <div className="relative"> */}
+                    {/*   <div className="absolute inset-0 flex items-center"> */}
+                    {/*     <span className="w-full border-t" /> */}
+                    {/*   </div> */}
+                    {/*   <div className="relative flex justify-center text-xs uppercase"> */}
+                    {/*     <span className="bg-background px-2 text-muted-foreground"> */}
+                    {/*       Or continue with */}
+                    {/*     </span> */}
+                    {/*   </div> */}
+                    {/* </div> */}
+                    <div className="flex flex-col gap-2">
+                      <Button
+                        type="button"
+                        size={'default'}
+                        variant="outline"
+                        className="gap-3"
+                        disabled={getGithubUrlLoading || getGoogleLinkLoading}
+                        loading={getGithubUrlLoading}
+                        onClick={() => getGithubUrl()}
+                      >
+                        <Icons.github className="h-4 w-4 opacity-70" />
+                        Continue with GitHub
+                      </Button>
+
+                      <Button
+                        disabled={getGoogleLinkLoading || getGithubUrlLoading}
+                        loading={getGoogleLinkLoading}
+                        onClick={() => getGoogleLink()}
+                        variant="outline"
+                        className="gap-3"
+                        type="button"
+                        size={'sm'}
+                      >
+                        <GoogleIcon />
+                        Continue with Google
+                      </Button>
+                    </div>
+
+                    <div className="relative">
+                      <div className="absolute inset-0 flex items-center">
+                        <span className="w-full border-t" />
+                      </div>
+                      <div className="relative flex justify-center text-xs uppercase">
+                        <span className="bg-background px-2 text-muted-foreground">OR</span>
+                      </div>
+                    </div>
+
+                    <Button
+                      variant="outline"
+                      size={'default'}
+                      onClick={() => handleButtonClick('email')}
+                      disabled={getGithubUrlLoading || getGoogleLinkLoading}
+                      className="gap-3"
+                    >
+                      <Icons.mail className="h-4 w-4 opacity-70" />
+                      Continue with email
+                    </Button>
+
+                    <div className={clsx(['flex items-center w-full justify-end  mt-2'], {})}>
+                      <div
+                        className={clsx(['flex space-x-3'], {
+                          shake: animateCheck === true && checked === false,
+                        })}
+                      >
+                        <label
+                          htmlFor="terms"
+                          className="cursor-pointer text-sm font-normal leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                          Accept terms and conditions
+                        </label>
+                        <Checkbox
+                          id="terms"
+                          checked={checked}
+                          onCheckedChange={() => setChecked(!checked)}
+                          className={clsx(['dark:border-gray-600 border-gray-800'], {
+                            'border-primary dark:border-primary': checked,
+                          })}
+                        />
                       </div>
                     </div>
                   </div>
-
-                  {/* <Button disabled={getGithubUrlLoading || getGoogleLinkLoading} size={'sm'}> */}
-                  {/*   Login with email */}
-                  {/* </Button> */}
-                  {/* <div className="relative"> */}
-                  {/*   <div className="absolute inset-0 flex items-center"> */}
-                  {/*     <span className="w-full border-t" /> */}
-                  {/*   </div> */}
-                  {/*   <div className="relative flex justify-center text-xs uppercase"> */}
-                  {/*     <span className="bg-background px-2 text-muted-foreground"> */}
-                  {/*       Or continue with */}
-                  {/*     </span> */}
-                  {/*   </div> */}
-                  {/* </div> */}
-                  <div className="flex flex-col gap-2">
-                    <Button
-                      type="button"
-                      size={'default'}
-                      variant="outline"
-                      className="gap-3"
-                      disabled={getGithubUrlLoading || getGoogleLinkLoading}
-                      loading={getGithubUrlLoading}
-                      onClick={() => getGithubUrl()}
-                    >
-                      <Icons.github className="h-4 w-4 opacity-70" />
-                      Github
-                    </Button>
-
-                    <Button
-                      disabled={getGoogleLinkLoading || getGithubUrlLoading}
-                      loading={getGoogleLinkLoading}
-                      onClick={() => getGoogleLink()}
-                      variant="outline"
-                      className="gap-3"
-                      type="button"
-                      size={'sm'}
-                    >
-                      <Icons.mail className="h-4 w-4" />
-                      Continue with email
-                    </Button>
-                  </div>
-
-                  <div className="relative">
-                    <div className="absolute inset-0 flex items-center">
-                      <span className="w-full border-t" />
-                    </div>
-                    <div className="relative flex justify-center text-xs uppercase">
-                      <span className="bg-background px-2 text-muted-foreground">OR</span>
-                    </div>
-                  </div>
-
-                  <Button
-                    variant="outline"
-                    size={'default'}
-                    onClick={() => handleButtonClick('email')}
-                    disabled={getGithubUrlLoading || getGoogleLinkLoading}
-                    className="gap-3"
-                  >
-                    <Icons.mail className="h-4 w-4 opacity-70" />
-                    Continue with email
-                  </Button>
-
-                  <div className={clsx(['flex items-center w-full justify-end  mt-2'], {})}>
-                    <div
-                      className={clsx(['flex space-x-3'], {
-                        shake: animateCheck === true && checked === false,
-                      })}
-                    >
-                      <label
-                        htmlFor="terms"
-                        className="cursor-pointer text-sm font-normal leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                      >
-                        Accept terms and conditions
-                      </label>
-                      <Checkbox
-                        id="terms"
-                        checked={checked}
-                        onCheckedChange={() => setChecked(!checked)}
-                        className={clsx(['dark:border-gray-600 border-gray-800'], {
-                          'border-primary dark:border-primary': checked,
-                        })}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
+                </CardContent>
+              </Card>
+            )}
           </div>
         </div>
       </div>
