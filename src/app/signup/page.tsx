bba349c41e56c74e503d62013b6d65792e6d86a6
dimak <rahbussiness@gmail.com>
2023-12-06 13:30:04 +0100

    auth page - session redirect

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,55 +1,13 @@
-'use client'
+import SignUp from '@/components/auth/SignUp'
+import { getSession } from '@/utils/auth/session'
 
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import SignUpCard from '@/components/auth/SignUpCard'
-import { useState } from 'react'
-import EmailSignUp from '@/components/auth/EmailSignUp'
-import clsx from 'clsx'
-import AuthFooter from '@/components/auth/Footer'
+const SignUpPage = async () => {
+  const session = await getSession()
 
-const SignUpPage = () => {
-  const [emailSignUp, setEmailSignUp] = useState<boolean>(false)
-
-  return (
-    <div
-      className={clsx(
-        ['flex flex-col gap-4 justify-center w-full bg-red-400X h-screen items-center'],
-        {
-          // '-mt-10': !emailSignUp,
-        }
-      )}
-    >
-      <div className="flex flex-col items-center gap-4">
-        {!emailSignUp && (
-          <>
-            <div className="flex flex-col gap-1 items-center">
-              <h1 className="dark:text-gray-200 scroll-m-20 text-4xl font-semibold tracking-tight transition-colors first:mt-0">
-                Sign Up to ZenEnv
-              </h1>
-              <div className="text-muted-foreground">
-                Already have an account?
-                <span>
-                  <Link href="/login">
-                    <Button variant="link" className="pl-1.5">
-                      Login
-                    </Button>
-                  </Link>
-                </span>
-              </div>
-            </div>
-            {/* // FORM */}
-            <div>
-              <SignUpCard onEmailSelect={() => setEmailSignUp(true)} />
-            </div>
-          </>
-        )}
-        {emailSignUp && <EmailSignUp onCancel={() => setEmailSignUp(false)} />}
-      </div>
-      {/* // */}
-      {!emailSignUp && <AuthFooter />}
-    </div>
-  )
+  if (session) {
+    redirect('/workspace')
+  }
+  return <SignUp />
 }
 
 export default SignUpPage
