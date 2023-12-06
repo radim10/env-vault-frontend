e2fb18b1310182bc6115c5c6ac25dc1c9945a8df
dimak <rahbussiness@gmail.com>
2023-12-06 17:37:28 +0100

    style

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,6 +1,6 @@
 const PageLoader = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center overflow-hidden">
+    <div className="h-screen w-screen flex justify-center items-center overflow-x-hidden overflow-y-hidden">
       <div className="w-8 h-8 rounded-full animate-spin border-4 border border-solid border-primary border-t-transparent"></div>
     </div>
   )
