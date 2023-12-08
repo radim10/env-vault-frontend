a121c36c0327427e5944a7a225f0a146dbeef0dd
dimak <rahbussiness@gmail.com>
2023-12-08 20:45:35 +0100

    props

--- src/components/OAuthError.tsx
+++ src/components/OAuthError.tsx
@@ -1,14 +1,22 @@
 import Link from 'next/link'
 import { Icons } from './icons'
 import { Button } from './ui/button'
+import { OauthErrorCode } from '@/utils/serverRequests'
 
-const OauthError = () => {
+interface Props {
+  errorCode?: OauthErrorCode
+}
+
+const OauthError: React.FC<Props> = ({ errorCode }) => {
   return (
     <div className=" flex justify-center items-center h-[70vh] px-4">
       <div className="flex flex-col justify-center gap-6">
         <h1 className="text-center dark:text-gray-200 scroll-m-20 text-4xl font-semibold tracking-tight transition-colors first:mt-0">
-          Something went {'wrong '}
-          <Icons.frown className="inline-block h-8 w-8" />
+          {errorCode && errorCode === 'user_banned' && 'Your account is banned'}
+          {errorCode && errorCode === 'invalid_code' && 'Invalid code'}
+          {errorCode && errorCode === 'unknown' && 'Something went wrong'}
+          {!errorCode && 'Something went wrong'}
+          <Icons.frown className="inline-block h-8 w-8 ml-2" />
         </h1>
 
         <div className="flex flex-row w-full gap-3">
