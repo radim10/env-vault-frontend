54e08a0f89782acdae697d0be03b99b03118af18
dimak <rahbussiness@gmail.com>
2023-12-08 18:40:50 +0100

    session store - loggingOut state

--- src/stores/session.ts
+++ src/stores/session.ts
@@ -3,15 +3,19 @@ import { create } from 'zustand'
 
 export interface SessionState {
   data: UserSession | null
+  loggingOut: boolean
 }
 
 export interface SessionActions {
   set: (user: UserSession | null) => void
+  setLoggingOut: (loggingOut: boolean) => void
 }
 
 const useSessionStore = create<SessionState & SessionActions>((set) => ({
   data: null,
+  loggingOut: false,
   set: (user) => set({ data: user }),
+  setLoggingOut: (loggingOut) => set({ loggingOut }),
 }))
 
 export default useSessionStore
