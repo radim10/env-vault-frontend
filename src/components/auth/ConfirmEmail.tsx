f8c3e983632529b32fcbbb64ed75d3efc4d34a23
dimak <rahbussiness@gmail.com>
2023-12-09 20:33:59 +0100

    props

--- src/components/auth/ConfirmEmail.tsx
+++ src/components/auth/ConfirmEmail.tsx
@@ -1,16 +1,21 @@
 'use client'
 
-import Link from 'next/link'
 import { Icons } from '../icons'
 import { Button } from '../ui/button'
 import { useRouter } from 'next/navigation'
 import { Card, CardContent } from '@/components/ui/card'
 
 interface Props {
-  error?: 'invalid_token' | 'token_expired' | 'email_already_confirmed' | 'unknown'
+  error?:
+    | 'invalid_token'
+    | 'token_expired'
+    | 'email_already_confirmed'
+    | 'email_not_available'
+    | 'unknown'
+  isChange?: boolean
 }
 
-const ConfirmEmail: React.FC<Props> = ({ error }) => {
+const ConfirmEmail: React.FC<Props> = ({ error, isChange }) => {
   const router = useRouter()
 
   const goToLogin = () => {
@@ -24,7 +29,9 @@ const ConfirmEmail: React.FC<Props> = ({ error }) => {
           {error && (
             <>
               <div className="flex flex-row gap-2 items-center">
-                <div className="font-semibold text-lg text-red-600">Email confirmation failed</div>
+                <div className="font-semibold text-lg text-red-600">
+                  {!isChange ? 'Email confirmation failed' : 'Email change failed'}
+                </div>
                 <Icons.xCircle className="text-red-600 h-5 w-5" />
               </div>
               <div className="mt-3 text-[0.92rem] opacity-90">
@@ -39,11 +46,15 @@ const ConfirmEmail: React.FC<Props> = ({ error }) => {
           {!error && (
             <>
               <div className="flex flex-row gap-2 items-center">
-                <div className="font-semibold text-lg text-green-600">Email has been confirmed</div>
+                <div className="font-semibold text-lg text-green-600">
+                  {!isChange ? 'Email has been confirmed' : 'Email has been changed'}
+                </div>
                 <Icons.checkCircle2 className="text-green-600 h-5 w-5" />
               </div>
               <div className="mt-3 text-[0.92rem] opacity-90">
-                Your email has been confirmed, you can login now
+                {!isChange
+                  ? 'Your email has been confirmed, you can login now'
+                  : 'Your email has been changed, you can now login with new email'}
               </div>
             </>
           )}
