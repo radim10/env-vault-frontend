abe1bd3195f7cda82d76a648feb51fcf0766ddf4
dimak <rahbussiness@gmail.com>
2023-12-10 16:46:00 +0100

    error component

--- src/components/SessionProvider.tsx
+++ src/components/SessionProvider.tsx
@@ -8,6 +8,8 @@ import { produce } from 'immer'
 import { useParams, useRouter } from 'next/navigation'
 import { useMount } from 'react-use'
 import PageLoader from './PageLoader'
+import { Icons } from './icons'
+import { Button } from './ui/button'
 
 // interface Props {
 //   session: UserSession | null
@@ -31,7 +33,7 @@ const AuthProvider: React.FC<Props> = ({ session, children }) => {
   // TODO: check if accessToken is expired and refresh
 
   // NOTE: or use server action but implment retry + error handling
-  const { isLoading } = useGetCurrentUser(
+  const { isLoading, error, refetch } = useGetCurrentUser(
     {
       // TODO: workspaceId???
       // workspaceId: '4ef8a291-024e-4ed8-924b-1cc90d01315e',
@@ -85,9 +87,38 @@ const AuthProvider: React.FC<Props> = ({ session, children }) => {
       </>
     )
   }
-  //
+
+  if (error) {
+    return <AuthProviderFallback onRefetch={refetch} />
+  }
+
   return <>{children}</>
-  // return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
+}
+
+//
+interface AuthProviderFallbackProps {
+  onRefetch: () => void
+}
+
+const AuthProviderFallback: React.FC<AuthProviderFallbackProps> = ({ onRefetch }) => {
+  return (
+    <>
+      <div className=" flex justify-center items-center h-[100vh] px-4">
+        <div className="flex flex-col justify-center gap-6">
+          <h1 className="text-center dark:text-gray-200 scroll-m-20 text-4xl font-semibold tracking-tight transition-colors first:mt-0">
+            Something went wrong
+          </h1>
+
+          <div className="flex flex-row w-full gap-3">
+            <Button className="w-full gap-3" variant="outline" onClick={onRefetch}>
+              <Icons.refresh className="inline-block h-4 w-4" />
+              Try again
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default AuthProvider
