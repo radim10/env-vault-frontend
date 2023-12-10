43718f1fcc7f7cc9942eaaea90bef2f6ee386788
dimak <rahbussiness@gmail.com>
2023-12-10 18:55:16 +0100

    component

--- src/components/personalSettings/GeneralSettings.tsx
+++ src/components/personalSettings/GeneralSettings.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import DangerZone from '../DangerZone'
+import DeleteAccount from './DeleteAccount'
 import ProfileSection from './ProfileSection'
 import WorkspacePreferences from './WorkspacePreferences'
 
@@ -12,16 +12,13 @@ const GeneralSettings = () => {
           <ProfileSection />
         </div>
         {/* // */}
-        <WorkspacePreferences />
+        <div>
+          <WorkspacePreferences />
+        </div>
         {/* // */}
-        <DangerZone
-          btn={{
-            onClick: () => {},
-            disabled: false,
-          }}
-          title="Delete my account"
-          description="Permanently delete this account, cannot be undone"
-        />
+        <div>
+          <DeleteAccount />
+        </div>
       </div>
     </>
   )
