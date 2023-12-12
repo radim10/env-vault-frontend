e5a6b8a94f307528b6a1be844ddb4ce44211c40a
dimak <rahbussiness@gmail.com>
2023-12-12 20:45:56 +0100

    delete account dialog

--- src/components/personalSettings/DeleteAccountDialog.tsx
+++ src/components/personalSettings/DeleteAccountDialog.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import DeleteDialog from '../DeleteDialog'
-import { Input } from '../ui/input'
 import { Icons } from '../icons'
-import { useDeleteProject } from '@/api/mutations/projects'
-import { projectErrorMsgFromCode } from '@/api/requests/projects/root'
-import { useUpdateEffect } from 'react-use'
-import { Textarea } from '../ui/textarea'
+import { useState } from 'react'
+import { Input } from '../ui/input'
 import { Button } from '../ui/button'
+import { Textarea } from '../ui/textarea'
+import DeleteDialog from '../DeleteDialog'
+import { useUpdateEffect } from 'react-use'
+import { useDeleteAccount } from '@/api/mutations/currentUser'
+import { currentUserErrorMsgFromCode } from '@/api/requests/currentUser'
 
 interface Props {
   opened: boolean
@@ -19,11 +19,11 @@ const DeleteAccountDialog: React.FC<Props> = ({ opened, onClose, onSuccess }) =>
   const [step, setStep] = useState<0 | 1>(0)
 
   const {
-    mutate: deleteProject,
+    mutate: deleteAccount,
     isLoading,
     error,
     reset,
-  } = useDeleteProject({
+  } = useDeleteAccount({
     onSuccess: () => onSuccess(),
   })
 
@@ -43,7 +43,6 @@ const DeleteAccountDialog: React.FC<Props> = ({ opened, onClose, onSuccess }) =>
         disabledConfirm={confirmText !== 'Delete account'}
         title="Delete your account?"
         onConfirm={() => setStep(1)}
-        description=""
         descriptionComponent={
           step === 0 && (
             <>
@@ -61,18 +60,23 @@ const DeleteAccountDialog: React.FC<Props> = ({ opened, onClose, onSuccess }) =>
         {step === 1 && (
           <div className="flex flex-col gap-2 p0-4 pb-0 mt-2">
             <div className="flex flex-col gap-3">
-              <Button className="h-20" variant={'destructive'}>
-                Delete account
+              <Button
+                className="h-20"
+                loading={isLoading}
+                variant={'destructive'}
+                onClick={() => deleteAccount(undefined)}
+              >
+                {isLoading ? 'Deleting...' : 'Delete account'}
               </Button>
 
-              <Button className="h-20" variant={'outline'} onClick={onClose}>
+              <Button className="h-20" variant={'outline'} onClick={onClose} disabled={isLoading}>
                 Cancel action
               </Button>
             </div>
             {error && (
               <div className="text-red-600 text-[0.92rem] flex items-center gap-2 mt-0">
                 <Icons.xCircle className="h-4 w-4" />
-                {projectErrorMsgFromCode(error.code) ?? 'Something went wrong'}
+                {currentUserErrorMsgFromCode(error.code) ?? 'Something went wrong'}
               </div>
             )}
           </div>
@@ -97,13 +101,6 @@ const DeleteAccountDialog: React.FC<Props> = ({ opened, onClose, onSuccess }) =>
               <div className="text-[0.92rem]">Tell us why you are deleting your account</div>
               <Textarea placeholder={'Optional'} rows={1} />
             </div>
-
-            {error && (
-              <div className="text-red-600 text-[0.92rem] flex items-center gap-2 mt-0">
-                <Icons.xCircle className="h-4 w-4" />
-                {projectErrorMsgFromCode(error.code) ?? 'Something went wrong'}
-              </div>
-            )}
           </div>
         )}
       </DeleteDialog>
