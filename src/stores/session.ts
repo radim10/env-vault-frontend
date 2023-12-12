7ae4ccea1c2f87f667277c3d3686ef1743ae95f1
dimak <rahbussiness@gmail.com>
2023-12-12 20:46:07 +0100

    session store - remove cookie

--- src/stores/session.ts
+++ src/stores/session.ts
@@ -9,13 +9,27 @@ export interface SessionState {
 export interface SessionActions {
   set: (user: UserSession | null) => void
   setLoggingOut: (loggingOut: boolean) => void
+  removeCookie: () => Promise<void>
 }
 
-const useSessionStore = create<SessionState & SessionActions>((set) => ({
+const useSessionStore = create<SessionState & SessionActions>((set, get) => ({
   data: null,
   loggingOut: false,
   set: (user) => set({ data: user }),
   setLoggingOut: (loggingOut) => set({ loggingOut }),
+  removeCookie: async () => {
+    if (get()?.loggingOut !== true) {
+      set({ loggingOut: true })
+    }
+
+    await fetch('/api/logout', {
+      method: 'POST',
+    })
+
+    if (get()?.loggingOut !== false) {
+      set({ loggingOut: false })
+    }
+  },
 }))
 
 export default useSessionStore
