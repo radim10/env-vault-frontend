b3a072105e4386ac1563f16b7a0a11245db66a07
dimak <rahbussiness@gmail.com>
2023-12-12 20:45:48 +0100

    delete account logic

--- src/components/personalSettings/DeleteAccount.tsx
+++ src/components/personalSettings/DeleteAccount.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
 import DangerZone from '../DangerZone'
 import DeleteAccountDialog from './DeleteAccountDialog'
+import useSessionStore from '@/stores/session'
+import { useRouter } from 'next/navigation'
+import { useToast } from '../ui/use-toast'
 
 const DeleteAccount = () => {
+  const router = useRouter()
+  const { toast } = useToast()
+  const session = useSessionStore()
+
   const [deleteDialogOpened, setDeleteDialogOpened] = useState<boolean | null>(null)
 
   const closeDialog = () => {
@@ -14,13 +21,23 @@ const DeleteAccount = () => {
     }, 150)
   }
 
+  const handleSuccesfulDelete = async () => {
+    await session.removeCookie()
+    toast({
+      variant: 'success',
+      title: 'Account deleted',
+      description: 'Your account has been permanently deleted',
+    })
+    router.push('/login')
+  }
+
   return (
     <>
       {deleteDialogOpened !== null && (
         <DeleteAccountDialog
           opened={deleteDialogOpened === true}
           onClose={closeDialog}
-          onSuccess={() => {}}
+          onSuccess={handleSuccesfulDelete}
         />
       )}
       <DangerZone
