5c43c5fe9a8973f5068c3e4729e69b63c26f6f41
dimak <rahbussiness@gmail.com>
2023-12-15 17:43:10 +0100

    secrets change types

--- src/types/envChangelog.ts
+++ src/types/envChangelog.ts
@@ -20,35 +20,82 @@ import { EnvironmentType } from './environments'
 export interface EnvChangelogItem {
   id: string
   createdAt: string
-  change: EnvChange
+  change?: EnvChange
   user?: {
     name: string
     avatarUrl: string | null
   }
+  secrets: SecretsChange[]
   // TODO: user
 }
 
+type SecretChange =
+  | NewSecretsChange
+  | RenamedSecretsChange
+  | UpdatedSecretChange
+  | DeletedSecretsChange
+
+export type NewSecretsChange = {
+  newKey: string
+  newValue: string
+}
+
+export type RenamedSecretsChange = {
+  oldKey: string
+  newKey: string
+  value: string
+}
+
+export type UpdatedSecretChange = {
+  key: string
+  oldValue: string
+  newValue: string
+}
+
+export type DeletedSecretsChange = {
+  oldKey: string
+  oldValue: string
+}
+
+// export type EnvChange =
+//   | SecretsEnvChage
+//   | CreatedEnvChange
+//   | RenamedEnvChange
+//   | UpdatedEnvDescription
+//   | LockedEnvChange
+//   | EnvTypeChange
+//
+// export type SecretsEnvChage = {
+//   action: 'secrets'
+//   data: Array<SecretsChange>
+// }
+//
+//
 export type EnvChange =
-  | SecretsEnvChage
   | CreatedEnvChange
   | RenamedEnvChange
   | UpdatedEnvDescription
   | LockedEnvChange
   | EnvTypeChange
 
-export type SecretsEnvChage = {
-  action: 'secrets'
-  data: Array<SecretsChange>
+export interface SecretsChange {
+  key?: string
+  oldKey?: string
+  newKey?: string
+
+  value?: string
+  oldValue?: string
+  newValue?: string
 }
 
-export interface SecretsChange {
-  key: string
-  old?: string
-  new?: string
-  // only renamed
-  newKey?: string
-  value?: string
-}
+// export interface SecretsChange {
+//   key: string
+//   old?: string
+//   new?: string
+//   // only renamed
+//   newKey?: string
+//   value?: string
+// }
 
 export type CreatedEnvChange = {
   action: 'created'
