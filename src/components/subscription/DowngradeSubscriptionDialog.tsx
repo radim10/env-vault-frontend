b071bebc18f94aae6e2d4d0fecdee82614dbbb48
dimak <rahbussiness@gmail.com>
2024-01-04 18:32:17 +0100

    fn

--- src/components/subscription/DowngradeSubscriptionDialog.tsx
+++ src/components/subscription/DowngradeSubscriptionDialog.tsx
@@ -1,5 +1,4 @@
-import DeleteDialog from '../DeleteDialog'
-import { useCancelSubscription } from '@/api/mutations/subscription'
+import { useDowngradeSubscription } from '@/api/mutations/subscription'
 import { Button } from '../ui/button'
 
 interface Props {
@@ -8,29 +7,31 @@ interface Props {
   onSuccess: () => void
 }
 
-const CancelSubscriptionDialog: React.FC<Props> = ({ onClose, onSuccess }) => {
-  const { mutate: cancelSubscription, isLoading } = useCancelSubscription({
+const DowngradeSubscriptionDialog: React.FC<Props> = ({ workspaceId, onClose, onSuccess }) => {
+  const { mutate: downgradeSubscription, isLoading } = useDowngradeSubscription({
     onSuccess: () => onSuccess(),
   })
 
   return (
     <>
       <div className=" fixed left-[50%] text-start top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full">
-        <div className="text-lg font-semibold">Cancel subscription</div>
+        <div className="text-lg font-semibold">Downgrade subscription</div>
         <div>
           <div className="text-[0.92rem] text-foreground">
-            After current billing cycle ends, your paid subscription will be degraded to{' '}
+            After current billing cycle ends, your paid subscription will be dowgraded to{' '}
             <b>Startup</b> plan. Please note that you will have to remove any exceeding workspace
             members and projects when the billing cycle ends.
           </div>
         </div>
         <div className="mt-2 flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2">
-          <Button variant="outline" onClick={() => onClose()}>
+          <Button variant="outline" onClick={() => onClose()} disabled={isLoading}>
             Cancel
           </Button>
           <Button
+            loading={isLoading}
             variant="outline"
             className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+            onClick={() => downgradeSubscription({ workspaceId })}
           >
             Confirm
           </Button>
@@ -40,4 +41,4 @@ const CancelSubscriptionDialog: React.FC<Props> = ({ onClose, onSuccess }) => {
   )
 }
 
-export default CancelSubscriptionDialog
+export default DowngradeSubscriptionDialog
