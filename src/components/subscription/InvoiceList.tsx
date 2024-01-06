adbe74d896ab5d1366242774b9f622c1e9394ecb
dimak <rahbussiness@gmail.com>
2024-01-06 13:44:08 +0100

    loading + err component

--- src/components/subscription/InvoiceList.tsx
+++ src/components/subscription/InvoiceList.tsx
@@ -1,42 +1,48 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
 import { Icons } from '../icons'
 import SubscriptionLayout from './SubscriptionLayout'
-import { useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import { listInvoices } from '@/api/requests/subscription'
 import { Button } from '../ui/button'
 import dayjs from 'dayjs'
 import { Separator } from '../ui/separator'
+import { Skeleton } from '../ui/skeleton'
 
 interface Props {
   workspaceId: string
 }
 
 const InvoiceList: React.FC<Props> = ({ workspaceId }) => {
-  const [hasMore, setHasMore] = useState(true)
+  const {
+    data,
+    error,
+    isFetching,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+    isRefetching,
+    isLoading,
+    refetch,
+  } = useInfiniteQuery(
+    [workspaceId, 'invoices'],
+    async ({ pageParam }) => {
+      const cursor = pageParam ? pageParam : undefined
 
-  const { data, error, isFetching, isFetchingNextPage, fetchNextPage, isRefetching, isLoading } =
-    useInfiniteQuery(
-      [workspaceId, 'invoices'],
-      async ({ pageParam }) => {
-        const cursor = pageParam ? pageParam : undefined
+      const res = await listInvoices({
+        workspaceId,
+        cursor,
+      })
 
-        const res = await listInvoices({
-          workspaceId,
-          cursor,
-        })
-
-        setHasMore(res?.cursor ? true : false)
-
-        return res
-        // return res.data
-      },
-      {
-        enabled: true,
-        structuralSharing: false,
-        getPreviousPageParam: (firstPage) => firstPage?.cursor,
-        getNextPageParam: (lastPage) => lastPage?.cursor,
-      }
-    )
+      return res
+      // return res.data
+    },
+    {
+      enabled: true,
+      structuralSharing: false,
+      getPreviousPageParam: (firstPage) => firstPage?.cursor,
+      getNextPageParam: (lastPage) => lastPage?.cursor,
+    }
+  )
 
   const flattenedData = useMemo(
     () => (data ? data?.pages.flatMap((item) => item.data) : []),
@@ -44,59 +50,86 @@ const InvoiceList: React.FC<Props> = ({ workspaceId }) => {
   )
 
   if (isLoading) {
-    return <>Loading...</>
+    return <Skeleton className=" border-2 h-40 w-full" />
   }
 
   return (
     <SubscriptionLayout title="Invoices" icon={Icons.receipt}>
-      <div className="flex flex-col gap-3">
-        {flattenedData.map((item, index) => (
-          <>
-            <div className="hidden md:flex flex-row gap-3 items-center justify-between">
-              <div className="gap-3 grid grid-cols-3 items-center w-full text-[0.93rem]">
-                <div>#{item?.number}</div>
-                <div>{dayjs(item?.createdAt).format('YYYY-MM-DD')}</div>
-                <div>${item?.amount}</div>
-              </div>
-              <Button
-                size="sm"
-                variant="outline"
-                onClick={() => {
-                  window.open(item.url as string, '_blank')
-                }}
-              >
-                <Icons.download className="h-4 w-4" />
+      {(error as any) && (
+        <div className="h-44 flex flex-row items-center">
+          <div className="flex flex-col gap-2 w-full items-center">
+            <div className="text-red-600 text-[0.92rem]">Something went wrong</div>
+            <div>
+              <Button size={'sm'} variant="outline" onClick={() => refetch()}>
+                Try again
               </Button>
             </div>
-
-            <div className="md:hidden flex flex-row gap-3 items-center justify-between text-[0.93rem]">
-              <div className="flex flex-col gap-2">
-                <div>#{item?.number}</div>
-                <div>{dayjs(item?.createdAt).format('YYYY-MM-DD')}</div>
-                <div>${item?.amount}</div>
-              </div>
-              <Button
-                size="sm"
-                variant="outline"
-                onClick={() => {
-                  window.open(item.url as string, '_blank')
-                }}
-              >
-                <Icons.download className="h-4 w-4" />
-              </Button>
-            </div>
-            {index !== flattenedData.length - 1 && <Separator />}
-          </>
-        ))}
-      </div>
-
-      {isFetchingNextPage && <div>Loading...</div>}
-      {!isFetchingNextPage && hasMore && (
-        <div className="flex justify-center items-center mt-5">
-          <Button className="w-fit" variant="outline" size={'sm'}>
-            Load more
-          </Button>
+          </div>
         </div>
+      )}
+      {!error && (
+        <>
+          <div className="flex flex-col gap-3">
+            {flattenedData.map((item, index) => (
+              <>
+                <div className="hidden md:flex flex-row gap-3 items-center justify-between">
+                  <div className="gap-3 grid grid-cols-3 items-center w-full text-[0.93rem]">
+                    <div>#{item?.number}</div>
+                    <div>{dayjs(item?.createdAt).format('YYYY-MM-DD')}</div>
+                    <div>${item?.amount}</div>
+                  </div>
+                  <Button
+                    size="sm"
+                    variant="outline"
+                    onClick={() => {
+                      window.open(item.url as string, '_blank')
+                    }}
+                  >
+                    <Icons.download className="h-4 w-4" />
+                  </Button>
+                </div>
+
+                <div className="md:hidden flex flex-row gap-3 items-center justify-between text-[0.93rem]">
+                  <div className="flex flex-col gap-2">
+                    <div>#{item?.number}</div>
+                    <div>{dayjs(item?.createdAt).format('YYYY-MM-DD')}</div>
+                    <div>${item?.amount}</div>
+                  </div>
+                  <Button
+                    size="sm"
+                    variant="outline"
+                    onClick={() => {
+                      window.open(item.url as string, '_blank')
+                    }}
+                  >
+                    <Icons.download className="h-4 w-4" />
+                  </Button>
+                </div>
+                {index !== flattenedData.length - 1 && <Separator />}
+              </>
+            ))}
+          </div>
+
+          {isFetchingNextPage && (
+            <div className="w-full flex flex-col gap-3 mt-3">
+              {Array.from({ length: 3 }).map(() => (
+                <Skeleton className="h-9 " />
+              ))}
+            </div>
+          )}
+          {!isFetchingNextPage && hasNextPage && (
+            <div className="flex justify-center items-center mt-5">
+              <Button
+                className="w-fit"
+                variant="outline"
+                size={'sm'}
+                onClick={() => fetchNextPage()}
+              >
+                Load more
+              </Button>
+            </div>
+          )}
+        </>
       )}
     </SubscriptionLayout>
   )
--- src/components/subscription/SubscriptionSettings.tsx
+++ src/components/subscription/SubscriptionSettings.tsx
@@ -5,6 +5,10 @@ import SubscriptionOverview from './SubscriptionOverview'
 import PaymentDetails from './PaymentDetails'
 import InvoiceList from './InvoiceList'
 import useCurrentUserStore from '@/stores/user'
+import { Skeleton } from '../ui/skeleton'
+import SubscriptionLayout from './SubscriptionLayout'
+import { Icons } from '../icons'
+import { Button } from '../ui/button'
 
 interface Props {
   workspaceId: string
@@ -25,26 +29,41 @@ const SubscriptionSettings: React.FC<Props> = ({ workspaceId }) => {
     }
   )
 
-  const { data, isLoading, isError } = useGetSubscription(workspaceId)
-
-  if (isLoading) {
-    return <>Loading...</>
-  }
-
-  if (isError) {
-    return <>Error</>
-  }
+  const { data, isLoading, error } = useGetSubscription(workspaceId)
 
   return (
-    <div className="flex flex-col gap-7">
-      {/* <Button onClick={() => refetch()}>Get url</Button> */}
-      <SubscriptionOverview
-        workspaceId={workspaceId}
-        data={data.subscription}
-        usersCount={data.usersCount}
-      />
-      {data.subscription?.payment && isOwnerRole() === true && (
-        <PaymentDetails workspaceId={workspaceId} paymentData={data.subscription?.payment} />
+    <div className="flex flex-col gap-7 mt-2">
+      {isLoading && (
+        <>
+          <Skeleton className="mt-0 border-2 h-80 w-full" />
+          {isOwnerRole() === true && <Skeleton className=" border-2 h-40 w-full" />}
+        </>
+      )}
+      {error && (
+        <SubscriptionLayout title="Overview" icon={Icons.alignLeft}>
+          <div className="h-44 flex flex-row items-center">
+            <div className="flex flex-col gap-2 w-full items-center">
+              <div className="text-red-600 text-[0.92rem]">Something went wrong</div>
+              <div>
+                <Button size={'sm'} variant="outline" onClick={() => refetch()}>
+                  Try again
+                </Button>
+              </div>
+            </div>
+          </div>
+        </SubscriptionLayout>
+      )}
+      {!isLoading && !error && (
+        <>
+          <SubscriptionOverview
+            workspaceId={workspaceId}
+            data={data.subscription}
+            usersCount={data.usersCount}
+          />
+          {data.subscription?.payment && isOwnerRole() === true && (
+            <PaymentDetails workspaceId={workspaceId} paymentData={data.subscription?.payment} />
+          )}
+        </>
       )}
       {isOwnerRole() === true && <InvoiceList workspaceId={workspaceId} />}
     </div>
