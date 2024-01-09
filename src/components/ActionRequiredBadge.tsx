9c906370d00777d9bc118ed8c5a5696b179be827
dimak <rahbussiness@gmail.com>
2024-01-09 19:50:50 +0100

    padding

--- src/components/ActionRequiredBadge.tsx
+++ src/components/ActionRequiredBadge.tsx
@@ -8,7 +8,7 @@ const ActionRequiredBadge: React.FC<Props> = ({ onClick: open }) => {
   return (
     <>
       <button className="mt-4 md:mt-3 text-sm w-full" onClick={open}>
-        <div className="flex flex-row items-center gap-2 rounded-md px-3 py-1.5  bg-red-600/90 dark:bg-red-800/50 text-white dark:text-gray-200">
+        <div className="flex flex-row items-center gap-2 rounded-md px-3 lg:px-4 py-1.5  bg-red-600/90 dark:bg-red-800/50 text-white dark:text-gray-200">
           <div className="">Action required</div>
           <div className="">
             <Icons.alertCircle className="h-4 w-4" />
